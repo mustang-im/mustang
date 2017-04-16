dcc184c1e18903b7a94c9dcd606433d06357b805
Ben Bucksch <ben.bucksch@beonex.com>
2017-04-16 18:40:08 +0200

    app.js - main process

---
+++ app.js
@@ -0,0 +1,43 @@
+/**
+ * This is the Electron main process.
+ */
+
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const url = require("url");
+require("app-module-path").addPath(__dirname + "/");
+global.__base = __dirname + "/";
+//require("logic/account/account-list");
+
+// Window will be closed once this object is garbage collected, so keep it
+var mainWindow;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  mainWindow.loadURL(url.format({
+    pathname : path.join(__dirname, "ui/mainwin/mainwin.html"),
+    protocol : 'file:',
+    slashes : true,
+  }));
+
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
