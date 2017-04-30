e0eaa68af2aeae921f2aa897246acb2e4300210c
Ben Bucksch <ben.bucksch@beonex.com>
2017-04-30 23:58:54 +0200

    Fix throttle event

--- trex/trex.js
+++ trex/trex.js
@@ -1,10 +1,10 @@
 var trexThrottleEvent = function(element, type, name) {
     var running = false;
-    obj.addEventListener(type, () => {
+    element.addEventListener(type, () => {
         if (running) { return; }
         running = true;
         requestAnimationFrame(() => {
-            obj.dispatchEvent(new CustomEvent(name));
+            element.dispatchEvent(new CustomEvent(name));
             running = false;
         });
     });
