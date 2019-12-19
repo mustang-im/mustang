d7ad130bb8bdf9da86489fc81f67bbda1a7c9e0d
Ben Bucksch <ben.bucksch@beonex.com>
2019-12-19 17:01:17 +0100

    Create React Electron desktop app - start with copy of simple UI

---
+++ react-electron/app.js
@@ -0,0 +1,46 @@
+/**
+ * This is the Electron main process.
+ */
+
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const url = require("url");
+require("app-module-path").addPath(__dirname + "/../");
+global.__base = __dirname + "/../";
+
+global.accounts = require("logic/account/account-list").getAllAccounts();
+global.makeNewAccount = require("logic/account/account-setup").makeNewAccount;
+
+// Window will be closed once this object is garbage collected, so keep it
+var mainWindow;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 800, height: 600,
+      webPreferences: { nodeIntegration: true }});
+
+  mainWindow.loadURL(url.format({
+    pathname : path.join(__dirname, "mainwin/mainwin.html"),
+    protocol : 'file:',
+    slashes : true,
+  }));
+
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
---
+++ react-electron/setup/mail-account-setup.js
@@ -0,0 +1,73 @@
+/*
+import { remote } from "electron";
+import appModulePath from "app-module-path";
+appModulePath.addPath(remote.getGlobal("__base"));
+import util from "../../util/util";
+util.importAll(util, global);
+const gAccounts = remote.getGlobal("accounts");
+const makeNewAccount = remote.getGlobal("makeNewAccount");
+*/
+var remote = require("electron").remote;
+require("app-module-path").addPath(remote.getGlobal("__base"));
+var util = require("util/util");
+util.importAll(util, global);
+var gAccounts = remote.getGlobal("accounts");
+var makeNewAccount = remote.getGlobal("makeNewAccount");
+
+
+function onLoad() {
+  try {
+    hookupReturnKey(E("realName"), function() {
+      E("emailAddress").focus();
+    });
+    hookupReturnKey(E("emailAddress"), function() {
+      E("password").focus();
+    });
+    hookupReturnKey(E("password"), function() {
+      ok();
+    });
+
+    assert(gAccounts);
+    var accountListE = new Fastlist(E("account-list"));
+    accountListE.showCollection(gAccounts);
+    gAccounts.registerObserver({
+      added : function(items) {
+        alert("Added " + items.map(account => account.emailAddress).join(", "));
+      },
+      removed : function(items) {
+      },
+    });
+  } catch(e) { errorShow(e); }
+}
+window.addEventListener("load", onLoad, false);
+
+function ok() {
+  try {
+    errorShow(false);
+    var checkingE = E("checking");
+    checkingE.removeAttribute("hidden");
+    resizeWindow();
+    var emailAddress = E("emailAddress").value;
+    var password = E("password").value;
+
+    assert(gAccounts);
+    makeNewAccount(emailAddress, gAccounts, function(account) {
+      account.setPassword(password);
+      account.login(true, function() {
+        window.close();
+      }, errorShow);
+    }, errorShow);
+  } catch (e) { errorShow(e); }
+}
+
+function errorShow(ex) {
+  var errorE = E("error");
+  if (ex) {
+    errorE.removeAttribute("hidden");
+    errorE.textContent = ex + "\n";
+    console.error(ex);
+  } else {
+    errorE.setAttribute("hidden", "true");
+  }
+  resizeWindow();
+}
