aaefc8f87fee31ecfca989e38344f3bbee432424
Ben Bucksch <ben.bucksch@beonex.com>
2021-01-12 05:58:06 +0100

    Adapt module load to Electron 11 - in React

--- react-electron/app.js
+++ react-electron/app.js
@@ -6,22 +6,25 @@ import electron from "electron";
 import path from "path";
 import url from "url";
 import appModulePath from "app-module-path";
-import accountList from "../logic/account/account-list";
-import accountSetup from "../logic/account/account-setup";
+import { getAllAccounts } from "../logic/account/account-list";
+import { makeNewAccount } from "../logic/account/account-setup";
 appModulePath.addPath(__dirname + "/../");
 global.__base = __dirname + "/../";
 
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-global.accounts = accountList.getAllAccounts();
-global.makeNewAccount = accountSetup.makeNewAccount;
+global.accounts = getAllAccounts();
+global.makeNewAccount = makeNewAccount;
 
 // Window will be closed once this object is garbage collected, so keep it
 var mainWindow;
 
 function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600,
-      webPreferences: { nodeIntegration: true }});
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    }});
 
   mainWindow.loadURL(url.format({
     pathname : path.join(__dirname, "mainwin/mainwin.html"),
