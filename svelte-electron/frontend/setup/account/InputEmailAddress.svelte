180b403b9ba16924bf329bb34ea7e456a7c6ea85
Ben Bucksch <ben.bucksch@beonex.com>
2021-01-19 02:13:37 +0100

    Input: Show errors markers next to input field

--- svelte-electron/frontend/setup/account/InputEmailAddress.svelte
+++ svelte-electron/frontend/setup/account/InputEmailAddress.svelte
@@ -6,6 +6,9 @@
     //config.password = "";
   }
 
+  let enteredRealName = false;
+  let enteredEmailAddress = false;
+  let enteredPassword = false;
   let passwordVisible = false;
   function togglePasswordView() {
     passwordVisible = !passwordVisible;
@@ -18,7 +21,9 @@
     id="realName"
     bind:value={config.realName}
     placeholder="Fred Flintstone"
+    class:entered={enteredRealName} on:blur={() => enteredRealName = true }
     required />
+  <span class="validity"/>
   <span class="explanation">Your name, as shown to others</span>
 
   <label for="emailAddress">Email address:</label>
@@ -28,22 +33,27 @@
     required
     pattern="[a-z0-9\-%+_\.\*]+@[a-z0-9\-\.]+\.[a-z]+"
     x-moz-errormessage="Please specify a valid email address"
+    class:entered={enteredEmailAddress} on:blur={() => enteredEmailAddress = true }
     on:keypress={onEmailAddressChanged} />
+  <span class="validity"/>
   <span class="explanation">Your existing email address</span>
 
   <label for="password">Password:</label>
   {#if passwordVisible }
-  <input type="text" bind:value={config.password} required />
+  <input type="text" bind:value={config.password} required
+    class:entered={enteredPassword} on:blur={() => enteredPassword = true } />
   {:else}
-  <input type="password" bind:value={config.password} required />
+  <input type="password" bind:value={config.password} required
+    class:entered={enteredPassword} on:blur={() => enteredPassword = true } />
   {/if}
+  <span class="validity"/>
   <span on:click={togglePasswordView}>Eye</span>
 </grid>
 
 <style>
   grid.inputs {
     display: grid;
-    grid-template-columns: max-content auto auto;
+    grid-template-columns: max-content auto max-content auto;
   }
   grid.inputs label {
     margin-right: 0.8em;
@@ -53,7 +63,17 @@
     margin-left: 1em;
     color: darkgrey;
   }
-  input:invalid {
+  input.entered:invalid {
     border: 2px solid red;
+  }
+  input.entered:invalid+.validity:after {
+    content: '✖';
+    padding-left: 5px;
+    color: red;
+  }
+  input:valid+.validity:after {
+    content: '✓';
+    padding-left: 5px;
+    color: green;
   }
 </style>
