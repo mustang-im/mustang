aad914cfb85e5d4396e8122340a5e48dfbd8031d
Ben Bucksch <ben.bucksch@beonex.com>
2023-03-22 01:16:35 +0100

    Add Svelte vite template, adapted

---
+++ svelte-gecko/src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import svelteLogo from './asset/icon/svelte.svg';
+  import viteLogo from '/icon/vite.svg';
+</script>
+
+<main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Mustang</h1>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+</style>
