7b0e11da02cee0462e339ae7328f67553b87d9a7
Ben Bucksch <ben.bucksch@beonex.com>
2023-03-22 01:16:35 +0100

    Add Svelte vite template, adapted

---
+++ svelte-gecko/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 5454,
+    strictPort: true,
+  },
+  plugins: [svelte()],
+})
