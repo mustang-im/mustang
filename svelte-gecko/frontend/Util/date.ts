c5ebef1b2ce256ea81a0b02c143b7eebd4de3e61
Ben Bucksch <ben.bucksch@beonex.com>
2023-04-19 08:42:12 +0200

    Short date strings

--- svelte-gecko/frontend/Util/date.ts
+++ svelte-gecko/frontend/Util/date.ts
@@ -9,26 +9,16 @@
 * See also <https://momentjs.com> for relative time
 */
 export function getDateString(date: Date): string {
-  let day = getDay(date);
   var dateDetails = null;
-  let today = getDay(null);
-  if (day.getMilliseconds() == today.getMilliseconds()) {
+  let today = new Date();
+  if (date.getDate() == today.getDate()) {
     dateDetails = { hour: "numeric", minute: "numeric" };
-  } else if (today.getMilliseconds() - day.getMilliseconds() < 7 * 24 * 60 * 60 * 1000) { // this week
+  } else if (today.getMilliseconds() - date.getMilliseconds() < 7 * 24 * 60 * 60 * 1000) { // this week
     dateDetails = { weekday: "short", hour: "numeric", minute: "numeric" };
-  } else if (day.getFullYear() == today.getFullYear()) { // this year
+  } else if (date.getFullYear() == today.getFullYear()) { // this year
     dateDetails = { month: "numeric", day: "numeric", hour: "numeric", minute: "numeric" };
   } else {
     dateDetails = { year: "numeric", month: "numeric", day: "numeric", hour: "numeric", minute: "numeric" };
   }
   return date.toLocaleString(navigator.language, dateDetails);
-}
-
-export function getDay(date: Date): Date {
-  let day = new Date(date);
-  day.setHours(0);
-  day.setMinutes(0);
-  day.setSeconds(0);
-  day.setMilliseconds(0);
-  return day;
 }
