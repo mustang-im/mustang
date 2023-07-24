6333cd4659bf23611a0685def0afa76503d8c336
Ben Bucksch <ben.bucksch@beonex.com>
2023-07-24 04:07:22 +0200

    Calendar: Title bar

--- svelte-gecko/frontend/Calendar/CalendarApp.svelte
+++ svelte-gecko/frontend/Calendar/CalendarApp.svelte
@@ -1,5 +1,5 @@
 <vbox class="app">
-  Calendar
+  <TitleBar bind:dateRange bind:dateInterval on:addEvent={addEvent} />
   <Stack>
     <hbox>dhdhd</hbox>
     <hbox>uzrztz</hbox>
@@ -7,7 +7,15 @@
 </vbox>
 
 <script lang="ts">
+  import TitleBar from "./TitleBar.svelte";
   import Stack from "../Shared/Stack.svelte";
+  
+  export let dateRange = new Date();
+  export let dateInterval: 1 | 7 | 31 | 28 = 7;
+
+  function addEvent() {
+    alert("add event");
+  }
 </script>
 
 <style>
