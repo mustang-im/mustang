d004890be9719015af03d5c9efdf270cb2d147c7
Ben Bucksch <ben.bucksch@beonex.com>
2023-11-22 05:24:52 +0100

    Matrix: Remove shadow message after sending

--- svelte-gecko/logic/Chat/Matrix/MatrixChatRoom.ts
+++ svelte-gecko/logic/Chat/Matrix/MatrixChatRoom.ts
@@ -11,10 +11,13 @@ export class MatrixChatRoom extends Chat {
   /** Our user wants to send this message out.
    * Data like recipient etc. is in the message object. */
   async sendMessage(message: UserChatMessage) {
-    console.log("Sending " + message.text + " to " + this.name);
-    await this.account.client.sendHtmlMessage(this.id, message.text, message.html);
-    //this.account.client.encryptAndSendToDevices();
     message.deliveryStatus = DeliveryStatus.Sending;
     this.messages.add(message);
+    //console.log("Sending", message.text, "to", this.name);
+    //this.account.client.encryptAndSendToDevices();
+    let response = await this.account.client.sendHtmlMessage(this.id, message.text, message.html);
+    message.id = response.event_id;
+    // By the time send() returns async, the server already sent us the message to the room
+    this.messages.remove(message);
   }
 }
