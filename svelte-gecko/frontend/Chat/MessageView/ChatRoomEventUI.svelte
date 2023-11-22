80822d7bb38a7e457edf8c09b81f2632dab38457
Ben Bucksch <ben.bucksch@beonex.com>
2023-11-22 07:15:29 +0100

    Chat: Refactor message vs. room event display

---
+++ svelte-gecko/frontend/Chat/MessageView/ChatRoomEventUI.svelte
@@ -0,0 +1,35 @@
+<hbox flex
+  class="chatroomevent"
+  class:incoming={!message.outgoing}
+  class:outgoing={message.outgoing}
+  >
+  <vbox class="right">
+    <hbox class="text selectable">{@html message.html }</hbox>
+  </vbox>
+</hbox>
+
+<script lang="ts">
+  import type { ChatRoomEvent } from "../../../logic/Chat/RoomEvent";
+
+  export let message: ChatRoomEvent;
+</script>
+
+<style>
+  .chatroomevent {
+    margin: 8px 32px 0 20px;
+    color: black;
+  }
+  .incoming {
+    align-self: flex-start;
+  }
+  .outgoing {
+    align-self: flex-end;
+  }
+
+  .text {
+    font-size: 9px;
+  }
+  .text :global(.person) {
+    color: blue;
+  }
+</style>
