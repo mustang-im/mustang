e80270674bf5f0a51495eb2ec76c76852202b03f
Ben Bucksch <ben.bucksch@beonex.com>
2023-11-29 17:23:32 +0100

    AppBar: Less width

--- svelte-gecko/frontend/MainWindow/AppBar.svelte
+++ svelte-gecko/frontend/MainWindow/AppBar.svelte
@@ -52,7 +52,7 @@
 
 <style>
   .app-bar {
-    width: 80px;
+    width: 64px;
     background-color: #ccc7d2;
     box-shadow: 1px 0px 3px 0px rgba(22, 12, 39, 20%); /* #160C27 */
     padding-top: 8px;
