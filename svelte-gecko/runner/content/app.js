969eb63dea376f1ce8e61c335ddf44969ec6dd80
Neil Rashbrook <neil.rashbrook@beonex.com>
2023-11-30 20:39:41 +0000

    Modernise the Gecko runner
    Most of XUL has been removed from Gecko, so I've replaced its use in app.xhtml with the equivalent XHTML.
    Modern Gecko requires HTTP content be loaded in a remote browser, using the `browser.loadURI()` call from an onload handler.
    The type="content-primary" attribute has also changed into type="content" primary="true" attributes, but the latter isn't useful with remote browsers, so that's also been removed.
    I don't know what the point of the separate app.xhtml and app.html files were, so I removed the latter.

--- svelte-gecko/runner/content/app.js
+++ svelte-gecko/runner/content/app.js
@@ -1,4 +1,11 @@
+var Services = Cu.createServicesCache();
 
 function start() {
+  console.log("starting");
+  let browser = document.getElementById("app-frame");
+  let triggeringPrincipal = Services.scriptSecurityManager.createNullPrincipal({});
+  browser.loadURI("http://localhost:5454/", { triggeringPrincipal });
   console.log("started");
 }
+
+window.onload = start;
