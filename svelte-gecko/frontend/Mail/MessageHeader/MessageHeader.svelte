80657b87c1e15d1b32ac06f7ff37876df15eb6a6
Ben Bucksch <ben.bucksch@beonex.com>
2023-12-06 00:43:38 +0100

    Mail: Message header: like Elena's design

--- svelte-gecko/frontend/Mail/MessageHeader/MessageHeader.svelte
+++ svelte-gecko/frontend/Mail/MessageHeader/MessageHeader.svelte
@@ -1,40 +1,108 @@
-<grid class="message-header">
-  <hbox class="label">Subject</hbox>
-  <value id="msg-subject">{message.subject}</value>
-  <hbox class="label">From</hbox>
-  <value id="msg-from">{from}</value>
-  <hbox class="label">To</hbox>
-  <value id="msg-from">{to}</value>
-  <hbox class="label">Date</hbox>
-  <value id="msg-date">{getDateString(message.received)}</value>
-</grid>
+<vbox class="message-header" class:outgoing={message.outgoing}>
+  <value class="subject">{message.subject}</value>
+  <hbox>
+    {#if message.contact instanceof Person}
+      <PersonPicture person={message.contact} />
+    {/if}
+    <vbox>
+      <value class="from">{from}</value>
+      <hbox class="to">
+        to&nbsp;
+        <value>{to}</value>
+      </hbox>
+    </vbox>
+    <hbox flex />
+    <vbox>
+      <hbox class="buttons">
+        <hbox class="star" class:starred={message.starred}>
+          <Button
+            icon={StarIcon}
+            iconSize="24px"
+            iconOnly
+            label="Remember this message"
+            on:click={toggleStar}
+            plain
+            />
+        </hbox>
+        <hbox>
+          <Button
+            icon={MoreIcon}
+            iconSize="24px"
+            iconOnly
+            label="Additional commands for this message"
+            plain
+            />
+        </hbox>
+      </hbox>
+      <value class="date">{getDateString(message.received)}</value>
+    </vbox>
+  </hbox>
+</vbox>
 
 <script lang="ts">
-  import type { Email } from "mustang-lib";
+  //import type { Email } from "mustang-lib";
+  import type { EMail } from "../../../logic/Mail/Message";
+  import type { MailAccount } from "../../../logic/Mail/Account";
+  import { Person } from "../../../logic/Abstract/Person";
+  import PersonPicture from "../../Shared/PersonPicture.svelte";
+  import Button from "../../Shared/Button.svelte";
   import { getDateString } from "../../Util/date";
+  import StarIcon from "lucide-svelte/icons/star";
+  import MoreIcon from "lucide-svelte/icons/more-horizontal";
 
-  export let message: Email;
+  export let message: EMail;
+  export let account: MailAccount;
 
   $: from = message.outgoing
-    ? message.authorEmailAddress
+    ? "me"
+      ?? account.userRealname
+      ?? message.authorEmailAddress
     : message.contact?.name
       ?? message.authorEmailAddress;
   $: to = message.outgoing
     ? message.contact?.name
       ?? message.recipientEmailAddress
-    : message.recipientEmailAddress;
+    : "me"
+      ?? account.userRealname
+      ?? message.recipientEmailAddress;
+
+  function toggleStar() {
+    message.starred = !message.starred;
+  }
 </script>
 
 <style>
   .message-header {
     min-height: 5em;
-    border-bottom: 1px solid grey;
-    padding: 8px 16px;
+    padding: 16px 20px 8px 20px;
+    background-color: #F9F9FD;
   }
-  grid.message-header {
-    grid-template-columns: auto 1fr;
+  .subject {
+    font-weight: 700;
+    margin-bottom: 8px;
   }
-  .label {
-    margin-right: 16px;
+  .from {
+    font-weight: bold;
+  }
+  .outgoing .from {
+    color: grey;
+  }
+  .to {
+    font-size: 13px;
+    color: grey;
+  }
+  .outgoing .to {
+    font-weight: bold;
+    color: inherit;
+  }
+  .buttons {
+    justify-content: end;
+    margin-bottom: 8px;
+  }
+  .buttons > * {
+    margin-left: 8px;
+  }
+  .star.starred :global(svg) {
+    fill: orange;
   }
 </style>
