5244cb299ee7d8f2e2bbe3fb20621e5d8cfb8570
Ben Bucksch <ben.bucksch@beonex.com>
2023-12-06 02:30:02 +0100

    Mail: Msg list and header: Read/Star buttons

--- svelte-gecko/frontend/Mail/MessageHeader/MessageHeader.svelte
+++ svelte-gecko/frontend/Mail/MessageHeader/MessageHeader.svelte
@@ -18,6 +18,16 @@
     <hbox flex />
     <vbox>
       <hbox class="buttons">
+        <hbox class="unread" class:read={message.read}>
+          <Button
+            icon={CircleIcon}
+            iconSize="16px"
+            iconOnly
+            label={message.read ? "Mark this message as unread" : "Mark this message as read"}
+            on:click={() => toggleRead()}
+            plain
+            />
+        </hbox>
         <hbox class="star" class:starred={message.starred}>
           <Button
             icon={StarIcon}
@@ -54,6 +64,7 @@
   import Button from "../../Shared/Button.svelte";
   import { getDateString } from "../../Util/date";
   import StarIcon from "lucide-svelte/icons/star";
+  import CircleIcon from "lucide-svelte/icons/circle";
   import MoreIcon from "lucide-svelte/icons/more-horizontal";
 
   export let message: EMail;
@@ -68,6 +79,9 @@
       ?? message.recipientEmailAddress
     : "me";
 
+  function toggleRead() {
+    message.read = !message.read;
+  }
   function toggleStar() {
     message.starred = !message.starred;
   }
@@ -97,6 +111,9 @@
     font-weight: bold;
     color: inherit;
   }
+  .date {
+    align-self: end;
+  }
   .buttons {
     justify-content: end;
     margin-bottom: 8px;
@@ -104,7 +121,13 @@
   .buttons > * {
     margin-left: 8px;
   }
+  .buttons :global(svg) {
+    stroke-width: 1.5px;
+  }
   .star.starred :global(svg) {
     fill: orange;
+  }
+  .unread:not(.read) :global(svg) {
+    fill: green;
   }
 </style>
--- svelte-gecko/frontend/Mail/MessageList/MessageList.svelte
+++ svelte-gecko/frontend/Mail/MessageList/MessageList.svelte
@@ -6,9 +6,29 @@
       <hbox>Date</hbox>
     </svelte:fragment>
     <svelte:fragment slot="row" let:item={msg}>
-      <hbox>{msg.contact.name}</hbox>
-      <hbox>{msg.subject}</hbox>
-      <hbox>{getDateString(msg.received)}</hbox>
+      <hbox class="correspondent">{msg.contact.name}</hbox>
+      <hbox class="subject" class:read={msg.read}>{msg.subject}</hbox>
+      <hbox class="date">{getDateString(msg.received)}</hbox>
+      <hbox class="unread button" class:read={msg.read}>
+        <Button
+          icon={CircleIcon}
+          iconSize="10px"
+          iconOnly
+          label={msg.read ? "Mark this message as unread" : "Mark this message as read"}
+          on:click={() => toggleRead(msg)}
+          plain
+          />
+      </hbox>
+      <hbox class="star button" class:starred={msg.starred}>
+        <Button
+          icon={StarIcon}
+          iconSize="14px"
+          iconOnly
+          label="Remember this message"
+          on:click={() => toggleStar(msg)}
+          plain
+          />
+      </hbox>
     </svelte:fragment>
   </FastList>
 </vbox>
@@ -17,10 +37,20 @@
   import type { EMail } from "../../../logic/Mail/Message";
   import type { Collection } from "svelte-collections";
   import FastList from "../../Shared/FastList.svelte";
+  import Button from "../../Shared/Button.svelte";
+  import StarIcon from "lucide-svelte/icons/star";
+  import CircleIcon from "lucide-svelte/icons/circle";
   import { getDateString } from "../../Util/date";
 
   export let messages: Collection<EMail>;
   export let selectedMessage: EMail;
+
+  function toggleRead(message: EMail) {
+    message.read = !message.read;
+  }
+  function toggleStar(message: EMail) {
+    message.starred = !message.starred;
+  }
 </script>
 
 <style>
@@ -29,5 +59,21 @@
   }
   .message-list :global(.fast-list table) {
     padding-left: 4px;
+  }
+  .subject:not(.read) {
+    font-weight: bold;
+  }
+  .button {
+    width: 20px;
+    vertical-align: middle;
+  }
+  .button :global(svg) {
+    stroke-width: 1px;
+  }
+  .star.starred :global(svg) {
+    fill: orange;
+  }
+  .unread:not(.read) :global(svg) {
+    fill: green;
   }
 </style>
