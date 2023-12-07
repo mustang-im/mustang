91b211708275487552b6cc2caa2598cb715e7bb7
Neil Rashbrook <neil.rashbrook@beonex.com>
2023-12-07 21:30:57 +0000

    Proof of concept of using resource: URLs to access the generated files

--- svelte-gecko/runner/content/app.js
+++ svelte-gecko/runner/content/app.js
@@ -1,8 +1,8 @@
 function start() {
   console.log("starting");
   let browser = document.getElementById("app-frame");
-  let triggeringPrincipal = Services.scriptSecurityManager.createNullPrincipal({});
-  browser.loadURI(Services.io.newURI("http://localhost:5454/"), { triggeringPrincipal });
+  let triggeringPrincipal = Services.scriptSecurityManager.getSystemPrincipal();
+  browser.loadURI(Services.io.newURI("resource://dist/index.html"), { triggeringPrincipal });
   console.log("started");
 }
 
