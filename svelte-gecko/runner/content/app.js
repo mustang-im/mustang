9e656491c8f78d07b45953cdd5038e0393f8d0cd
Neil Rashbrook <neil.rashbrook@beonex.com>
2023-12-07 23:24:06 +0100

    Runner: Use local static files

--- svelte-gecko/runner/content/app.js
+++ svelte-gecko/runner/content/app.js
@@ -1,8 +1,10 @@
 function start() {
   console.log("starting");
   let browser = document.getElementById("app-frame");
-  let triggeringPrincipal = Services.scriptSecurityManager.createNullPrincipal({});
-  browser.loadURI(Services.io.newURI("http://localhost:5454/"), { triggeringPrincipal });
+  //let triggeringPrincipal = Services.scriptSecurityManager.createNullPrincipal({});
+  //browser.loadURI(Services.io.newURI("http://localhost:5454/"), { triggeringPrincipal });
+  let triggeringPrincipal = Services.scriptSecurityManager.getSystemPrincipal();
+  browser.loadURI(Services.io.newURI("resource://code/index.html"), { triggeringPrincipal });
   console.log("started");
 }
 
