801d1834f55f2f73f79703169dec266f04bdd689
Ben Bucksch <ben.bucksch@beonex.com>
2023-12-07 19:03:17 +0100

    Mail: Message header: Add More menu

--- svelte-gecko/frontend/Mail/Message/MessageToolbar.svelte
+++ svelte-gecko/frontend/Mail/Message/MessageToolbar.svelte
@@ -19,6 +19,26 @@
       plain
       />
   </hbox>
+  <hbox class="trash">
+    <Button
+      icon={TrashIcon}
+      iconSize="16px"
+      iconOnly
+      label={"Delete this message"}
+      on:click={() => catchErrors(() => deleteMessage())}
+      plain
+      />
+  </hbox>
+  <hbox class="spam">
+    <Button
+      icon={SpamIcon}
+      iconSize="16px"
+      iconOnly
+      label={"Mark as spam"}
+      on:click={() => catchErrors(() => markAsSpam())}
+      plain
+      />
+  </hbox>
   <hbox class="unread" class:read={message.read}>
     <Button
       icon={CircleIcon}
@@ -40,26 +60,22 @@
       />
   </hbox>
   <hbox>
-    <Button
-      icon={MoreIcon}
-      iconSize="24px"
-      iconOnly
-      label="Additional commands for this message"
-      plain
-      />
-  </hbox>
+      <MessageMenu {message} {account} />
+    </hbox>
 </hbox>
 
 <script lang="ts">
   //import type { Email } from "mustang-lib";
   import type { EMail } from "../../../logic/Mail/Message";
   import type { MailAccount } from "../../../logic/Mail/Account";
+  import MessageMenu from "./MessageMenu.svelte";
   import Button from "../../Shared/Button.svelte";
   import StarIcon from "lucide-svelte/icons/star";
   import CircleIcon from "lucide-svelte/icons/circle";
-  import MoreIcon from "lucide-svelte/icons/more-horizontal";
   import ReplyIcon from "lucide-svelte/icons/reply";
   import ReplyAllIcon from "lucide-svelte/icons/reply-all";
+  import TrashIcon from "lucide-svelte/icons/trash-2";
+  import SpamIcon from "lucide-svelte/icons/shield-x";
   import { catchErrors } from "../../Util/error";
 
   export let message: EMail;
@@ -74,6 +90,10 @@
   function reply() {
   }
   function replyAll() {
+  }
+  function deleteMessage() {
+  }
+  function markAsSpam() {
   }
 </script>
 
