150e3fbfb4c1fe924e6564dc4d825d9183ab49a4
Ben Bucksch <ben.bucksch@beonex.com>
2023-12-07 16:53:50 +0100

    Mail: Split message toolbar out

--- svelte-gecko/frontend/Mail/Message/MessageHeader.svelte
+++ svelte-gecko/frontend/Mail/Message/MessageHeader.svelte
@@ -17,37 +17,7 @@
     </vbox>
     <hbox flex />
     <vbox>
-      <hbox class="buttons">
-        <hbox class="unread" class:read={message.read}>
-          <Button
-            icon={CircleIcon}
-            iconSize="16px"
-            iconOnly
-            label={message.read ? "Mark this message as unread" : "Mark this message as read"}
-            on:click={() => toggleRead()}
-            plain
-            />
-        </hbox>
-        <hbox class="star" class:starred={message.starred}>
-          <Button
-            icon={StarIcon}
-            iconSize="24px"
-            iconOnly
-            label="Remember this message"
-            on:click={toggleStar}
-            plain
-            />
-        </hbox>
-        <hbox>
-          <Button
-            icon={MoreIcon}
-            iconSize="24px"
-            iconOnly
-            label="Additional commands for this message"
-            plain
-            />
-        </hbox>
-      </hbox>
+      <MessageToolbar {message} {account} />
       <value class="date" title={message.received?.toLocaleString()}>
         {getDateString(message.received)}
       </value>
@@ -60,12 +30,9 @@
   import type { EMail } from "../../../logic/Mail/Message";
   import type { MailAccount } from "../../../logic/Mail/Account";
   import { Person } from "../../../logic/Abstract/Person";
+  import MessageToolbar from "./MessageToolbar.svelte";
   import PersonPicture from "../../Shared/Person/PersonPicture.svelte";
-  import Button from "../../Shared/Button.svelte";
   import { getDateString } from "../../Util/date";
-  import StarIcon from "lucide-svelte/icons/star";
-  import CircleIcon from "lucide-svelte/icons/circle";
-  import MoreIcon from "lucide-svelte/icons/more-horizontal";
 
   export let message: EMail;
   export let account: MailAccount;
@@ -78,13 +45,6 @@
     ? message.contact?.name
       ?? message.recipientEmailAddress
     : "me";
-
-  function toggleRead() {
-    message.read = !message.read;
-  }
-  function toggleStar() {
-    message.starred = !message.starred;
-  }
 </script>
 
 <style>
@@ -114,21 +74,5 @@
   }
   .date {
     align-self: end;
-  }
-  .buttons {
-    justify-content: end;
-    margin-bottom: 8px;
-  }
-  .buttons > * {
-    margin-left: 8px;
-  }
-  .buttons :global(svg) {
-    stroke-width: 1.5px;
-  }
-  .star.starred :global(svg) {
-    fill: orange;
-  }
-  .unread:not(.read) :global(svg) {
-    fill: green;
   }
 </style>
---
+++ svelte-gecko/frontend/Mail/Message/MessageToolbar.svelte
@@ -0,0 +1,70 @@
+<hbox class="buttons">
+  <hbox class="unread" class:read={message.read}>
+    <Button
+      icon={CircleIcon}
+      iconSize="16px"
+      iconOnly
+      label={message.read ? "Mark this message as unread" : "Mark this message as read"}
+      on:click={() => toggleRead()}
+      plain
+      />
+  </hbox>
+  <hbox class="star" class:starred={message.starred}>
+    <Button
+      icon={StarIcon}
+      iconSize="24px"
+      iconOnly
+      label="Remember this message"
+      on:click={toggleStar}
+      plain
+      />
+  </hbox>
+  <hbox>
+    <Button
+      icon={MoreIcon}
+      iconSize="24px"
+      iconOnly
+      label="Additional commands for this message"
+      plain
+      />
+  </hbox>
+</hbox>
+
+<script lang="ts">
+  //import type { Email } from "mustang-lib";
+  import type { EMail } from "../../../logic/Mail/Message";
+  import type { MailAccount } from "../../../logic/Mail/Account";
+  import Button from "../../Shared/Button.svelte";
+  import StarIcon from "lucide-svelte/icons/star";
+  import CircleIcon from "lucide-svelte/icons/circle";
+  import MoreIcon from "lucide-svelte/icons/more-horizontal";
+
+  export let message: EMail;
+  export let account: MailAccount;
+
+  function toggleRead() {
+    message.read = !message.read;
+  }
+  function toggleStar() {
+    message.starred = !message.starred;
+  }
+</script>
+
+<style>
+  .buttons {
+    justify-content: end;
+    margin-bottom: 8px;
+  }
+  .buttons > * {
+    margin-left: 8px;
+  }
+  .buttons :global(svg) {
+    stroke-width: 1.5px;
+  }
+  .star.starred :global(svg) {
+    fill: orange;
+  }
+  .unread:not(.read) :global(svg) {
+    fill: green;
+  }
+</style>
