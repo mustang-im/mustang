3a3c733e842529678266259a7d13e12fea0e8f93
Ben Bucksch <ben.bucksch@beonex.com>
2023-12-10 21:59:48 +0100

    Calendar: Icon: Change date at midnight

--- svelte-gecko/frontend/Calendar/CalendarIcon.svelte
+++ svelte-gecko/frontend/Calendar/CalendarIcon.svelte
@@ -5,12 +5,29 @@
 </Icon>
 
 <script lang="ts">
-import Icon from '../Shared/Icon.svelte';
-import calendar from '../asset/icon/appBar/calendar.svg?raw';
+  import Icon from '../Shared/Icon.svelte';
+  import calendar from '../asset/icon/appBar/calendar.svg?raw';
+  import { onMount } from 'svelte';
 
-export let size: string = "24px";
+  export let size: string = "24px";
 
-let day = new Date().getDate();
+  let day = new Date().getDate();
+
+  onMount(setupNextUpdate);
+  function setupNextUpdate() {
+    let now = new Date();
+    let tomorrow = new Date();
+    tomorrow.setDate(now.getDate() + 1);
+    tomorrow.setHours(0);
+    tomorrow.setMinutes(0);
+    tomorrow.setSeconds(0);
+    tomorrow.setMilliseconds(0);
+    let nextUpdate = tomorrow.getTime() - now.getTime();
+    setTimeout(() => {
+      day = new Date().getDate();
+      setupNextUpdate();
+    }, nextUpdate);
+  }
 </script>
 
 <style>
