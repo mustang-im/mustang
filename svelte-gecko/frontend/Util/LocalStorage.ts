59521b8c7dd546dd2266b590e2523c33bb3e1b40
Ben Bucksch <ben.bucksch@beonex.com>
2023-12-10 20:53:02 +0100

    LocalStorage: Remove debug logs

--- svelte-gecko/frontend/Util/LocalStorage.ts
+++ svelte-gecko/frontend/Util/LocalStorage.ts
@@ -26,11 +26,9 @@ const settings = new Map<string, ObservableLocalStorageSetting<any>>();
 export function getLocalStorage<T>(key: string, defaultValue?: T): ObservableLocalStorageSetting<T> {
   let existing = settings.get(key);
   if (existing) {
-    console.log("existing localstorage for", key, existing);
     return existing;
   }
   let setting = new ObservableLocalStorageSetting(key, defaultValue);
   settings.set(key, setting);
-  console.log("new localstorage for", key, setting);
   return setting;
 }
