90f20420d48fdbef6c96f0b54f89f9d8671bb516
Ben Bucksch <ben.bucksch@beonex.com>
2023-12-13 19:33:16 +0100

    Appbar lost the shadow due to z-index

--- svelte-gecko/frontend/MainWindow/AppBar.svelte
+++ svelte-gecko/frontend/MainWindow/AppBar.svelte
@@ -57,6 +57,7 @@
     background-color: #5C5C71;
     box-shadow: 1px 0px 3px 0px rgba(22, 12, 39, 20%); /* #160C27 */
     padding-top: 8px;
+    z-index: 4;
   }
   .app-bar :global(.app-button),
   .bottom {
