668f040efe532b5db849c1318966d3fe647d6097
Ben Bucksch <ben.bucksch@beonex.com>
2023-12-15 21:02:00 +0100

    Electron: Render the frontend

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -17,11 +17,14 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
+  // TODO HACK
+  mainWindow.loadURL("http://localhost:5454");
+  /*
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
-  }
+    mainWindow.loadFile(path.join(__dirname, `../../index.html`));
+  }*/
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
