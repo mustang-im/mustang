537db633ce040aba81af7d1440fd9e46d5469b8e
Ben Bucksch <ben.bucksch@beonex.com>
2023-12-16 09:55:18 +0100

    PersonList: Placeholder when no picture

--- app/frontend/Shared/Person/PersonPicture.svelte
+++ app/frontend/Shared/Person/PersonPicture.svelte
@@ -1,11 +1,14 @@
 {#if person?.picture}
-  <vbox class="image">
+  <vbox class="avatar image">
     <img
-    src={person.picture}
-    width={size} height={size}
-    title="Picture of {person.name}"
-    alt="" />
+      src={person.picture}
+      width={size} height={size}
+      title="Picture of {person.name}"
+      alt=""
+      />
   </vbox>
+{:else}
+  <vbox class="avatar dummy" style="width: {size}px; height: {size}px;" />
 {/if}
 
 <script lang="ts">
@@ -16,8 +19,10 @@
 </script>
 
 <style>
-  .image {
+  .avatar {
     margin: 7px 12px;
+  }
+  .image {
     clip-path: circle();
   }
 </style>
