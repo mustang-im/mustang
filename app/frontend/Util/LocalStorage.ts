392071b4a451261dcacc02f180ab7962be4bb61e
Ben Bucksch <ben.bucksch@beonex.com>
2023-12-18 03:39:34 +0100

    Observer: Decorators

--- app/frontend/Util/LocalStorage.ts
+++ app/frontend/Util/LocalStorage.ts
@@ -1,4 +1,4 @@
-import { Observable } from "./Observable";
+import { Observable } from "../../logic/util/Observable";
 
 class ObservableLocalStorageSetting<T> extends Observable<ObservableLocalStorageSetting<T>> {
   readonly key: string;
