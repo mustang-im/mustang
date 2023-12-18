3f7bd3ad4a4f39c6f4979a43426afda7ca536e40
Ben Bucksch <ben.bucksch@beonex.com>
2023-12-18 11:12:18 +0100

    Mail: Delete and J keyboard shortcuts in message list

--- app/frontend/Mail/3pane/TableMessageList.svelte
+++ app/frontend/Mail/3pane/TableMessageList.svelte
@@ -1,5 +1,8 @@
-<vbox flex class="message-list">
-  <FastList items={sortedMessages} bind:selectedItem={selectedMessage}>
+<vbox flex class="message-list"
+  on:keydown={event => catchErrors(() => onKey(event))}>
+  <FastList items={sortedMessages}
+    bind:selectedItem={selectedMessage}
+    bind:selectedItems={selectedMessages}>
     <svelte:fragment slot="header">
       <hbox></hbox>
       <hbox></hbox>
@@ -13,14 +16,26 @@
 
 <script lang="ts">
   import type { EMail } from "../../../logic/Mail/Message";
-  import type { Collection } from "svelte-collections";
+  import type { Collection, ArrayColl } from "svelte-collections";
   import FastList from "../../Shared/FastList.svelte";
   import TableMessageListItem from "./TableMessageListItem.svelte";
+  import { catchErrors } from "../../Util/error";
 
   export let messages: Collection<EMail>;
+  export let selectedMessages: ArrayColl<EMail>;
   export let selectedMessage: EMail;
 
   $: sortedMessages = messages.sortBy(email => -email.received.getTime());
+
+  async function onKey(event: KeyboardEvent) {
+    if (event.key == "Delete") {
+      await Promise.all(selectedMessages.map(msg => msg.deleteMessage()));
+      event.stopPropagation();
+    } else if (event.key == "j") {
+      await Promise.all(selectedMessages.map(msg => msg.deleteMessage()));
+      event.stopPropagation();
+    }
+  }
 </script>
 
 <style>
--- app/frontend/Mail/Vertical/VerticalMessageList.svelte
+++ app/frontend/Mail/Vertical/VerticalMessageList.svelte
@@ -1,5 +1,8 @@
-<vbox flex class="message-list">
-  <FastList items={sortedMessages} bind:selectedItem={selectedMessage}>
+<vbox flex class="message-list"
+  on:keydown={event => catchErrors(() => onKey(event))}>
+  <FastList items={sortedMessages}
+    bind:selectedItem={selectedMessage}
+    bind:selectedItems={selectedMessages}>
     <svelte:fragment slot="header">
     </svelte:fragment>
     <VerticalMessageListItem slot="row" let:item message={item} />
@@ -8,14 +11,26 @@
 
 <script lang="ts">
   import type { EMail } from "../../../logic/Mail/Message";
-  import type { Collection } from "svelte-collections";
+  import type { Collection, ArrayColl } from "svelte-collections";
   import FastList from "../../Shared/FastList.svelte";
   import VerticalMessageListItem from "./VerticalMessageListItem.svelte";
+  import { catchErrors } from "../../Util/error";
 
   export let messages: Collection<EMail>;
   export let selectedMessage: EMail;
+  export let selectedMessages: ArrayColl<EMail>;
 
   $: sortedMessages = messages.sortBy(email => -email.received.getTime());
+
+  async function onKey(event: KeyboardEvent) {
+    if (event.key == "Delete") {
+      await Promise.all(selectedMessages.map(msg => msg.deleteMessage()));
+      event.stopPropagation();
+    } else if (event.key == "j") {
+      await Promise.all(selectedMessages.map(msg => msg.deleteMessage()));
+      event.stopPropagation();
+    }
+  }
 </script>
 
 <style>
