eae1ddfa09efe72732a5ff8c97731896898a74dc
Ben Bucksch <ben.bucksch@beonex.com>
2023-12-19 11:32:51 +0100

    Rename logic.js to .ts

--- electron/src/logic.ts
+++ electron/src/logic.ts
@@ -1,13 +1,13 @@
 //import { readAccounts, addNewAccountFromConfig, findAccountConfig } from "mustang-lib";
-import { readAccounts, addNewAccountFromConfig } from "mustang-lib/logic/account/account-list";
-import { findAccountConfig } from "mustang-lib/logic/account/setup/setup";
+import { readAccounts, addNewAccountFromConfig } from "./lib/logic/account/account-list";
+import { findAccountConfig } from "./lib/logic/account/setup/setup";
 //import SQLAccount from "mustang-lib/logic/storage/SQLAccount";
-import JPCWebSocket from 'jpc-ws'
+//import JPCWebSocket from 'jpc-ws'
 
 export async function startupLogic() {
-  let appGlobal = await createSharedAppObject();
-  let jpc = new JPCWebSocket(appGlobal);
-  await jpc.listen(kSecret, 5455, false);
+  //  let appGlobal = await createSharedAppObject();
+  //  let jpc = new JPCWebSocket(appGlobal);
+  //  await jpc.listen(kSecret, 5455, false);
 }
 
 const kSecret = 'eyache5C'; // TODO generate, and communicate to client, or save in config files.
