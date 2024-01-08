81ca37a8ed87a540fd604b485d3056af5a4add6d
Ben Bucksch <ben.bucksch@beonex.com>
2024-01-08 18:39:37 +0100

    Meet: Setup: Device selection

--- app/frontend/Meet/Setup/DeviceSetup.svelte
+++ app/frontend/Meet/Setup/DeviceSetup.svelte
@@ -5,34 +5,78 @@
   </vbox>
   <hbox class="buttons">
     <RoundButton
-      label="Mute"
+      label={$micOn ? "Mute" : "Unmute"}
       classes="toggle-mic"
       on:click={() => catchErrors(toggleMic)}
       icon={$micOn ? MicrophoneIcon : MicrophoneOffIcon}
       iconSize="24px"
       />
+    <Menu>
+      <RoundButton
+        slot="control"
+        classes="select-mic"
+        label="Select microphone"
+        icon={DownIcon}
+        iconSize="16px"
+        disabled={!$micOn}
+        />
+      <Menu.Label>Your microphones</Menu.Label>
+      {#if devices}
+        {#each devices.filter(d => d.kind == "audioinput") as device (device.deviceId)}
+          <Menu.Item
+            on:click={() => catchErrors(() => selectMic(device))}
+            icon={MicrophoneIcon}>
+            {device.label}
+          </Menu.Item>
+        {/each}
+      {/if}
+    </Menu>
     <RoundButton
-      label="Camera"
+      label={$cameraOn ? "Turn camera off" : "Turn camera on"}
       classes="toggle-camera"
       on:click={() => catchErrors(toggleCamera)}
       icon={$cameraOn ? CameraIcon : CameraOffIcon}
       iconSize="24px"
       />
+    <Menu>
+      <RoundButton
+        slot="control"
+        classes="select-camera"
+        label="Select camera"
+        icon={DownIcon}
+        iconSize="16px"
+        disabled={!$cameraOn}
+        />
+      <Menu.Label>Your cameras</Menu.Label>
+      {#if devices}
+        {#each devices.filter(d => d.kind == "videoinput") as device (device.deviceId)}
+          <Menu.Item
+            on:click={() => catchErrors(() => selectCamera(device))}
+            icon={CameraIcon}>
+            {device.label}
+          </Menu.Item>
+        {/each}
+      {/if}
+    </Menu>
   </hbox>
 </vbox>
 
 <script lang="ts">
-  import { cameraOn, micOn } from "./devices";
+  import { cameraOn, micOn, selectedCamera } from "./devices";
   import RoundButton from "../../Shared/RoundButton.svelte";
+  import { Menu } from "@svelteuidev/core";
   import CameraIcon from "lucide-svelte/icons/video";
   import CameraOffIcon from "lucide-svelte/icons/video-off";
   import MicrophoneIcon from "lucide-svelte/icons/mic";
   import MicrophoneOffIcon from "lucide-svelte/icons/mic-off";
+  import DownIcon from "lucide-svelte/icons/chevron-down";
   import { catchErrors } from "../../Util/error";
-  import { onMount } from "svelte";
+  import { assert } from "../../../logic/util/util";
+  import { onMount, tick } from "svelte";
 
   let cameraStream: MediaStream;
   let videoEl: HTMLVideoElement;
+  let devices: MediaDeviceInfo[];
 
   async function toggleCamera() {
     $cameraOn = !$cameraOn;
@@ -59,6 +103,10 @@
     }
     videoEl.srcObject = cameraStream;
     await videoEl.play();
+
+    if (!devices) {
+      await getDevices();
+    }
   }
 
   async function stopCamMic() {
@@ -71,6 +119,22 @@
     videoEl.pause();
     videoEl.srcObject = null;
     cameraStream = null;
+  }
+
+  async function getDevices() {
+    await tick();
+    let allDevices = await navigator.mediaDevices.enumerateDevices();
+    devices = allDevices.filter(d => !d.label.startsWith("Monitor of"));
+  }
+
+  function selectCamera(device: MediaDeviceInfo) {
+    assert(device?.kind == "videoinput", "Need camera");
+    $selectedCamera = device.deviceId;
+  }
+
+  function selectMic(device: MediaDeviceInfo) {
+    assert(device?.kind == "audioinput", "Need microphone");
+    $selectedCamera = device.deviceId;
   }
 
   onMount(async () => catchErrors(startCamMic));
@@ -93,5 +157,11 @@
   }
   .buttons :global(> *) {
     margin-left: 4px;
+  }
+  .buttons :global(.select-camera),
+  .buttons :global(.select-mic) {
+    margin-left: -14px;
+    margin-top: 20px;
+    padding: 2px;
   }
 </style>
