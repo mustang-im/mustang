03cdfb2f2b64f76eb660d7f9703d3ed72a440df0
Ben Bucksch <ben.bucksch@beonex.com>
2024-01-23 02:43:05 +0100

    Mail: Allow click on recipient -> Contacts

--- app/frontend/Mail/Message/Recipient.svelte
+++ app/frontend/Mail/Message/Recipient.svelte
@@ -1,12 +1,23 @@
-<value title={recipient.emailAddresses.first.value}>
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<value title={recipient.emailAddresses.first.value} on:click={selectPerson}>
   {recipient.name}
 </value>
 
 <script lang="ts">
   import type { Person } from "../../../logic/Abstract/Person";
+  import { selectedPerson } from "../../Shared/Person/PersonOrGroup";
+  import { AppArea, selectedApp } from "../../MainWindow/app";
 
   export let recipient: Person;
+
+  function selectPerson() {
+    $selectedPerson = recipient;
+    $selectedApp = AppArea.Contacts;
+  }
 </script>
 
 <style>
+  value {
+    cursor: pointer;
+  }
 </style>
