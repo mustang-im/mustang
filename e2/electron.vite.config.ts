4ed49dffa39038f77bd0a43d46be5247a355d747
Ben Bucksch <ben.bucksch@beonex.com>
2024-01-24 00:56:26 +0100

    e2: Local files from installation

--- e2/electron.vite.config.ts
+++ e2/electron.vite.config.ts
@@ -1,9 +1,24 @@
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [
+      externalizeDepsPlugin(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: '../lib/logic/storage/mail-sql/',
+            dest: '.'
+          },
+          {
+            src: '../lib/locale/',
+            dest: '.'
+          }
+        ]
+      })
+    ]
   },
   preload: {
     plugins: [externalizeDepsPlugin()]
