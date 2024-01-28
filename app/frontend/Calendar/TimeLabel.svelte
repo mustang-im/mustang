ff1c54612b8badb700198bcedc6dabea65b4d700
Ben Bucksch <ben.bucksch@beonex.com>
2024-01-28 18:58:47 +0100

    Calendar: Day: Hours

--- app/frontend/Calendar/TimeLabel.svelte
+++ app/frontend/Calendar/TimeLabel.svelte
@@ -1,5 +1,5 @@
 <hbox class="time">
-  {time.toLocaleTimeString(undefined, { hour: "numeric", minute: "numeric" })}
+  {time.toLocaleTimeString(undefined, { hour: "numeric" })}
 </hbox>
 
 <script lang="ts">
@@ -11,5 +11,6 @@
     margin: -0.8em 8px 0 0;
     color: #AAABB4;
     font-weight: 300;
+    justify-content: end;
   }
 </style>
