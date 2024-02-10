831644bdc49188c62032a4595c92489130d2c0c4
Ben Bucksch <ben.bucksch@beonex.com>
2024-02-10 04:51:55 +0100

    Person pills - part 1

---
+++ app/frontend/Calendar/EditEvent/PersonAvailability.svelte
@@ -0,0 +1,24 @@
+<hbox class="availability" class:yes class:no={!yes}>
+  {yes ? "Available" : "Not available"}
+</hbox>
+
+<script lang="ts">
+  import type { Person } from "../../../logic/Abstract/Person";
+
+  export let person: Person;
+
+  $: yes = person?.firstName.length <= 6;
+</script>
+
+<style>
+  .availability {
+    font-size: 12px;
+    margin-top: -1px;
+  }
+  .availability.yes {
+    color: green;
+  }
+  .availability.no {
+    color: red;
+  }
+</style>
---
+++ app/frontend/Shared/PersonAutocomplete/PersonAutocompleteResult.svelte
@@ -0,0 +1,32 @@
+<hbox flex class="person">
+  <PersonPicture person={person} size={32} />
+  <vbox flex class="right">
+    <hbox class="top-row">
+      <hbox flex class="name">{person.name}</hbox>
+    </hbox>
+    <hbox class="bottom-row">
+      <slot name="bottom-row" />
+  </vbox>
+</hbox>
+
+<script lang="ts">
+  import type { Person } from "../../../logic/Abstract/Person";
+  import PersonPicture from "../Person/PersonPicture.svelte";
+
+  export let person: Person;
+</script>
+
+<style>
+  .person,
+  .name {
+    align-items: center;
+  }
+  .person :global(.avatar) {
+    margin: 0px;
+    margin-right: 6px;
+  }
+  .top-row,
+  .bottom-row {
+    height: 16px;
+  }
+</style>
