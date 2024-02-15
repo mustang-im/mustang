cd19b02d58de6589b602942e68759afa1ab66b09
Ben Bucksch <ben.bucksch@beonex.com>
2024-02-15 06:47:19 +0100

    Mail/Chat: HTML converter stub functions

--- app/logic/Abstract/Message.ts
+++ app/logic/Abstract/Message.ts
@@ -1,5 +1,6 @@
 import type { Contact } from "./Contact";
 import type { Person } from "./Person";
+import { convertHTMLToText, convertTextToHTML, sanitizeHTML } from "../util/convertHTML";
 import { Observable, notifyChangedProperty } from "../util/Observable";
 import { MapColl } from "svelte-collections";
 
@@ -27,10 +28,35 @@ export class Message extends Observable {
 
   /** Plaintext version of the message */
   @notifyChangedProperty
-  text: string;
+  _text: string;
+  get text(): string {
+    if (this._text) {
+      return this._text;
+    }
+    if (this._html) {
+      return convertHTMLToText(this._html);
+    }
+    return null;
+  }
+  set text(val: string) {
+    this._text = val;
+  }
+
   /** HTML version of the message */
   @notifyChangedProperty
-  html: string;
+  _html: string;
+  get html(): string {
+    if (this._html) {
+      return this._html;
+    }
+    if (this._text) {
+      return convertTextToHTML(this._text);
+    }
+    return null;
+  }
+  set html(val: string) {
+    this._html = val;
+  }
 
   readonly reactions = new MapColl<Person, string>();
 
