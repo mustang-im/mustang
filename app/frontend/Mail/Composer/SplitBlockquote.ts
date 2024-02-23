efbd17c582fd28f8a3a12a73339b7a2f68fc61e0
Ben Bucksch <1907525+benbucksch@users.noreply.github.com>
2024-02-23 01:15:23 +0100

    Add comment describing what the extension does

--- app/frontend/Mail/Composer/SplitBlockquote.ts
+++ app/frontend/Mail/Composer/SplitBlockquote.ts
@@ -1,5 +1,7 @@
 import Blockquote from '@tiptap/extension-blockquote';
 
+/** On Enter key, the blockquote is split into 2 blockquotes, at the place where the cursor was.
+ * New text appears in the middle of the 2 blockquotes. It is not part of any blockquote. */
 export const SplitBlockquote = Blockquote.extend({
   name: 'split-blockquote',
   addKeyboardShortcuts() {
