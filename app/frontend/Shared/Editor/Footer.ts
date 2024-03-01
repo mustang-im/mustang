0178a57dab9c8ab651902b343f7cca9ae63d1a52
Jeremy Chang <jermybz01@gmail.com>
2024-03-01 10:07:23 -0600

    add footerOptions

--- app/frontend/Shared/Editor/Footer.ts
+++ app/frontend/Shared/Editor/Footer.ts
@@ -1,20 +1,30 @@
 import { mergeAttributes, Node } from "@tiptap/core";
 
-/* `<footer>` Node */
-export const Footer = Node.create({
+export interface FooterOptions {
+  HTMLAttributes: Record<string, any>,
+}
+
+/** `<footer>` Node */
+export const Footer = Node.create<FooterOptions>({
   name: 'footer',
+
   group: 'block',
-  content: 'block*',
+
+  content: 'block+',
+
   addOptions() {
     return {
       HTMLAttributes: {},
     }
   },
+
   parseHTML() {
     return [
-      { tag: 'footer',}
+      { tag: 'footer', }
     ]
   },
+
+
   renderHTML({HTMLAttributes}) {
     return ['footer', mergeAttributes(this.options.HTMLAttributes, HTMLAttributes), 0]
   },
