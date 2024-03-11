a14ce27bdfa767431fe9a01ec1c3bed5c6083b9c
Ben Bucksch <ben.bucksch@beonex.com>
2024-03-11 21:45:06 +0100

    Search field: Factor out into component

---
+++ app/frontend/Shared/SearchField.svelte
@@ -0,0 +1,45 @@
+<hbox class="search">
+  <SearchIcon size="16px" />
+  <input type="search" bind:value={searchInput} placeholder="Search for a person or group" />
+  {#if searchInput}
+    <RoundButton icon={XIcon} iconSize="16px" padding="4px" border={false}
+      on:click={() => searchInput = undefined} />
+  {/if}
+</hbox>
+
+<script lang="ts">
+  import RoundButton from "./RoundButton.svelte";
+  import SearchIcon from "lucide-svelte/icons/search";
+  import XIcon from "lucide-svelte/icons/x";
+
+  /** out only */
+  export let searchTerm: string;
+
+  let searchInput: string;
+  $: searchTerm = searchInput?.toLowerCase();
+</script>
+
+<style>
+  .search {
+    margin: 0px 12px 8px 12px;
+    border: 1px solid #A1E4DA;
+    padding-left: 8px;
+    padding-right: 4px;
+    border-radius: 100px;
+    background-color: white;
+    align-items: center;
+  }
+  .search :global(svg) {
+    color: #808080;
+  }
+  input[type="search"] {
+    width: 100%;
+    height: 32px;
+    border: none;
+    margin-left: 4px;
+    border-radius: 100px;
+  }
+  input::placeholder {
+    color: #808080;
+  }
+</style>
