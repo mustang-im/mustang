3380c4fed0ea1493380d2649e8fb94dab809886d
Jeremy Chang <71667021+jermy-c@users.noreply.github.com>
2024-03-12 22:26:21 -0600

    SplitBlockquote: node is null (#46)
    ### Changes
    
    1. Change `setNodeSelection()` to `setTextSelection()`
    2. Add validation of node position in `SplitFirstParent()`
    3. Add validation if `SplitFirstParent()` can be dispatched
    4. Declare interface for `SplitBlockquote`
    5. Scroll in to view after splitting
    
    ### Reproduction of Bug
    
    1. Press enter in non-blockquote area i.e. create paragraphs
    2. Select the empty paragraphs created
    3. Press toggle blockquote button
    6. Place cursor in the blockquote with empty paragraphs
    7. Press enter
    
    ### Notes
    
    1. The bug was probably cause by the blockquote being empty
    2. `tr` or transaction is more up-to-date than `state` so you when press enter it fixes the unresponsiveness on the first press because it has the outdated state
    
    ### Commits
    
    * Declare extension interface
    * Possibly fix node is null in splitBlockquote()
    * Check for null in splitFirstParent()
    * Remove null checks
    * Change state to tr
    * Scroll into view after splitting
    * Revert "Change state to tr"
    This reverts commit cf09b60c351f40b7108ceed7bb8f766abefb1e79.
    * Revert "Revert "Change state to tr""
    This reverts commit 6e4dcdfa0bf9f9256cc3f28f98fa4616f7525426.
    * Check if splitFirstParent can be dispatched and if position is valid

--- app/frontend/Shared/Editor/SplitBlockquote.ts
+++ app/frontend/Shared/Editor/SplitBlockquote.ts
@@ -1,5 +1,20 @@
 import Blockquote from '@tiptap/extension-blockquote';
 
+declare module '@tiptap/core' {
+  interface Commands<ReturnType> {
+    splitBlockquote: {
+      /**
+       * Split a blockquote node
+       */
+      splitBlockquote: () => ReturnType,
+      /**
+       * Split first parent node at the given position
+       */
+      splitFirstParent: (pos: number) => ReturnType,
+    }
+  }
+}
+
 /** On Enter key, the blockquote is split into 2 blockquotes, at the place where the cursor was.
  * New text appears in the middle of the 2 blockquotes. It is not part of any blockquote. */
 export const SplitBlockquote = Blockquote.extend({
@@ -14,28 +29,31 @@ export const SplitBlockquote = Blockquote.extend({
   },
   addCommands() {
     return {
-      splitBlockquote: () => ({ chain, state }) => {
-        let {$from, $to} = state.selection;
+      splitBlockquote: () => ({ chain, tr }) => {
+        let {$from, $to} = tr.selection;
         // if not in blockquote
         if ($from.node(1).type.name !== 'blockquote') {
           return false;
         }
         // if at start of blockquote
         if ($from.before(1) + $from.depth === $from.pos) {
-          return chain().insertContentAt($from.before(1), '<p></p>').run();
+          return chain().insertContentAt($from.before(1), '<p></p>').scrollIntoView().run();
         }
         // if at end of blockquote
         if ($to.after(1) - $to.depth === $to.pos) {
-          return chain().insertContentAt($to.after(1), '<p></p>').run();
+          return chain().insertContentAt($to.after(1), '<p></p>').scrollIntoView().run();
         }
         // default split in middle of blockquote
-        return chain().setNodeSelection($to.pos).splitFirstParent($to.pos)
-        .insertContentAt($to.pos + $to.depth, '<p></p>').run();
+        return chain().setTextSelection($to.pos).splitFirstParent($to.pos)
+        .insertContentAt($to.pos + $to.depth, '<p></p>').scrollIntoView().run();
       },
-      splitFirstParent: (pos: number) => ({ tr }) => {
+      splitFirstParent: (pos: number) => ({ tr, dispatch }) => {
         let node = this.editor.$pos(pos);
-        tr.split(node.pos, node.depth);
-        return true;
+        if (dispatch && node) {
+          tr.split(node.pos, node.depth);
+          return true;
+        }
+        return false;
       }
     }
   },
