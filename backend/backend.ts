02b570c10ae677366d0576fddb552497ad441115
Ben Bucksch <ben.bucksch@beonex.com>
2024-03-14 04:50:01 +0100

    File location for DB

--- backend/backend.ts
+++ backend/backend.ts
@@ -1,6 +1,9 @@
 import JPCWebSocket from '../lib/jpc-ws';
 import { ImapFlow } from 'imapflow';
 import { Database } from "@radically-straightforward/sqlite";
+import path from "node:path";
+import os from "node:os";
+import fs from "node:fs";
 
 export async function startupBackend() {
   let appGlobal = await createSharedAppObject();
@@ -22,5 +25,18 @@ function createIMAPFlowConnection(...args): ImapFlow {
 }
 
 function getSQLiteDatabase(filename: string): Database {
-  return new Database(filename);
+  return new Database(path.join(getConfigDir(), filename));
+}
+
+const kAppConfigDir = "Mustang";
+/**
+ * Get the user config directory on disk.
+ * Linux: /home/USER/.mustang/
+ * Windows: C:\Users\USER\AppData\Mustang\
+ */
+function getConfigDir(): string {
+  let dirname = os.platform() == "win32" || os.platform() == "darwin" ? kAppConfigDir : "." + kAppConfigDir.toLowerCase();
+  let dir = path.join(os.homedir(), dirname);
+  fs.mkdirSync(dir, { recursive: true });
+  return dir;
 }
