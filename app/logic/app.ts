03f72ce61646ce3d6717bd67663f57e4adb8c757
Ben Bucksch <ben.bucksch@beonex.com>
2024-03-16 01:24:08 +0100

    Logic: Separate appGlobal and startup

--- app/logic/app.ts
+++ app/logic/app.ts
@@ -5,14 +5,9 @@ import type { VideoConfMeeting } from './Meet/VideoConfMeeting';
 import type { Calendar } from './Calendar/Calendar';
 import type { Directory } from './Files/File';
 import Apps from './Apps/Apps';
-import { readMailAccounts } from './Mail/MailAccounts';
-import { readChatAccounts } from './Chat/ChatAccounts';
-import { getTestObjects } from './testData';
-import { testDatabase } from './Mail/SQL/sql';
 import { ArrayColl } from 'svelte-collections';
-import JPCWebSocket from '../../lib/jpc-ws';
 
-export class AppGlobal {
+class AppGlobal {
   readonly emailAccounts = new ArrayColl<MailAccount>();
   readonly chatAccounts = new ArrayColl<ChatAccount>();
   readonly calendars = new ArrayColl<Calendar>();
@@ -23,45 +18,5 @@ export class AppGlobal {
   remoteApp: any;
   me: Person;
 }
+
 export let appGlobal = new AppGlobal();
-
-const kSecret = 'eyache5C'; // TODO generate, and communicate to client, or save in config files.
-
-export async function getStartObjects(): Promise<void> {
-  let jpc = new JPCWebSocket(null);
-  await jpc.connect(kSecret, "localhost", 5455);
-  console.log("connected to server");
-  appGlobal.remoteApp = await jpc.getRemoteStartObject();
-  appGlobal.emailAccounts.addAll(await readMailAccounts());
-  appGlobal.chatAccounts.addAll(await readChatAccounts());
-  await testDatabase();
-  if (appGlobal.emailAccounts.isEmpty && appGlobal.chatAccounts.isEmpty) {
-    await getTestObjects(appGlobal);
-  }
-}
-
-/**
- * Logs in to all accounts for which we have the credentials stored.
- *
- * @param errorCallback Called for login errors.
- *   May be called multiple times, e.g. once per account.
- */
-export async function loginOnStartup(errorCallback: (ex) => void): Promise<void> {
-  for (let account of appGlobal.chatAccounts) {
-    try {
-      await account.login(false);
-    } catch (e) {
-      errorCallback(e);
-    }
-  }
-
-  for (let account of appGlobal.emailAccounts) {
-    //if (!(await account.isLoggedIn) && (await account.haveStoredLogin())) {
-    try {
-      console.log("Logging in mail account", account.name);
-      await account.login(false);
-    } catch (e) {
-      errorCallback(e);
-    }
-  }
-}
---
+++ app/logic/startup.ts
@@ -0,0 +1,47 @@
+import { appGlobal } from './app';
+import { readMailAccounts } from './Mail/MailAccounts';
+import { readChatAccounts } from './Chat/ChatAccounts';
+import { getTestObjects } from './testData';
+// import { testDatabase } from './Mail/SQL/test';
+import JPCWebSocket from '../../lib/jpc-ws';
+
+const kSecret = 'eyache5C'; // TODO generate, and communicate to client, or save in config files.
+
+export async function getStartObjects(): Promise<void> {
+  let jpc = new JPCWebSocket(null);
+  await jpc.connect(kSecret, "localhost", 5455);
+  console.log("connected to server");
+  appGlobal.remoteApp = await jpc.getRemoteStartObject();
+  appGlobal.emailAccounts.addAll(await readMailAccounts());
+  appGlobal.chatAccounts.addAll(await readChatAccounts());
+  // await testDatabase();
+  if (appGlobal.emailAccounts.isEmpty && appGlobal.chatAccounts.isEmpty) {
+    await getTestObjects();
+  }
+}
+
+/**
+ * Logs in to all accounts for which we have the credentials stored.
+ *
+ * @param errorCallback Called for login errors.
+ *   May be called multiple times, e.g. once per account.
+ */
+export async function loginOnStartup(errorCallback: (ex) => void): Promise<void> {
+  for (let account of appGlobal.chatAccounts) {
+    try {
+      await account.login(false);
+    } catch (e) {
+      errorCallback(e);
+    }
+  }
+
+  for (let account of appGlobal.emailAccounts) {
+    //if (!(await account.isLoggedIn) && (await account.haveStoredLogin())) {
+    try {
+      console.log("Logging in mail account", account.name);
+      await account.login(false);
+    } catch (e) {
+      errorCallback(e);
+    }
+  }
+}
