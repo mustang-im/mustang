67ecda1c2f08f8ffa1ec897035d6674ff919c270
Ben Bucksch <ben.bucksch@beonex.com>
2024-03-18 19:35:54 +0100

    SQL: Properly re-init test database

--- app/logic/Mail/SQL/SQLDatabase.ts
+++ app/logic/Mail/SQL/SQLDatabase.ts
@@ -15,7 +15,6 @@ export async function getDatabase(): Promise<Database> {
   }
   const getDatabase = appGlobal.remoteApp.getSQLiteDatabase;
   mailDatabase = await getDatabase("mail.db");
-  let dbVersion = (await mailDatabase.pragma('user_version') as any)[0].user_version;
   await mailDatabase.migrate(mailDatabaseSchema);
   await mailDatabase.pragma('foreign_keys = true');
   await mailDatabase.pragma('journal_mode = DELETE');
@@ -30,14 +29,17 @@ export async function getDatabase(): Promise<Database> {
 export async function makeTestDatabase(): Promise<Database> {
   const getDatabase = appGlobal.remoteApp.getSQLiteDatabase;
   mailDatabase = await getDatabase("test.db");
+  await deleteDatabase();
+  await mailDatabase.migrate(mailDatabaseSchema);
+  await mailDatabase.pragma('foreign_keys = true');
+  return mailDatabase;
+}
+
+async function deleteDatabase(): Promise<void> {
   let tables = await mailDatabase.all(sql`SELECT name FROM sqlite_schema WHERE type='table'`) as any[];
   for (let row of tables) {
     let table = row.name;
-    console.log("deleting table", table);
-    await mailDatabase.run(sql`DROP TABLE ${table}`);
+    await mailDatabase.execute(sql`DROP TABLE IF EXISTS ${table};`);
   }
-  await mailDatabase.migrate(mailDatabaseSchema);
-  await mailDatabase.pragma('foreign_keys = true');
-  await mailDatabase.pragma('journal_mode = DELETE');
-  return mailDatabase;
+  await mailDatabase.pragma('user_version = 0');
 }
