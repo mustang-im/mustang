a3afa776f41066d8edaca84ce6ae9f7774a6d134
Jeremy Chang <jermybz01@gmail.com>
2024-03-19 14:38:15 -0600

    Functional whitelist extension

--- app/frontend/Shared/Editor/Whitelist.ts
+++ app/frontend/Shared/Editor/Whitelist.ts
@@ -1,4 +1,4 @@
-import { Extension, type Extensions, getSchema, Node, type GlobalAttributes, type Attribute } from '@tiptap/core';
+import { Extension, type Extensions, getSchema, Node, type GlobalAttributes, type Attribute, mergeAttributes } from '@tiptap/core';
 import type { Schema } from '@tiptap/pm/model';
 
 export interface WhitelistOptions {
@@ -13,8 +13,9 @@ export interface allowedAttributes {
   [key: string]: string[],
 }
 
-export interface WhitelistStorage {
-  extensions: string[],
+// Common options interface Arbitrary Elements
+export interface TagOptions {
+  HTMLAttributes: Record<string, any>,
 }
 
 /**
@@ -22,17 +23,12 @@ export interface WhitelistStorage {
  * 1. It needs a list of extensions for adding attributes to all tags and prevention of adding tags that already exist
  * 2. We cannot the list of extension before the editor is initialized
  * 3. Tiptap seems to want devs to enable and disable manually e.g. History and Collaboration extension conflict
+ * 4. We cannot add another tag type to the Schema after initialization
  */
 
 /** Extension which allows arbitrary HTML elements and attributes */
-export const Whitelist = Extension.create<WhitelistOptions, WhitelistStorage>({
+export const Whitelist = Extension.create<WhitelistOptions>({
   name: 'whitelist',
-  priority: 50, // Load later
-  addStorage() {
-    return {
-      extensions: [],
-    }
-  },
   addOptions() {
     return {
       editorExtensions: [],
@@ -58,96 +54,113 @@ export const Whitelist = Extension.create<WhitelistOptions, WhitelistStorage>({
   addExtensions() {
     let tags: Extensions = [];
     let schema: Schema = getSchema(this.options.editorExtensions);
-    let editorTags: string[] = getTags(schema);
-
-    /**
-     * TODO
-     * 1. Allow HTML Attributes option for each node i.e. add merge attribute in parseHTML
-     */
+    let editorTags: string[] = getTags(schema); // Gets list of existing tags in editor
 
     // Create a Node Type for each tag
-    this.options.allowedTags.forEach(tag => {
+    for (const tag in this.options.allowedTags) {
       if (!editorTags.includes(tag)) {
-        const node: Node = Node.create({
-          name: tag,
-          content: 'block+',
-          group: 'block',
-          parseHTML() {
-            return [
-              {
-                tag: tag,
-              }
-            ]
-          },
-          renderHTML() {
-            return [
-              tag,
-              0
-            ]
-          }
-        });
-        tags.push(node);
+        continue;
       }
-    });
+
+      const node: Node = Node.create<TagOptions>({
+        name: tag,
+        priority: 50, // Load after all defaults
+        content: 'block+',
+        group: 'block',
+        addOptions() {
+          return {
+            HTMLAttributes: {},
+          }
+        },
+        parseHTML() {
+          return [
+            {
+              tag: tag,
+            }
+          ]
+        },
+        renderHTML({HTMLAttributes}) {
+          return [
+            tag,
+            mergeAttributes(this.options.HTMLAttributes, HTMLAttributes),
+            0
+          ]
+        }
+      });
+
+      tags.push(node);
+    }
 
     return tags;
   },
   addGlobalAttributes() {
     let allowedAttributes = this.options.allowedAttributes;
     let attributesList: GlobalAttributes = [];
+    let schema: Schema = getSchema(this.options.editorExtensions);
+    let tagExtension: Object = getTagExtension(schema);
+    let extensionAttribute: Object = getExtensionAttribute(schema);
 
+    this.options.allowedTags.forEach(tag => {
+      if (!tagExtension.hasOwnProperty(tag)) {
+        tagExtension[tag] = tag;
+      }
+      if (!extensionAttribute.hasOwnProperty(tag)) {
+        extensionAttribute[tag] = null;
+      }
+    });
     /**
      * TODO
      * 1. Check and validate tag is the schema
      * 2. Search for the tag in the schema
+     * 3. Check if tag already has the attribute
      */
 
+    /**
+     * 1. List of tag: extension
+     * 2. List of extension: attribute
+     */
     // Create attribute for each attribute
     for (const tag in allowedAttributes) {
+      if (!tagExtension.hasOwnProperty(tag) && tag !== '*') {
+        continue;
+      }
+
       let tags: string[] =  [tag];
+      if (tag === '*') {
+        tags = [];
+        for (const tag in tagExtension) {
+          let extension = tagExtension[tag];
+          if (!tags.includes(extension)) {
+            tags.push(extension);
+          }
+        }
+      }
+
+      if (tagExtension.hasOwnProperty(tag)) {
+        tags = [tagExtension[tag]];
+      }
 
       // Create a attribute for each attribute
       let attributes = {};
       allowedAttributes[tag].forEach(a => {
-        attributes[a] = {
-          default: null,
-        } as Attribute;
+        if (tag === '*' || extensionAttribute[tags[0]] && !extensionAttribute[tags[0]].includes(a)) {
+          attributes[a] = {
+            default: null,
+          } as Attribute;
+        }
       });
 
-      // Get all node/mark types to add the attributes to
-      if (tag === '*') {
-        tags = [];
-        let schema: Schema = getSchema(this.options.editorExtensions);
-        
-        // Get add to all node types
-        let nodes: string[] = [];
-        for (const node in schema.nodes) {
-          if (schema.nodes[node].name !== 'text' && schema.nodes[node].name !== 'doc') {
-            nodes.push(schema.nodes[node].name);
-          }
-        }
-        tags.push(...nodes);
-
-        // Get all mark types
-        let marks: string[] = [];
-        for (const mark in schema.marks) {
-          marks.push(schema.marks[mark].name);
-        }
-        tags.push(...marks);
-
-        // Get all tags already in the editor
-        let editorTags: string[] = this.storage.extensions;
-        this.options.allowedTags.forEach(tag => {
-          if (!editorTags.includes(tag)) {
-            tags.push(tag);
-          }
-        });
+      if (Object.keys(attributes).length === 0) {
+        continue;
       }
 
       let newAttribute = {
         types: [ ...tags ],
         attributes: attributes,
-      };      
+      }; 
+
+      console.log(newAttribute);
+
       attributesList.push(newAttribute);
     };
     return attributesList;
@@ -155,18 +168,20 @@ export const Whitelist = Extension.create<WhitelistOptions, WhitelistStorage>({
 });
 
 // Exclude the [src=*] in img[src=*]
-const regexp: RegExp = /\w+/;
+const tagOnly: RegExp = /\w+/;
 
 /** Gets Tags from schema */
 function getTags(schema: Schema): string[] {
   let tags: string[] = [];
 
   // Get all node types
-  for (const node in schema.nodes) {
-    if (schema.nodes[node].spec.parseDOM) {
-      schema.nodes[node].spec.parseDOM.forEach(rule => {
+  let nodes = schema.nodes;
+  for (const node in nodes) {
+    let parseDOM = nodes[node].spec.parseDOM;
+    if (parseDOM) {
+      parseDOM.forEach(rule => {
         if (rule.tag) {
-          let tag = rule.tag.match(regexp)[0];
+          let tag = rule.tag.match(tagOnly)[0];
           tags.push(tag);  
         }
       });
@@ -174,15 +189,88 @@ function getTags(schema: Schema): string[] {
   }
 
   // Get all mark types
-  for (const mark in schema.marks) {
-    if (schema.marks[mark].spec.parseDOM) {
-      schema.marks[mark].spec.parseDOM.forEach(rule => {
+  let marks = schema.marks;
+  for (const mark in marks) {
+    let parseDOM = marks[mark].spec.parseDOM;
+    if (parseDOM) {
+      parseDOM.forEach(rule => {
         if (rule.tag) {
-          let tag = rule.tag.match(regexp)[0];
+          let tag = rule.tag.match(tagOnly)[0];
           tags.push(tag);  
         }
       });
     }
   }
   return tags;
+}
+
+/** Get a map of `[tag]:[extension]` */
+function getTagExtension(schema: Schema): Object {
+  let tagExtension: Object = {};
+
+  // Get all node types
+  let nodes = schema.nodes;
+  for (const node in nodes) {
+    let parseDOM = nodes[node].spec.parseDOM;
+    if (parseDOM) {
+      parseDOM.forEach(rule => {
+        if (rule.tag) {
+          let tag = rule.tag.match(tagOnly)[0];
+          let nodeName = nodes[node].name;
+          tagExtension[tag] = nodeName; 
+        }
+      });
+    }
+  }
+
+  // Get all mark types
+  let marks = schema.marks;
+  for (const mark in marks) {
+    let parseDOM = marks[mark].spec.parseDOM;
+    if (parseDOM) {
+      parseDOM.forEach(rule => {
+        if (rule.tag) {
+          let tag = rule.tag.match(tagOnly)[0];
+          let markName = marks[mark].name;
+          tagExtension[tag] = markName;
+        }
+      });
+    }
+  }
+  return tagExtension;
+}
+
+/** Get a map of `[extension]:[...attributes]` */
+function getExtensionAttribute(schema: Schema): Object {
+  let extensionAttribute: Object = {};
+    // Get all attributes in Node
+    let nodes = schema.nodes;
+    for (const node in nodes) {
+      let attrs = nodes[node].spec.attrs;
+      if (!attrs) {
+        continue;
+      }
+      let attributesList: string[] = [];
+      for (const attribute in attrs) {
+        attributesList.push(attribute);
+      }
+      let nodeName = nodes[node].name;
+      extensionAttribute[nodeName] = attributesList;
+    }
+  
+    // Get attributes in Mark
+    let marks = schema.marks;
+    for (const mark in marks) {
+      let attrs = marks[mark].spec.attrs;
+      if (!attrs) {
+        continue;
+      }
+      let attributesList: string[] = [];
+      for (const attribute in attrs) {
+        attributesList.push(attribute);
+      }
+      let markName = marks[mark].name;
+      extensionAttribute[markName] = attributesList;
+    }
+    return extensionAttribute;
 }
