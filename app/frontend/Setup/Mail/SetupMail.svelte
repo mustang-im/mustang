17c1b8f1f96367dc0687adf559513277dc386177
Ben Bucksch <ben.bucksch@beonex.com>
2024-03-20 00:03:28 +0100

    Setup: Enter key, Found, Spinner size

--- app/frontend/Setup/Mail/SetupMail.svelte
+++ app/frontend/Setup/Mail/SetupMail.svelte
@@ -2,13 +2,23 @@
   <hbox flex />
   <vbox class="page-box">
     <Header title="Set up existing email address" subtitle="You can use Mustang with your existing email address or you can make a new email address." />
-    <MailAddress bind:emailAddress />
-    <Password bind:password />
+    <MailAddress bind:emailAddress bind:this={mailAddressEl} on:continue={doFocusPassword} />
+    <Password bind:password bind:this={passwordEl} on:continue={onFindConfig} />
     {#if step == 2}
-      <hbox flex class="checking">
+      <hbox flex class="checking middle">
         <Spinner size="24px" />
         <hbox flex class="message">
           We are looking for the configuration of your email account...
+        </hbox>
+      </hbox>
+    {:else if step == 3}
+      <hbox flex class="results middle">
+        <hbox flex class="message">
+          <CheckIcon />
+          <vbox>
+            <hbox>Congratulations!</hbox>
+            <hbox>We found the configuration in our ISP database.</hbox>
+          </vbox>
         </hbox>
       </hbox>
     {/if}
@@ -36,22 +46,39 @@
   import Button from "../../Shared/Button.svelte";
   import BackgroundVideo from "./BackgroundVideo.svelte";
   import Spinner from "./Spinner.svelte";
+  import CheckIcon from "lucide-svelte/icons/check";
+  import { sleep } from "../../../logic/util/util";
+  import { onMount } from "svelte";
 
   let emailAddress: string;
   let password: string;
 
   $: canContinue = step == 1 && emailAddress && password;
-  let step = 2;
+  let step = 1;
 
-  function onFindConfig() {
+  async function onFindConfig() {
     step = 2;
-  }
-  function onManualSetup() {
+    await sleep(3);
     step = 3;
   }
-  function onNewEmailAddress() {
-    step = 4;
+  function onManualSetup() {
+    step = 30;
   }
+  function onNewEmailAddress() {
+    step = 40;
+  }
+
+  let passwordEl: Password;
+  let mailAddressEl: MailAddress;
+
+  function doFocusPassword() {
+    passwordEl.focus();
+  }
+
+  onMount(() => {
+    mailAddressEl.focus();
+    onFindConfig();
+  });
 </script>
 
 <style>
@@ -64,20 +91,34 @@
     padding: 24px 48px 20px 48px;
     background-color: white;
   }
-  .checking {
-    margin-top: 32px;
+  .middle {
+    margin-top: 24px;
     margin-bottom: 20px;
   }
+  .message {
+    margin-left: 8px;
+    margin-right: 24px;
+    padding: 4px 24px;
+    border-radius: 16px;
+  }
   .checking .message {
+    margin-left: 16px;
     background-color: #F0F9F8;
     color: #455468;
-    margin-left: 12px;
-    padding: 4px 16px;
-    border-radius: 100px;
   }
-  .warning {
+  .warning .message {
     background-color: #FFFAEC;
     color: #FFC83A;
+  }
+  .results .message {
+    padding-left: 16px;
+    background-color: #E7F9EC;
+    color: #0BC241;
+    border: 1px solid #0BC241;
+    justify-content: start;
+  }
+  .results .message :global(svg) {
+    margin-right: 6px;
   }
   .buttons {
     align-items: end;
