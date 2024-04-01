9badb7afe30398161078bfe8aacf5d052cc8ccb2
Ben Bucksch <ben.bucksch@beonex.com>
2024-04-02 01:28:54 +0200

    Setup: Mail: AutoConfig: Use PriorityAbortable

--- app/logic/Mail/AutoConfig/findConfig.ts
+++ app/logic/Mail/AutoConfig/findConfig.ts
@@ -2,7 +2,7 @@ import type { MailAccount } from "../MailAccount";
 import { fetchConfig, getDomainForEmailAddress } from "./fetchConfig";
 import type { ArrayColl } from "svelte-collections";
 
-export async function findConfig(emailAddress: string, password: string): Promise<ArrayColl<MailAccount>> {
+export async function findConfig(emailAddress: string, password: string, abort: AbortController): Promise<ArrayColl<MailAccount>> {
   let domain = getDomainForEmailAddress(emailAddress);
-  return await fetchConfig(domain, emailAddress);
+  return await fetchConfig(domain, emailAddress, abort);
 }
