89ee2b481212c42a37fffaf05109fc8fcecf43bb
Ben Bucksch <ben.bucksch@beonex.com>
2024-04-09 00:20:27 +0200

    Settings: About

---
+++ app/frontend/Settings/About/About.svelte
@@ -0,0 +1,17 @@
+<Scroll>
+  <vbox class="app-logo">
+    <Icon data={logo} size="20px" />
+  </vbox>
+  <h1>Mustang</h1>
+
+  <div>Copyright 2024 Ben Bucksch, Mustang GmbH and other contributors</div>
+</Scroll>
+
+<script lang="ts">
+  import Scroll from "../../Shared/Scroll.svelte";
+  import Icon from 'svelte-icon/Icon.svelte';
+  import logo from '../../asset/icon/general/logo.svg?raw';
+</script>
+
+<style>
+</style>
