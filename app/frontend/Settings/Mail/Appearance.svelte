7875051b422286de978c30282227ba63552db9ca
Ben Bucksch <ben.bucksch@beonex.com>
2024-04-09 07:24:57 +0200

    Settings: Mail: Appearance dummy

---
+++ app/frontend/Settings/Mail/Appearance.svelte
@@ -0,0 +1,14 @@
+<vbox class="view-switcher">
+  <ViewSwitcher />
+</vbox>
+
+<script lang="ts">
+  import ViewSwitcher from "../../Mail/LeftPane/ViewSwitcher.svelte";
+</script>
+
+<style>
+  .view-switcher :global(svg) {
+    width: 128px;
+    height: 128px;
+  }
+</style>
