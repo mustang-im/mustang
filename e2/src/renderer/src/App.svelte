3a7cb8692058502107e55703ebd812701a712d23
Ben Bucksch <ben.bucksch@beonex.com>
2024-04-11 00:35:20 +0200

    Installer: Let renderer/index.html load from app/ - App loads in dist build 🎊🎉🎊

--- e2/src/renderer/src/App.svelte
+++
@@ -1,5 +0,0 @@
-<MainWindow />
-
-<script lang="ts">
-  import MainWindow from "../../../../app/frontend/MainWindow/MainWindow.svelte";
-</script>
--- e2/src/renderer/src/main.ts
+++
@@ -1,8 +0,0 @@
-import './assets/style.css'
-import App from './App.svelte'
-
-const app = new App({
-  target: document.getElementById('app')
-})
-
-export default app
