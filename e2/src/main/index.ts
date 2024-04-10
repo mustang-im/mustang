ab5df6bc06a14040933b700f360dc0d27c6481ea
Ben Bucksch <ben.bucksch@beonex.com>
2024-04-10 23:13:24 +0200

    e2: Remove unneeded async func

--- e2/src/main/index.ts
+++ e2/src/main/index.ts
@@ -4,7 +4,7 @@ import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 
-async function createWindow(): Promise<void> {
+function createWindow(): void {
   try {
     startupBackend();
 
