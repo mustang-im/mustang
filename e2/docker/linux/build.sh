4edc97c2e9ec17540cf6a21a334ad0338e36feba
Ben Bucksch <ben.bucksch@beonex.com>
2024-04-11 04:42:43 +0200

    Installer: Docker build to create Windows installers and Linux packages

---
+++ e2/docker/linux/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /mustang/
+(cd app/; yarn)
+(cd backend/; yarn)
+(cd lib/jpc-ws/; yarn)
+(cd e2/; yarn)
+cp app/node_modules/olm/olm.wasm app/public/olm.wasm
+
+cd /mustang/e2/
+yarn run build:linux
