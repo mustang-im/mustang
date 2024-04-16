916697c891e98750fb083831518235a893b51a1d
Ben Bucksch <ben.bucksch@beonex.com>
2024-04-16 02:38:21 +0200

    Settings: Mail: Folders

---
+++ app/frontend/Settings/Mail/Account/Folders.svelte
@@ -0,0 +1,43 @@
+<vbox flex>
+  <h2>Folder</h2>
+  <hbox class="subtitle">Rename folders, set special folders, share them with others</hbox>
+
+  {#if $mailAccount?.isLoggedIn && $mailAccount.rootFolders.hasItems}
+    <Splitter name="settings-mail-folders" initialRightRatio={4}>
+      <FolderList folders={mailAccount.rootFolders} bind:selectedFolder={folder} slot="left" />
+      <vbox class="right" slot="right">
+        {#if folder}
+          <AccountFolderGeneral {folder} />
+        {/if}
+      </vbox>
+    </Splitter>
+  {:else if !$mailAccount?.isLoggedIn}
+    Please log in to account {mailAccount?.name} first
+    <!--<Button label="Login" on:click={() => catchErrors(() => mailAccount.login(true))} />-->
+  {/if}
+</vbox>
+
+
+<script lang="ts">
+  import type { Account } from "../../../../logic/Abstract/Account";
+  import type { Folder } from "../../../../logic/Mail/Folder";
+  import type { MailAccount } from "../../../../logic/Mail/MailAccount";
+  import AccountFolderGeneral from "./FolderGeneral.svelte";
+  import FolderList from "../../../Mail/LeftPane/FolderList.svelte";
+  import Splitter from "../../../Shared/Splitter.svelte";
+
+  export let folder: Folder = null;
+  export let account: Account = folder?.account;
+
+  $: mailAccount = account as MailAccount;
+</script>
+
+<style>
+  h2 {
+    margin-top: 0px;
+    margin-bottom: 0px;
+  }
+  .right {
+    margin: 12px 32px;
+  }
+</style>
