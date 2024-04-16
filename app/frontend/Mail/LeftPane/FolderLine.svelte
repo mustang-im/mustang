e5fd80324c096b7dc55c985b69d1d12814b1dec7
Ben Bucksch <ben.bucksch@beonex.com>
2024-04-16 02:17:45 +0200

    Mail: Watch folder name changes

---
+++ app/frontend/Mail/LeftPane/FolderLine.svelte
@@ -0,0 +1,30 @@
+<hbox class="folder" on:drop={(event) => catchErrors(() => onDropMail(event, folder))} on:dragover={(event) => catchErrors(() => onDragOverMail(event, folder))}>
+  <hbox class="icon">
+    <FolderIcon {folder} size="14px" />
+  </hbox>
+  <hbox class="label">{$folder.name}</hbox>
+</hbox>
+
+<script lang="ts">
+  import type { Folder } from '../../../logic/Mail/Folder';
+  import { onDropMail, onDragOverMail } from '../Message/drag';
+  import FolderIcon from './FolderIcon.svelte';
+  import { catchErrors } from '../../Util/error';
+
+  export let folder: Folder;
+</script>
+
+<style>
+  .folder {
+    align-items: center;
+    padding-left: 12px;
+    padding-top: 2px;
+    padding-bottom: 2px;
+  }
+  .icon :global(.cls-2) {
+    stroke: black;
+  }
+  .label {
+    padding-left: 8px;
+  }
+</style>
--- app/frontend/Mail/LeftPane/FolderList.svelte
+++ app/frontend/Mail/LeftPane/FolderList.svelte
@@ -3,24 +3,15 @@
     <svelte:fragment slot="header">
       <hbox class="header">Folders</hbox>
     </svelte:fragment>
-    <svelte:fragment slot="row" let:item={folder}>
-      <hbox class="folder" on:drop={(event) => catchErrors(() => onDropMail(event, folder))} on:dragover={(event) => catchErrors(() => onDragOverMail(event, folder))}>
-        <hbox class="icon">
-          <FolderIcon {folder} size="14px" />
-        </hbox>
-        <hbox class="label">{folder.name}</hbox>
-      </hbox>
-    </svelte:fragment>
+    <FolderLine {folder} let:item={folder} slot="row"/>
   </FastList>
 </vbox>
 
 <script lang="ts">
   import type { Folder } from '../../../logic/Mail/Folder';
-  import { onDropMail, onDragOverMail } from '../Message/drag';
-  import FolderIcon from './FolderIcon.svelte';
   import FastList from "../../Shared/FastList.svelte";
-  import { catchErrors } from '../../Util/error';
   import type { Collection } from 'svelte-collections';
+  import FolderLine from './FolderLine.svelte';
 
   export let folders: Collection<Folder>;
   export let selectedFolder: Folder; /* in/out */
@@ -36,17 +27,5 @@
     padding-left: 10px !important;
     color: grey;
     font-size: 12px;
-  }
-  .folder {
-    align-items: center;
-    padding-left: 12px;
-    padding-top: 2px;
-    padding-bottom: 2px;
-  }
-  .icon :global(.cls-2) {
-    stroke: black;
-  }
-  .label {
-    padding-left: 8px;
   }
 </style>
