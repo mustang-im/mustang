d922385da86fa1e6117382d2c74c9bb9e9f08154
Ben Bucksch <ben.bucksch@beonex.com>
2024-04-28 10:10:43 +0200

    HTML sanitizer: Use DOMPurity instead of insane

--- app/logic/util/convertHTML.ts
+++ app/logic/util/convertHTML.ts
@@ -1,4 +1,4 @@
-import insane from "insane";
+import DOMPurify from "dompurify"; // https://github.com/cure53/DOMPurify
 import TurndownService from "turndown";
 import markdownit from "markdown-it";
 
@@ -19,34 +19,5 @@ export function sanitizeHTML(html: string): string {
   if (!html) {
     return null;
   }
-  //html = html.replaceAll(">", ">\n");
-  if (html.startsWith("<!DOCTYPE")) {
-    html = html.substring(html.indexOf(">") + 1);
-  }
-  //console.log("html before", html);
-  let result = insane(html, {
-    "allowedAttributes": {
-      "a": ["href", "name", "target"],
-      "iframe": ["allowfullscreen", "frameborder", "src"],
-      "img": ["src", "alt", "height", "width"],
-      "blockquote": ["cite"],
-      "td": ["width", "valign", "align"],
-      "font": ["size", "color"],
-      "*": ["class", "name", "id", "title", "style", "border"],
-    },
-    "allowedClasses": {},
-    "allowedSchemes": ["cid", "http", "https", "mailto"],
-    "allowedTags": [
-      "html", "head", "title", "body",  "style",
-      "a", "article", "b", "blockquote", "br", "caption", "code", "del", "details", "div", "em",
-      "h1", "h2", "h3", "h4", "h5", "h6", "hr", "i", "img", "ins", "kbd", "li", "main", "ol",
-      "p", "pre", "section", "span", "strike", "strong", "sub", "summary", "sup", "table",
-      "tbody", "td", "th", "thead", "tr", "u", "ul",
-      "font",
-    ],
-    "filter": null,
-    "transformText": null,
-  });
-  //console.log("insane", result);
-  return result;
+  return DOMPurify.sanitize(html, { USE_PROFILES: { html: true } });
 }
