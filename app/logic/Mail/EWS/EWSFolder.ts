8ee6f0293e2fdaa21a21e5e20604570e770a4aa6
Neil Rashbrook <neil.rashbrook@beonex.com>
2024-05-03 22:42:17 +0100

    Improve code for listing folders

--- app/logic/Mail/EWS/EWSFolder.ts
+++ app/logic/Mail/EWS/EWSFolder.ts
@@ -1,9 +1,36 @@
-import { Folder } from "../Folder";
+import { Folder, SpecialFolder } from "../Folder";
 import { EWSEMail } from "./EWSEMail";
+import type { EWSAccount } from "./EWSAccount";
 
 export class EWSFolder extends Folder {
+  account: EWSAccount;
 
   newEMail(): EWSEMail {
     return new EWSEMail(this);
+  }
+
+  fromXML(xmljs: any) {
+    this.id = xmljs.FolderId.Id;
+    this.name = xmljs.DisplayName;
+    this.countTotal = Number(xmljs.TotalCount);
+    this.countUnread = Number(xmljs.UnreadCount);
+    switch (xmljs.DistinguishedFolderId) {
+    case "inbox":
+      this.specialFolder = SpecialFolder.Inbox;
+      break;
+    case "drafts":
+      this.specialFolder = SpecialFolder.Drafts;
+      break;
+    case "sentitems":
+      this.specialFolder = SpecialFolder.Sent;
+      break;
+    case "junkemail":
+      this.specialFolder = SpecialFolder.Spam;
+      break;
+    case "deleteditems":
+      this.specialFolder = SpecialFolder.Trash;
+      break;
+    //case "outbox":
+    }
   }
 }
