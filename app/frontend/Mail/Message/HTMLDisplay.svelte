16233fdbadf6520632b6ac7610ff06dd7c6922ab
Ben Bucksch <ben.bucksch@beonex.com>
2024-05-13 20:18:24 +0200

    HTMLDisplay: Import font css directly, not using @import

--- app/frontend/Mail/Message/HTMLDisplay.svelte
+++ app/frontend/Mail/Message/HTMLDisplay.svelte
@@ -3,6 +3,7 @@
 <script lang="ts">
   import cssContent from "./content.css?inline";
   import cssBody from "./content-body.css?inline";
+  import cssFont from "../../asset/font/Karla.css?inline";
   import WebView from "../../Shared/WebView.svelte";
 
   /** DANGER Attention: You must sanitize the HTML. It comes from untrusted sources.
@@ -10,5 +11,5 @@
   export let html: string;
   export let allowExternalImages = false;
 
-  $: headHTML = `<style>${cssBody}\n${cssContent}</style>`;
+  $: headHTML = `<style>\n${cssBody}\n${cssContent}\n${cssFont}\n</style>`;
 </script>
