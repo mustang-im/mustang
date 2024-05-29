1832ae41928c5464866af00b312f3626dd0442ee
Ben Bucksch <ben.bucksch@beonex.com>
2024-05-30 00:04:15 +0200

    Mail: Write: Use common function

--- app/frontend/Mail/LeftPane/WriteButton.svelte
+++ app/frontend/Mail/LeftPane/WriteButton.svelte
@@ -7,7 +7,6 @@
 
 <script lang="ts">
   import type { MailAccount } from "../../../logic/Mail/MailAccount";
-  import { SpecialFolder } from "../../../logic/Mail/Folder";
   import { mailMustangApp } from "../MailMustangApp";
   import RoundButton from "../../Shared/RoundButton.svelte";
   import WriteIcon from "lucide-svelte/icons/pencil";
@@ -15,11 +14,7 @@
   export let selectedAccount: MailAccount; /* in/out */
 
   function newMail() {
-    let folder = selectedAccount.getSpecialFolder(SpecialFolder.Drafts);
-    let mail = folder.newEMail();
-    mail.needToLoadBody = false;
-    mail.from.emailAddress = selectedAccount.emailAddress;
-    mail.from.name = selectedAccount.userRealname;
-    mailMustangApp.writeMail(mail);
+    let email = selectedAccount.newEMailFrom();
+    mailMustangApp.writeMail(email);
   }
 </script>
