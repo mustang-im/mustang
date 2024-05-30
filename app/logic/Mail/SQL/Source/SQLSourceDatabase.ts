2c083b14f2c9402f2f0da1016d0c26f637e5d98c
Ben Bucksch <ben.bucksch@beonex.com>
2024-05-30 02:00:54 +0200

    SQL: Ensure dir

--- app/logic/Mail/SQL/Source/SQLSourceDatabase.ts
+++ app/logic/Mail/SQL/Source/SQLSourceDatabase.ts
@@ -8,6 +8,8 @@ export async function getDatabase(): Promise<Database> {
   if (mailSourceDatabase) {
     return mailSourceDatabase;
   }
+  let dir = await appGlobal.remoteApp.getFilesDir() + "/backup";
+  await appGlobal.remoteApp.fs.mkdir(dir, { recursive: true, mode: 0o700 });
   const getDatabase = appGlobal.remoteApp.getSQLiteDatabase;
   mailSourceDatabase = await getDatabase("backup/mail-backup.db");
   await mailSourceDatabase.migrate(mailSourceDatabaseSchema);
