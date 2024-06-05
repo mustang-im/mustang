c0fa7863c0eba4666b46f7f278585d20a4cb3cd9
Ben Bucksch <ben.bucksch@beonex.com>
2024-06-05 04:14:51 +0200

    Implement AutoDiscover V1 XML and V2 JSON - dry-coded

--- app/logic/Mail/AutoConfig/exchangeConfig.ts
+++ app/logic/Mail/AutoConfig/exchangeConfig.ts
@@ -1,25 +1,152 @@
-import type { MailAccount } from "../MailAccount";
-import { readConfigFromXML } from "./readConfig";
+import { AuthMethod, MailAccount, TLSSocketType, type ConfigSource } from "../MailAccount";
+import { newAccountForProtocol } from "../AccountsList/MailAccounts";
+import { kStandardPorts } from "./configInfo";
+import { OAuth2URLs } from "../../Auth/OAuth2URLs";
 import { appGlobal } from "../../app";
-import { PriorityAbortable, makeAbortable } from "../../util/Abortable";
-import { sanitize } from "../../../../lib/util/sanitizeDatatypes";
 import { getBaseDomainFromHost } from "../../util/netUtil";
+import { sanitize } from "../../../../lib/util/sanitizeDatatypes";
+import JXON from "../../../../lib/util/JXON";
+import { PriorityAbortable, makeAbortable } from "../../util/Abortable";
 import { assert, type URLString } from "../../util/util";
-import type { ArrayColl } from "svelte-collections";
+import { ArrayColl } from "svelte-collections";
 
 /** Implements the Exchange AutoDiscover V1 (XML-SOAP-based) protocol */
-export async function exchangeAutoDiscoverV1XML(domain: string, emailAddress: string, abort: AbortController): Promise<ArrayColl<MailAccount>> {
+export async function exchangeAutoDiscoverV1XML(domain: string, emailAddress: string, username: string | null, password: string, confirmCallback: () => boolean, abort: AbortController): Promise<ArrayColl<MailAccount>> {
   domain = sanitize.hostname(domain);
 
+  // <https://technet.microsoft.com/en-us/library/bb124251(v=exchg.160).aspx#Autodiscover%20services%20in%20Outlook>
+  // <https://docs.microsoft.com/en-us/previous-versions/office/developer/exchange-server-interoperability-guidance/hh352638(v%3Dexchg.140)>, search for "The Autodiscover service uses one of these four methods"
+  const url1 = `https://autodiscover.${domain}/autodiscover/autodiscover.xml`;
+  const url2 = `https://${domain}/autodiscover/autodiscover.xml`;
+  const url3 = `http://autodiscover.${domain}/autodiscover/autodiscover.xml`;
+  const body = `<?xml version="1.0" encoding="utf-8"?>
+    <Autodiscover xmlns="http://schemas.microsoft.com/exchange/autodiscover/outlook/requestschema/2006">
+      <Request>
+        <EMailAddress>${emailAddress}</EMailAddress>
+        <AcceptableResponseSchema>http://schemas.microsoft.com/exchange/autodiscover/outlook/responseschema/2006a</AcceptableResponseSchema>
+      </Request>
+    </Autodiscover>`;
+
+  const callArgs = {
+    body: body,
+    headers: {
+      // outlook.com needs this exact string, with space and lower case "utf".
+      // Compare bug 1454325 comment 15.
+      "Content-Type": "text/xml; charset=utf-8",
+    },
+    username: username ?? emailAddress,
+    password: password,
+    allowAuthPrompt: false,
+  };
+
   let priorityOrder = new PriorityAbortable(abort, [
-    fetchConfigFromISP(domain, emailAddress, abort),
-    fetchConfigFromISPDB(domain, abort),
-    fetchConfigForMX(domain, abort),
+    fetchV1(url1, callArgs, abort),
+    fetchV1(url2, callArgs, abort),
+    fetchV1HTTP(url3, callArgs, abort),
   ]);
   try {
     return await priorityOrder.run();
   } catch (ex) {
     throw new Error(`Could not find a config for ${emailAddress}`);
+  }
+}
+
+async function fetchV1(url: URLString, callArgs: any, abort: AbortController): Promise<ArrayColl<MailAccount>> {
+  let xmlStr = await fetchXML(url, callArgs, abort);
+  return readAutoDiscoverV1XML(xmlStr);
+}
+
+async function fetchV1HTTP(url: URLString, callArgs: any, abort: AbortController): Promise<ArrayColl<MailAccount>> {
+  // url3 is HTTP (not HTTPS), so suppress password. Even MS spec demands so.
+  const call3Args: any = {};
+  Object.assign(call3Args, callArgs);
+  delete call3Args.username;
+  delete call3Args.password;
+
+  // TODO catch redirect, and if HTTPS *and* a trusted domain, include password
+
+  return await fetchV1(url, call3Args, abort);
+}
+
+function readAutoDiscoverV1XML(xmlStr: string): ArrayColl<MailAccount> {
+  let jxon = JXON.parse(xmlStr);
+  assert(typeof (jxon) == "object", "Did not receive XML");
+  let protocolsXML = jxon?.Autodiscover?.Response?.Account?.Protocol;
+  console.log("Got AutoDiscover XML", protocolsXML, "full", jxon);
+  assert(protocolsXML, "Did not receive valid AutoDiscover XML");
+  let accounts = new ArrayColl<MailAccount>();
+  for (let protocolXML of protocolsXML) {
+    try {
+      let protocol = sanitize.enum(protocolXML.Type,
+        ["WEB", "EXHTTP", "EXCH", "EXPR", "POP3", "IMAP", "SMTP"], null);
+      if (protocol == "WEB") {
+        let urlXML = protocolXML.External ?? protocolXML.Internal;
+        assert(urlXML, "AutoDiscover: WEB: Nothing found");
+        let owaURL = sanitize.url(urlXML.OWAUrl?.value, null);
+        let ewsURL = sanitize.url(urlXML.Protocol?.ASUrl, null);
+        if (owaURL && !accounts.find(acc => acc.url == owaURL && acc.protocol == "owa")) {
+          accounts.add(newURLAccount(owaURL, "owa", "autodiscover-xml"));
+        }
+        if (ewsURL && !accounts.find(acc => acc.url == ewsURL && acc.protocol == "ews")) {
+          accounts.add(newURLAccount(ewsURL, "ews", "autodiscover-xml"));
+        }
+      } else if (protocol == "EXHTTP" || protocol == "EXCH") {
+        let ewsURL = sanitize.url(protocolXML.EwsUrl ?? protocolXML.ASUrl, null);
+        if (ewsURL && !accounts.find(acc => acc.url == ewsURL && acc.protocol == "ews")) {
+          accounts.add(newURLAccount(ewsURL, "ews", "autodiscover-xml"));
+        }
+      } else if (protocol == "IMAP" || protocol == "POP3") {
+        let acc = newAccountForProtocol(protocol.toLowerCase());
+        acc.hostname = sanitize.hostname(protocolXML.Server);
+        acc.port = sanitize.portTCP(protocolXML.Port);
+        let ssl = typeof (protocolXML.SSL) == "string" && protocolXML.SSL.toLowerCase() == "on" || // "On" or "Off"
+          typeof (protocolXML.Encryption) == "string" && protocolXML.Encryption.toLowerCase() != "none";  // "None", "SSL", "TLS", "Auto"
+        // SSL is too unspecific. Do they mean STARTTLS or normal TLS?
+        let tlsOptions = kStandardPorts.filter(p => p.protocol == acc.protocol && p.port == acc.port);
+        acc.tls = ssl
+          ? tlsOptions.find(p => p.tls != TLSSocketType.Plain)?.tls ?? TLSSocketType.TLS
+          : tlsOptions.find(p => p.tls != TLSSocketType.STARTTLS)?.tls ?? TLSSocketType.Plain;
+        acc.authMethod = AuthMethod.Password;
+        acc.username = sanitize.string(protocolXML.LoginName, null);
+        accounts.add(acc);
+      } else {
+        // Skip unknown
+      }
+    } catch (ex) {
+      console.error(ex); // Do not report to user nor error server
+    }
+  }
+  // emailAddress, username and password will be filled in by `fillConfig()` later
+  assert(accounts.hasItems, "No applicable config found");
+  return accounts;
+}
+
+function setSTMPServer(accounts: ArrayColl<MailAccount>) {
+  let smtpAccounts = accounts.filter(acc => acc.protocol == "smtp");
+  let imapPOPAccounts = accounts.filter(acc => acc.protocol == "imap" || acc.protocol == "pop3");
+  if (imapPOPAccounts.hasItems && smtpAccounts.isEmpty) {
+    let imap = imapPOPAccounts.first;
+    let smtp = newAccountForProtocol("smtp");
+    smtp.hostname = imap.hostname;
+    smtp.tls = imap.tls;
+    smtp.port = kStandardPorts.find(p => p.protocol == "smtp" && p.tls == smtp.tls)?.port;
+    smtp.authMethod = imap.authMethod;
+    smtp.source = imap.source;
+  }
+  if (imapPOPAccounts.hasItems) {
+    let smtp = smtpAccounts.first;
+    if (!smtp) {
+      let imap = imapPOPAccounts.first;
+      smtp = newAccountForProtocol("smtp");
+      smtp.hostname = imap.hostname;
+      smtp.tls = imap.tls;
+      smtp.port = kStandardPorts.find(p => p.protocol == "smtp" && p.tls == smtp.tls)?.port;
+      smtp.authMethod = imap.authMethod;
+      smtp.source = imap.source;
+    }
+    for (let acc of imapPOPAccounts) {
+      acc.outgoing = smtp;
+    }
   }
 }
 
@@ -27,11 +154,17 @@ export async function exchangeAutoDiscoverV1XML(domain: string, emailAddress: st
 export async function exchangeAutoDiscoverV2JSON(domain: string, emailAddress: string, abort: AbortController): Promise<ArrayColl<MailAccount>> {
   domain = sanitize.hostname(domain);
 
-  let priorityOrder = new PriorityAbortable(abort, [
-    fetchConfigFromISP(domain, emailAddress, abort),
-    fetchConfigFromISPDB(domain, abort),
-    fetchConfigForMX(domain, abort),
-  ]);
+  // <https://learn.microsoft.com/en-us/exchange/architecture/client-access/autodiscover?view=exchserver-2019>
+  // <https://www.msxfaq.de/exchange/autodiscover/autodiscover_v2.htm>
+  const url1 = `https://autodiscover.${domain}/autodiscover/autodiscover.json/v1.0/${emailAddress}?Protocol=`;
+  const url2 = `https://autodiscover.${domain}/AutoDiscover/AutoDiscover.json/v1.0/${emailAddress}?Protocol=`;
+  const url3 = `https://${domain}/AutoDiscover/AutoDiscover.json/v1.0/${emailAddress}?Protocol=`;
+  // each URL with each protocol: 3 URLs x 2 protocols = 6 URLs
+  let urls = [url1, url2, url3];
+
+  let priorityOrder = new PriorityAbortable(abort, urls.map(url =>
+    fetchV2AllProtocols(url, abort)
+  ));
   try {
     return await priorityOrder.run();
   } catch (ex) {
@@ -39,86 +172,93 @@ export async function exchangeAutoDiscoverV2JSON(domain: string, emailAddress: s
   }
 }
 
-const kISPDBURL = "https://v1.ispdb.net/";
-const kMXService = "https://mx.thunderbird.net/dns/mx/";
-
-/**
- * Tries to get a configuration for this ISP from our central database.
- */
-async function fetchConfigFromISPDB(domain: string, abort: AbortController) {
-  let xmlStr = await fetchText(kISPDBURL + domain, abort);
-  return readConfigFromXML(xmlStr, domain, "ispdb");
+async function fetchV2AllProtocols(urlPrefix: URLString, abort: AbortController): Promise<ArrayColl<MailAccount>> {
+  let protocols = ["ews", "activesync"];
+  let results = await Promise.allSettled<MailAccount>(protocols.map(protocol =>
+    fetchV2SingleProtocol(urlPrefix, protocol, abort)
+  ));
+  return new ArrayColl(results.filter(r => r.status == "fulfilled").map(r => (r as any).value));
 }
 
-async function fetchConfigFromISP(domain: string, emailAddress: string | null, abort: AbortController) {
-  let url1 = `https://autoconfig.${domain}/mail/config-v1.1.xml`;
-  if (emailAddress) {
-    url1 += `?emailaddress=${emailAddress}`;
+/** @param protocol Like `Account.protocol` */
+async function fetchV2SingleProtocol(urlPrefix: URLString, protocol: string, abort: AbortController): Promise<MailAccount> {
+  let exchangeProtocol = sanitize.translate(protocol, {
+    "ews": "Ews",
+    "activesync": "ActiveSync",
+  });
+  let json = await fetchJSON(urlPrefix + exchangeProtocol, abort);
+  return readAutoDiscoverV2JSON(json, protocol);
+}
+
+/** @param protocol Like `Account.protocol` */
+function readAutoDiscoverV2JSON(json: any, protocol: string): MailAccount {
+  if (json.ErrorMessage) {
+    throw new Error(json.ErrorMessage);
   }
-  let url2 = `https://${domain}/.well-known/autoconfig/mail/config-v1.1.xml`;
-  let url3 = `http://autoconfig.${domain}/mail/config-v1.1.xml`; // TODO HTTP needed, given MX?
-
-  let priorityOrder = new PriorityAbortable(abort, [
-    fetchText(url1, abort),
-    fetchText(url2, abort),
-    fetchText(url3, abort),
-  ]);
-  let xmlStr = await priorityOrder.run();
-  return readConfigFromXML(xmlStr, domain, "autoconfig-isp");
+  let url = json?.Url;
+  assert(url, "No URL found");
+  return newURLAccount(url, protocol, "autodiscover-json");
 }
 
-async function fetchConfigForMX(domain, abort: AbortController): Promise<ArrayColl<MailAccount>> {
-  let mx = await getMX(domain, abort);
-  let mxDomain = getBaseDomainFromHost(mx);
-  let priorityOrder = new PriorityAbortable(abort, [
-    fetchConfigFromISP(mxDomain, null, abort), // without emailAddress
-    fetchConfigFromISPDB(mxDomain, abort),
-  ]);
-  return await priorityOrder.run();
-}
-
-/**
- * Queries the DNS MX for the domain
- *
- * @param domain {String}
- * @returns hostname of the first MX server
- */
-async function getMX_node(domain: string, abort: AbortController): Promise<string> {
-  let results = []; // await dns.resolveMx(domain);
-  let result = results[0];
-  assert(result, `No MX found for domain ${domain}`);
-  return sanitize.hostname(result.exchange);
-}
-
-/**
- * Queries the DNS MX for the domain
- *
- * TODO Use DoH?
- * TODO Cache result
- * TODO Query both web service and local DNS, and
- * use the result only when they match.
- *
- * @param domain {String}
- * @returns hostname of the first MX server
- */
-export async function getMX(domain: string, abort: AbortController): Promise<string> {
-  let mxStr = await fetchText(kMXService + domain, abort);
-  let mxs = mxStr.split("\n"); // last line might be empty
-  let mx = mxs[0];
-  assert(mx, `No MX found for domain ${domain}`);
-  return sanitize.hostname(mx);
+/** @param protocol Like `Account.protocol` */
+function newURLAccount(url: URLString, protocol: string, source: ConfigSource): MailAccount {
+  assert(url.startsWith("https://"), "URL must be https:");
+  let acc = newAccountForProtocol(protocol);
+  acc.url = url;
+  acc.hostname = new URL(acc.url).hostname;
+  acc.port = 443;
+  acc.tls = TLSSocketType.TLS;
+  acc.authMethod = OAuth2URLs.some(oauth => oauth.domains.includes(acc.hostname))
+    ? AuthMethod.OAuth2
+    : AuthMethod.Password;
+  // emailAddress, username and password will be filled in by `fillConfig()` later
+  acc.source = source;
+  return acc;
 }
 
 let ky;
 
-async function fetchText(url: URLString, abort: AbortController) {
+/**
+ * @return {JSON}
+ */
+async function fetchJSON(url: URLString, abort: AbortController): Promise<any> {
   if (!ky) {
     ky = await appGlobal.remoteApp.kyCreate();
   }
   try {
-    let text = await makeAbortable(ky.get(url, { result: "text", retry: 0 }), abort);
-    assert(text && typeof (text) == "string", "Did not receive text");
-    return text;
+    let params = {
+      result: "json",
+      "Content-Type": "text/json; charset=uft8",
+      retry: 0,
+    };
+    let json = await makeAbortable(ky.get(url, params), abort);
+    assert(json && typeof (json) == "object", "Did not receive JSON");
+    return json;
+  } catch (ex) {
+    throw new Error(`Failed to fetch ${url}: ${ex.message}`);
+  }
+}
+
+/**
+ * @return XML as string
+ */
+async function fetchXML(url: URLString, params: any, abort: AbortController): Promise<string> {
+  if (!ky) {
+    ky = await appGlobal.remoteApp.kyCreate();
+  }
+  try {
+    Object.assign(params, { result: "text", retry: 0 });
+    if (params.username && params.password) {
+      if (!params.header) {
+        params.header = {};
+      }
+      let str = params.username + ":" + params.password;
+      let utf8 = String.fromCharCode(...new TextEncoder().encode(str));
+      params.header.Authorization = "Basic " + btoa(utf8);
+    }
+    let xmlStr = await makeAbortable(ky.post(url, params), abort);
+    assert(xmlStr && typeof (xmlStr) == "string", "Did not receive text");
+    return xmlStr;
   } catch (ex) {
     throw new Error(`Failed to fetch ${url}: ${ex.message}`);
   }
--- app/logic/Mail/AutoConfig/findConfig.ts
+++ app/logic/Mail/AutoConfig/findConfig.ts
@@ -2,20 +2,25 @@ import type { MailAccount } from "../MailAccount";
 import { fetchConfig } from "./fetchConfig";
 import { exchangeAutoDiscoverV1XML, exchangeAutoDiscoverV2JSON } from "./exchangeConfig";
 import { guessConfig } from "./guessConfig";
+import { PriorityAbortable } from "../../util/Abortable";
 import { getDomainForEmailAddress } from "../../util/netUtil";
 import { ArrayColl } from "svelte-collections";
 
 export async function findConfig(emailAddress: string, password: string, abort: AbortController): Promise<ArrayColl<MailAccount>> {
   let domain = getDomainForEmailAddress(emailAddress);
   try {
-    return await fetchConfig(domain, emailAddress, abort);
+    let priorityOrder = new PriorityAbortable(abort, [
+      fetchConfig(domain, emailAddress, abort),
+      exchangeAutoDiscoverV2JSON(domain, emailAddress, abort),
+    ]);
+    return await priorityOrder.run();
   } catch (ex) {
     console.log(`Fetch config for ${emailAddress} failed`);
   }
   try {
-    return await exchangeAutoDiscoverV1XML(domain, emailAddress, abort);
+    return await exchangeAutoDiscoverV1XML(domain, emailAddress, null, password, () => false, abort);
   } catch (ex) {
-    console.log(`Fetch config for ${emailAddress} failed`);
+    console.log(`AutoDiscover for ${emailAddress} failed`);
   }
   try {
     let config = await guessConfig(domain, emailAddress, abort);
