df0748410adb228b9f20cf0756944daac57ffe07
Neil Rashbrook <neil.rashbrook@beonex.com>
2024-06-15 02:34:59 +0200

    Calendar: UI fixes

--- app/frontend/Calendar/EditEvent/TimeInput.svelte
+++ app/frontend/Calendar/EditEvent/TimeInput.svelte
@@ -4,11 +4,12 @@
   export let time: Date; /* in/out */
 
   let inputE: HTMLInputElement;
+  let userValue: string = "";
 
-  $: userValue = convertToUserInput(time);
-  function convertToUserInput(time: Date): string {
+  $: updateUserValue(time);
+  function updateUserValue(time: Date): string {
     try {
-      return time.toLocaleString(undefined, {
+      userValue = time.toLocaleString(undefined, {
         hour: "numeric",
         minute: "numeric",
       });
@@ -19,7 +20,26 @@
 
   export function onChange() {
     try {
-      time = new Date(userValue);
+      do {
+        let parts = userValue.match(/^(\d{1,2})(:?(\d\d))?\s*(am?|(pm?))?$/i);
+        if (!parts) break;
+        let hour = Number(parts[1]);
+        if (parts[4]) {
+          if (hour < 1 || hour > 12) break;
+          hour %= 12;
+          if (parts[5]) {
+            hour += 12;
+          }
+        } else {
+          if (hour > 23) break;
+        }
+        let minute = Number(parts[3] || 0);
+        if (minute > 59) break;
+        time.setHours(hour, minute, 0, 0);
+        time = time; // force refresh
+        return;
+      } while (false);
+      inputE.setCustomValidity("A valid time is required");
     } catch (ex) {
       inputE.setCustomValidity(ex.message);
     }
