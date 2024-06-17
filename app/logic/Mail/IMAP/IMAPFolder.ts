6030b9847c1bebab19add3cdc931b7dcc28b18bc
Neil Rashbrook <neil.rashbrook@beonex.com>
2024-06-17 20:08:35 +0100

    Make backend use native Node buffer and frontend use npm polyfill (#86)

--- app/logic/Mail/IMAP/IMAPFolder.ts
+++ app/logic/Mail/IMAP/IMAPFolder.ts
@@ -6,7 +6,7 @@ import { SQLEMail } from "../SQL/SQLEMail";
 import type { EMail } from "../EMail";
 import { ArrayColl, Collection } from "svelte-collections";
 import { assert } from "../../util/util";
-import { Buffer } from "buffer/";
+import { Buffer } from "buffer";
 
 export class IMAPFolder extends Folder {
   account: IMAPAccount;
