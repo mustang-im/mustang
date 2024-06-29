56a06fe7e0d0abb61c4a6e95383186d6516cc6c5
Ben Bucksch <ben.bucksch@beonex.com>
2024-06-29 21:56:45 +0200

    Formatting

--- app/lingui.config.ts
+++ app/lingui.config.ts
@@ -1,18 +1,18 @@
 import { jstsExtractor, svelteExtractor } from 'svelte-i18n-lingui/extractor';
 
 export default {
-	locales: ['ar','cs','da','de','el','en','es','fi','fr',
+  locales: ['ar','cs','da','de','el','en','es','fi','fr',
     'it','ja','nl','no','pl','pt','ro','ru','sv','uk','zh'
   ],
-	sourceLocale: 'en',
-	catalogs: [
-		{
-			path: 'l10n/locales/{locale}/messages',
-			include: [
+  sourceLocale: 'en',
+  catalogs: [
+    {
+      path: 'l10n/locales/{locale}/messages',
+      include: [
         "frontend/",
         "logic/",
       ]
-		}
-	],
-	extractors: [jstsExtractor, svelteExtractor]
+    }
+  ],
+  extractors: [jstsExtractor, svelteExtractor]
 };
