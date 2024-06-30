a58318e25f1226325b5716395fc5c26a469113ee
Ben Bucksch <ben.bucksch@beonex.com>
2024-06-30 20:50:39 +0200

    Build: Parula: Take version from e2, not app/

--- app/build/parula-brand.sh
+++ app/build/parula-brand.sh
@@ -1,5 +1,6 @@
 # Run from app/build/ directory
-VERSION=`grep "\"version\"" ../package.json | sed -e "s|^.*\"version\": \"||" -e "s|\",$||"`
+VERSION=`grep "\"version\"" ../../e2/package.json | sed -e "s|^.*\"version\": \"||" -e "s|\",$||"`
+echo Building version $VERSION
 perl -p -i \
   -e "s|production = false|production = true|;" \
   -e "s|'Mustang'|'Parula'|;" \
@@ -10,8 +11,8 @@ perl -p -i \
   -e "s|Mustang|Parula|g;" \
   -e "s|https://mustang.im|https://parula.beonex.com|g;" \
   -e "s|\"name\": \"mustang\"|\"name\": \"parula\"|;" \
-  -e "s|\"version\": \".*\"|\"version\": \"$VERSION\"|;" \
   ../../e2/package.json
+#  -e "s|\"version\": \".*\"|\"version\": \"$VERSION\"|;" \
 perl -p -i \
   -e "s|Mustang GmbH|Beonex GmbH|g;" \
   -e "s|Mustang|Parula|g;" \
