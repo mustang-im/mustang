88ab3c8bf041815ca102c78299d6d1a80e15b599
Neil Rashbrook <neil.rashbrook@beonex.com>
2024-07-01 21:30:17 +0100

    Support OWA calendar

--- app/logic/Calendar/OWA/OWACalendar.ts
+++ app/logic/Calendar/OWA/OWACalendar.ts
@@ -1,12 +1,143 @@
 import { Calendar } from "../Calendar";
 import { OWAEvent } from "./OWAEvent";
 import type { OWAAccount } from "../../Mail/OWA/OWAAccount";
+import { kMaxCount } from "../../Mail/OWA/OWAFolder";
+import { sanitize } from "../../../../lib/util/sanitizeDatatypes";
+import type { ArrayColl } from "svelte-collections";
 
 export class OWACalendar extends Calendar {
   readonly protocol: string = "calendar-owa";
+  readonly events: ArrayColl<OWAEvent>;
   account: OWAAccount;
 
   newEvent(): OWAEvent {
     return new OWAEvent(this);
+  }
+
+  getEventByItemID(id: string): OWAEvent | void {
+    return this.events.find(p => p.itemID == id);
+  }
+
+  async listEvents() {
+    let events: OWAEvent[] = [];
+    await this.listFolder("calendar", events);
+    /* Disabling tasks for now.
+    await this.listFolder("tasks", events);
+    */
+    this.events.replaceAll(events);
+  }
+
+  async listFolder(folder: string, events: OWAEvent[]) {
+    let request = {
+      __type: "FindItemJsonRequest:#Exchange",
+      Header: {
+        __type: "JsonRequestHeaders:#Exchange",
+        RequestServerVersion: "Exchange2013",
+      },
+      Body: {
+        __type: "FindItemRequest:#Exchange",
+        ItemShape: {
+          __type: "ItemResponseShape:#Exchange",
+          BaseShape: "IdOnly",
+        },
+        ParentFolderIds: [{
+          __type: "DistinguishedFolderId:#Exchange",
+          Id: folder,
+        }],
+        Traversal: "Shallow",
+        Paging: {
+          __type: "IndexedPageView:#Exchange",
+          BasePoint: "Beginning",
+          Offset: 0,
+          MaxEntriesReturned: kMaxCount,
+        },
+      },
+    };
+    let result: any = { RootFolder: { IncludesLastItemInRange: "false" } };
+    while (result?.RootFolder?.IncludesLastItemInRange === "false") {
+      result = await this.account.callOWA(request);
+      if (!result?.RootFolder?.Items?.length) {
+        break;
+      }
+      request.Body.Paging.Offset = sanitize.integer(result.RootFolder.IndexedPagingOffset);
+      await this.getEvents(result.RootFolder.Items.map(item => item.ItemId.Id), events);
+    }
+  }
+
+  async getEvents(eventIDs: string[], events: OWAEvent[]) {
+    if (!eventIDs.length) {
+      return;
+    }
+    let request = {
+      __type: "GetItemJsonRequest:#Exchange",
+      Header: {
+        __type: "JsonRequestHeaders:#Exchange",
+        RequestServerVersion: "Exchange2013",
+      },
+      Body: {
+        __type: "GetItemRequest:#Exchange",
+        ItemShape: {
+          __type: "ItemResponseShape:#Exchange",
+          BaseShape: "Default",
+          BodyType: "Best",
+          AdditionalProperties: [{
+            __type: "PropertyUri:#Exchange",
+            FieldURI: "item:Body",
+          }, {
+            __type: "PropertyUri:#Exchange",
+            FieldURI: "item:ReminderIsSet",
+          }, {
+            __type: "PropertyUri:#Exchange",
+            FieldURI: "item:ReminderMinutesBeforeStart",
+          }, {
+            __type: "PropertyUri:#Exchange",
+            FieldURI: "item:LastModifiedTime",
+          }, {
+            __type: "PropertyUri:#Exchange",
+            FieldURI: "item:TextBody",
+          }, {
+            __type: "PropertyUri:#Exchange",
+            FieldURI: "calendar:IsAllDayEvent",
+          }, {
+            __type: "PropertyUri:#Exchange",
+            FieldURI: "calendar:RequiredAttendees",
+          }, {
+            __type: "PropertyUri:#Exchange",
+            FieldURI: "calendar:OptionalAttendees",
+          }, {
+            __type: "PropertyUri:#Exchange",
+            FieldURI: "calendar:Recurrence",
+          }, {
+            __type: "PropertyUri:#Exchange",
+            FieldURI: "calendar:UID",
+          }, {
+            __type: "PropertyUri:#Exchange",
+            FieldURI: "task:Recurrence",
+          }],
+        },
+        ItemIds: eventIDs.map(id => ({
+          __type: "ItemId:#Exchange",
+          Id: id,
+        })),
+      },
+    };
+    let results = await this.account.callOWA(request);
+    let items = results.ResponseMessages ? results.ResponseMessages.Items.map(item => item.Items[0]) : results.Items;
+    for (let item of items) {
+      try {
+        let event = this.getEventByItemID(sanitize.nonemptystring(item.ItemId.Id));
+        if (event) {
+          event.fromJSON(item);
+          //await SQLEvent.save(event);
+        } else {
+          event = this.newEvent();
+          event.fromJSON(item);
+          //await SQLEvent.save(event);
+          events.push(event);
+        }
+      } catch (ex) {
+        this.account.errorCallback(ex);
+      }
+    }
   }
 }
--- app/logic/Calendar/OWA/OWAEvent.ts
+++ app/logic/Calendar/OWA/OWAEvent.ts
@@ -1,4 +1,176 @@
 import { Event } from "../Event";
+import type { OWACalendar } from "./OWACalendar";
+import WindowsTimezones from "../EWS/WindowsTimezones";
+import { PersonUID, findOrCreatePersonUID } from "../../Abstract/PersonUID";
+import OWACreateItemRequest from "../../Mail/OWA/OWACreateItemRequest";
+import OWADeleteItemRequest from "../../Mail/OWA/OWADeleteItemRequest";
+import OWAUpdateItemRequest from "../../Mail/OWA/OWAUpdateItemRequest";
+import { sanitize } from "../../../../lib/util/sanitizeDatatypes";
+
+const gTimeZone = WindowsTimezones[Intl.DateTimeFormat().resolvedOptions().timeZone] || "UTC";
 
 export class OWAEvent extends Event {
+  calendar: OWACalendar;
+
+  get itemID(): string | null {
+    return this.pID;
+  }
+  set itemID(val: string | null) {
+    this.pID = val;
+  }
+
+  fromJSON(json: any) {
+    this.itemID = sanitize.nonemptystring(json.ItemId.Id);
+    this.calUID = sanitize.nonemptystring(json.UID, null);
+    this.title = sanitize.nonemptystring(json.Subject, "");
+    if (json.Body?.BodyType == "Text") {
+      this.descriptionText = sanitize.nonemptystring(json.Body.Value, "");
+    } else {
+      this.descriptionText = sanitize.nonemptystring(json.TextBody?.Value, "");
+      if (json.Body?.BodyType == "HTML") {
+        this.descriptionHTML = sanitize.nonemptystring(json.Body.Value, "");
+      }
+    }
+    if (json.Start) {
+      this.startTime = sanitize.date(json.Start);
+    }
+    if (json.End) {
+      this.endTime = sanitize.date(json.End);
+    }
+    if (json.DueDate) {
+      this.endTime = sanitize.date(json.DueDate);
+    }
+    this.allDay = sanitize.boolean(json.IsAllDayEvent);
+    if (json.Recurrence) {
+      this.repeat = true;
+      if (!this.startTime && json.Recurrence.EndDateRecurrence) {
+        this.startTime = sanitize.date(json.Recurrence.EndDateRecurrence.StartDate.slice(0, 10));
+      }
+    }
+    if (json.ReminderIsSet == "true") {
+      this.alarm = new Date(this.startTime.getTime() - 60 * sanitize.integer(json.ReminderMinutesBeforeStart));
+    } else {
+      this.alarm = null;
+    }
+    this.location = sanitize.nonemptystring(json.Location?.DisplayName, "");
+    let participants: PersonUID[] = [];
+    if (json.RequiredAttendees) {
+      addParticipants(json.RequiredAttendees, participants);
+    }
+    if (json.OptionalAttendees) {
+      addParticipants(json.OptionalAttendees, participants);
+    }
+    this.participants.replaceAll(participants);
+    if (json.LastModifiedTime) {
+      this.lastMod = sanitize.date(json.LastModifiedTime);
+    }
+  }
+
+  async save() {
+    /* Disabling tasks for now.
+    if (this.startTime) {
+    */
+      await this.saveCalendarItem();
+    /* Disabling tasks for now.
+    } else {
+      await this.saveTask();
+    }
+    */
+    //await super.save();
+  }
+
+  async saveCalendarItem() {
+    let request: any = this.itemID ?
+      new OWAUpdateItemRequest(this.itemID, {SendMeetingInvitationsOrCancellations: "SendToAllAndSaveCopy"}) :
+      new OWACreateItemRequest({SendMeetingInvitations: "SendToAllAndSaveCopy"});
+    request.addField("CalendarItem", "Subject", this.title, "item:Subject");
+    request.addField("CalendarItem", "Body", this.descriptionHTML ? { __type: "BodyContentType:#Exchange", BodyType: "HTML", Value: this.descriptionHTML } : this.descriptionText ? { __type: "BodyContentType:#Exchange", BodyType: "Text", Value: this.descriptionText } : "", "item:Body");
+    request.addField("CalendarItem", "ReminderIsSet", this.alarm != null, "item:ReminderIsSet");
+    request.addField("CalendarItem", "ReminderMinutesBeforeStart", this.alarmMinutesBeforeStart(), "item:ReminderMinutesBeforeStart");
+    request.addField("CalendarItem", "UID", this.calUID, "calendar:UID");
+    request.addField("CalendarItem", "Start", this.dateString(this.startTime), "calendar:Start");
+    request.addField("CalendarItem", "End", this.dateString(this.endTime), "calendar:End");
+    request.addField("CalendarItem", "IsAllDayEvent", this.allDay, "calendar:IsAllDayEvent");
+    request.addField("CalendarItem", "Location", this.location, "calendar:Location");
+    request.addField("CalendarItem", "RequiredAttendees", this.participants.hasItems ? this.participants.contents.map(entry => ({
+      __type: "AttendeeType:#Exchange",
+      Mailbox: {
+        EmailAddress: entry.emailAddress,
+        Name: entry.name,
+      }
+    })) : [], "calendar:RequiredAttendees");
+    // No support for optional attendees in mustang;
+    // all attendees get converted to be required for now.
+    request.addField("CalendarItem", "OptionalAttendees", null, "calendar:OptionalAttendees");
+    request.addField("CalendarItem", "StartTimeZone", { Id: gTimeZone }, "calendar:StartTimeZone");
+    request.addField("CalendarItem", "EndTimeZone", { Id: gTimeZone }, "calendar:EndTimeZone");
+    let response = await this.calendar.account.callOWA(request);
+    if (this.calUID) {
+      return;
+    }
+    // Need an extra server roundtrip to get the UID
+    request = {
+      __type: "GetItemJsonRequest:#Exchange",
+      Header: {
+        __type: "JsonRequestHeaders:#Exchange",
+        RequestServerVersion: "Exchange2013",
+      },
+      Body: {
+        __type: "GetItemRequest:#Exchange",
+        ItemShape: {
+          __type: "ItemResponseShape:#Exchange",
+          BaseShape: "IdOnly",
+          AdditionalProperties: [{
+            __type: "PropertyUri:#Exchange",
+            FieldURI: "calendar:UID",
+          }],
+        },
+        ItemIds: [{
+          __type: "ItemId:#Exchange",
+          Id: response.Items[0].ItemId.Id,
+        }],
+      },
+    };
+    response = await this.calendar.account.callOWA(request);
+    this.calUID = sanitize.nonemptystring(response.Items[0].UID);
+    this.itemID = sanitize.nonemptystring(response.Items[0].ItemId.Id);
+  }
+
+  async saveTask() {
+    let request = this.itemID ? new OWAUpdateItemRequest(this.itemID) : new OWACreateItemRequest();
+    request.addField("Task", "Subject", this.title, "item:Subject");
+    request.addField("Task", "ReminderIsSet", this.alarm != null, "item:ReminderIsSet");
+    request.addField("Task", "ReminderMinutesBeforeStart", this.alarmMinutesBeforeStart(), "item:ReminderMinutesBeforeStart");
+    request.addField("Task", "DueDate", this.endTime?.toISOString(), "task:DueDate");
+    let response = await this.calendar.account.callOWA(request);
+    this.itemID = sanitize.nonemptystring(response.Items[0].ItemId.Id);
+  }
+
+  dateString(date: Date): string {
+    if (this.allDay) {
+      return date.getFullYear() + "-" + String(date.getMonth() + 1).padStart(2, "0") + "-" + String(date.getDate()).padStart(2, "0");
+    }
+    return date.toISOString();
+  }
+
+  alarmMinutesBeforeStart(): number {
+    if (!this.alarm) {
+      // Exchange requires a value, even if there is no alarm.
+      // It uses a separate flag for whether the alarm is set.
+      return 0;
+    }
+    return (this.alarm.getTime() - this.startTime.getTime()) / -60 | 0;
+  }
+
+  async deleteIt() {
+    let request = new OWADeleteItemRequest(this.itemID, {SendMeetingCancellations: "SendToAllAndSaveCopy"});
+    await this.calendar.account.callOWA(request);
+    await super.deleteIt();
+  }
+}
+
+function addParticipants(attendees, participants: PersonUID[]) {
+  for (let attendee of attendees) {
+    participants.push(findOrCreatePersonUID(sanitize.emailAddress(attendee.Mailbox.EmailAddress), sanitize.nonemptystring(attendee.Mailbox.Name, null)));
+  }
 }
