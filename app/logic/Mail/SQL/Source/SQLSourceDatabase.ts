0c6a2dc7afa48434278b3b9abef98ef4f1486a82
Ben Bucksch <ben.bucksch@beonex.com>
2024-07-01 17:46:45 +0200

    SQL: use path.join() for DB filename

--- app/logic/Mail/SQL/Source/SQLSourceDatabase.ts
+++ app/logic/Mail/SQL/Source/SQLSourceDatabase.ts
@@ -8,10 +8,12 @@ export async function getDatabase(): Promise<Database> {
   if (mailSourceDatabase) {
     return mailSourceDatabase;
   }
-  let dir = await appGlobal.remoteApp.getFilesDir() + "/backup";
+  let dir = await appGlobal.remoteApp.path.join(
+    await appGlobal.remoteApp.getFilesDir(), "backup");
   await appGlobal.remoteApp.fs.mkdir(dir, { recursive: true, mode: 0o700 });
   const getDatabase = appGlobal.remoteApp.getSQLiteDatabase;
-  mailSourceDatabase = await getDatabase("backup/mail-backup.db");
+  let file = await appGlobal.remoteApp.path.join("backup", "mail-backup.db");
+  mailSourceDatabase = await getDatabase(file);
   await mailSourceDatabase.migrate(mailSourceDatabaseSchema);
   return mailSourceDatabase;
 }
