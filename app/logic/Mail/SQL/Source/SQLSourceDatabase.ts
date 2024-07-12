55f402453fe92b80a656fb4037970053d33ed45a
Neil Rashbrook <neil.rashbrook@beonex.com>
2024-07-12 16:54:25 +0100

    Consistently use config and files dir #128

--- app/logic/Mail/SQL/Source/SQLSourceDatabase.ts
+++ app/logic/Mail/SQL/Source/SQLSourceDatabase.ts
@@ -9,7 +9,7 @@ export async function getDatabase(): Promise<Database> {
     return mailSourceDatabase;
   }
   let dir = await appGlobal.remoteApp.path.join(
-    await appGlobal.remoteApp.getFilesDir(), "backup");
+    await appGlobal.remoteApp.getConfigDir(), "backup");
   await appGlobal.remoteApp.fs.mkdir(dir, { recursive: true, mode: 0o700 });
   const getDatabase = appGlobal.remoteApp.getSQLiteDatabase;
   let file = await appGlobal.remoteApp.path.join("backup", "mail-backup.db");
