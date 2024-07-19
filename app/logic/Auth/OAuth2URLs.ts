1605095c8653b148485ffd35858a1a1c885bc497
Ben Bucksch <ben.bucksch@beonex.com>
2024-07-19 04:23:37 +0200

    OAuth2: Change scope for Microsoft to https://outlook.office.com/.default #137

--- app/logic/Auth/OAuth2URLs.ts
+++ app/logic/Auth/OAuth2URLs.ts
@@ -17,7 +17,7 @@ export const OAuth2URLs = [
     tokenURL: "https://login.microsoftonline.com/common/oauth2/v2.0/token",
     tokenURLPasswordAuth: "https://login.microsoftonline.com/organizations/oauth2/v2.0/token",
     logoutURL: "https://login.microsoftonline.com/common/oauth2/logout",
-    scope: "offline_access https://outlook.office365.com/.default",
+    scope: "offline_access https://outlook.office.com/.default",
     clientID: "5cf03223-8b81-4558-ae82-a8e31e66a889",
     clientSecret: null,
   },
