003d654a1200cbc036cfc3222e5c079fd45f7a84
Ben Bucksch <ben.bucksch@beonex.com>
2024-07-23 22:48:04 +0200

    ADA CASA: Remove unused build scripts

--- e2/docker/linux/build-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# ADA CASA https://docs.fluidattacks.com/criteria/vulnerabilities/380 wants a digest for the docker container image, but that's not possible here.
-# We're not currently using this build path anyways, so just commenting it out
-echo Disabled
-exit 1
-
-#docker run -it -v /usr/src/mustang/release-build/:/mustang/ --user 1000 mustang-build-linux /bin/bash /mustang/e2/docker/linux/build.sh
