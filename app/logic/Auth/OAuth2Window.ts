deb8257f8c5bee7bce18b689ca8f85d3e1e5e99e
Ben Bucksch <ben.bucksch@beonex.com>
2024-07-24 22:46:39 +0200

    ADA CASA: window.open() noopener broke the popup object
    * We're overriding the features in index.js, and noopener is included there
    * We're opening a different URL on another domain, so the browser shouldn't
      allow cross-domain access anyways.

--- app/logic/Auth/OAuth2Window.ts
+++ app/logic/Auth/OAuth2Window.ts
@@ -14,7 +14,7 @@ export class OAuth2Window extends OAuth2UI {
   async login(): Promise<string> {
     let url = this.oAuth2.getAuthURL();
     // Ends up in e2/src/main/index.ts setWindowOpenHandler(). Features are overridden there.
-    let popup = window.open(url, "_blank", "center,noopener,noreferrer,oauth2popup") as Window;
+    let popup = window.open(url, "_blank", "center,oauth2popup") as Window;
     assert(popup, "Failed to open OAuth2 window");
     return await new Promise((resolve, reject) => {
       let ipcRenderer = (window as any).electron.ipcRenderer; // TODO use JPC, or remove window
