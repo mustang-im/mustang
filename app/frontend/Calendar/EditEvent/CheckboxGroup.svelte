ef405c506e5f0b4a1fb012ab84643c6eb8ac9d41
Neil Rashbrook <neil.rashbrook@beonex.com>
2024-08-08 22:17:05 +0100

    Implement frontend for editing recurring events
    - Ensure recurring instances have been generated for the next three years
    - Allow the start date of events to be changed as well as the time
    - Don't allow exceptions to be recurring events
    - Basic UI for entering recurrence rules (pluralised but not localised)
    - Choice of recurrence frequency and interval
    - Choice of days of week for weekly recurrence pattern
    - Choice of day of month or day of week of month for monthly or yearly recurrence patterns
    - Choice of recurrence pattern limit
    - Allow exclusions to be created (also supports deleting whole events)
    - Warn when removing or changing a recurrence pattern

---
+++ app/frontend/Calendar/EditEvent/CheckboxGroup.svelte
@@ -0,0 +1,26 @@
+<!--
+  Svelte's bind:group only works for literal <input type=checkbox> (issue 2308).
+  This isn't a true bind:group because it can't react to changes in group.
+  TODO: support other Svelteui <Checkbox> properties
+-->
+<hbox>
+  <slot><label>{label}</label></slot>
+  {#each items as item, i}
+    <Checkbox {size} {radius} {...item} bind:checked={item.checked} />
+  {/each}
+</hbox>
+
+<script lang="ts">
+  import { Checkbox } from "@svelteuidev/core";
+  export let label = "";
+  export let size = undefined;
+  export let radius = undefined;
+  export let items = [];
+  export let group = [];
+
+  for (let item of items) {
+    item.checked = group.includes(item.value);
+  }
+
+  $: group = items.filter(item => item.checked).map(item => item.value);
+</script>
