40f8d2137290096b158ed628511f6e57e4e679a0
Neil Rashbrook <neil.rashbrook@beonex.com>
2024-08-09 18:31:56 +0100

    Fix deleting items in EWS

--- app/logic/Mail/EWS/EWSDeleteItemRequest.ts
+++ app/logic/Mail/EWS/EWSDeleteItemRequest.ts
@@ -1,13 +1,13 @@
 export default class EWSDeleteItemRequest {
   m$DeleteItem: any = {
-    m$Items: {
+    m$ItemIds: {
       t$ItemId: {}
     },
     DeleteType: "MoveToDeletedItems",
   };
 
   constructor(id: string, attributes?: {[key: string]: string | boolean}) {
-    this.m$DeleteItem.m$Items.t$ItemId.Id = id;
+    this.m$DeleteItem.m$ItemIds.t$ItemId.Id = id;
     Object.assign(this.m$DeleteItem, attributes);
   }
 }
