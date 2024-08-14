d90576d86dd4d66744837a4d1ac37e8c15d00d2e
Jeremy Chang <71667021+jermy-c@users.noreply.github.com>
2024-08-14 11:23:22 -0600

    Add client ID

--- app/logic/Auth/OAuth2URLs.ts
+++ app/logic/Auth/OAuth2URLs.ts
@@ -18,7 +18,7 @@ export const OAuth2URLs = [
     tokenURLPasswordAuth: "https://login.microsoftonline.com/organizations/oauth2/v2.0/token",
     logoutURL: "https://login.microsoftonline.com/common/oauth2/logout",
     scope: "offline_access https://outlook.office.com/.default",
-    clientID: "5cf03223-8b81-4558-ae82-a8e31e66a889",
+    clientID: "1d01f7d4-334b-475a-9176-a0d504c6a38a",
     clientSecret: null,
     doPKCE: false,
   },
