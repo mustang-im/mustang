cd7d4aac25ea9dd60b85e94c6c35af795b5dd57e
Jeremy Chang <jeremy.chang@beonex.com>
2024-09-13 14:54:33 +0000

    OWA: Outlook.com: Login success not detected - fixes #198 (#205)
    * Port Owl logic
    * Use the hostname from the cookie
    * Set "no landing page", if it's a personal account
    
    ---------
    Co-authored-by: Ben Bucksch <ben.bucksch@beonex.com>

--- backend/owa.ts
+++ backend/owa.ts
@@ -1,10 +1,19 @@
 import { session as Session, BrowserWindow } from "electron";
 
+const kCanaryName = "X-OWA-CANARY";
+const kHotmailServer = "outlook.live.com";
+
 export async function fetchSessionData(partition: string, url: string, interactive: boolean) {
   let session = Session.fromPartition(partition);
   let response = await session.fetch(url + 'sessiondata.ashx', { method: 'POST' });
   if ([401, 440].includes(response.status) && interactive) {
     return await new Promise(resolve => {
+      let urlObj = new URL(url);
+      // We want to skip the landing page for personal Microsoft accounts.
+      let isHotmail = urlObj.hostname == kHotmailServer;
+      if (isHotmail) {
+        url = url + "?nlp=1";
+      }
       let popup = new BrowserWindow({
         //parent: mainWindow,
         //modal: true,
@@ -25,7 +34,7 @@ export async function fetchSessionData(partition: string, url: string, interacti
           return;
         }
         finished = true;
-        session.cookies.removeListener('changed', checkCanary);
+        session.cookies.removeListener('changed', onCookie);
         resolve(data);
         if (!popup.isDestroyed()) {
           popup.destroy();
@@ -33,23 +42,49 @@ export async function fetchSessionData(partition: string, url: string, interacti
       };
       let checkLoginFinished = async function() {
         try {
+          url = urlObj.href;
           response = await session.fetch(url + 'sessiondata.ashx', { method: 'POST' });
-          finish(await response.json());
+          let json = await response.json();
+          if (isHotmail) {
+            json.owaURL = url;
+          }
+          finish(json);
         } catch (ex) {
         }
       }
-      let checkCanary = function(_event, cookie, _cause, removed) {
-        if (!removed && cookie.name == 'X-OWA-CANARY') {
-          checkLoginFinished();
+      let onCookie = async function (_event, cookie, _cause, removed) {
+        try {
+          if (removed) {
+            return;
+          }
+          // For Hotmail, check the path to the CANARY cookie.
+          if (cookie.domain == kHotmailServer &&
+            cookie.name == kCanaryName &&
+            cookie.path?.startsWith("/owa/")) {
+            // Hotmail also sets cookies for /owa/0/, /mail/0/, /calendar/0/ etc.,
+            // but we can use only the /owa/0/ cookie.
+            // We also need to use that URL for the service request.
+            // This needs to happen before CheckLoginFinished().
+            urlObj.hostname = cookie.domain;
+            urlObj.pathname = cookie.path;
+            isHotmail = true;
+          }
+          // If we receive the canary cookie, check whether we're logged in
+          if (cookie.name == kCanaryName &&
+            cookie.domain == urlObj.hostname) {
+            await checkLoginFinished();
+          }
+        } catch (ex) {
+          console.error(ex);
         }
       };
       let checkLoaded = async function(_event) {
-        let cookies = await Session.fromPartition(partition).cookies.get({ name: 'X-OWA-CANARY' });
+        let cookies = await Session.fromPartition(partition).cookies.get({ name: kCanaryName });
         if (cookies[0]?.value) {
-          checkLoginFinished();
+          await checkLoginFinished();
         }
       };
-      session.cookies.on('changed', checkCanary);
+      session.cookies.on('changed', onCookie);
       popup.on('closed', function() { finish(null); });
       popup.webContents.on('did-stop-loading', checkLoaded);
       popup.loadURL(url);
@@ -71,17 +106,16 @@ export async function fetchJSON(partition: string, url: string, action: string, 
     json: null,
   };
   let session = Session.fromPartition(partition);
-  let cookies = await session.cookies.get({ name: 'X-OWA-CANARY' });
+  let cookies = await session.cookies.get({ name: kCanaryName });
   if (!cookies.length) {
     result.status = 401;
     return result;
   }
   let options = {
     method: "POST",
-    headers: {
-      "X-OWA-CANARY": cookies[0].value,
-    },
+    headers: {},
   };
+  options.headers[kCanaryName] = cookies[0].value;
   if (action) {
     options.headers.Action = action;
   }
@@ -107,7 +141,7 @@ export async function streamJSON(partition: string, url: string) {
     body: null,
   };
   let session = Session.fromPartition(partition);
-  let cookies = await session.cookies.get({ name: 'X-OWA-CANARY' });
+  let cookies = await session.cookies.get({ name: kCanaryName });
   if (!cookies.length) {
     result.status = 401;
     return result;
