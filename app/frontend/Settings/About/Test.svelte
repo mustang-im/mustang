75ec684ffb6303e5814cd0d9f76d5b6ceaae8d28
Ben Bucksch <ben.bucksch@beonex.com>
2024-09-14 02:43:13 +0200

    About: Test: Close after finish loading

--- app/frontend/Settings/About/Test.svelte
+++ app/frontend/Settings/About/Test.svelte
@@ -1,17 +1,24 @@
 <hbox class="buttons">
   <Button label={$t`Load test data`}
-    onClick={getTestObjects}
+    onClick={loadTest}
     />
 </hbox>
 
 <script lang="ts">
   import { getTestObjects } from "../../../logic/testData";
+  import { openApp } from "../../AppsBar/selectedApp";
+  import { chatMustangApp } from "../../Chat/ChatMustangApp";
   import Button from "../../Shared/Button.svelte";
   import { t } from "../../../l10n/l10n";
+
+  async function loadTest() {
+    await getTestObjects();
+    openApp(chatMustangApp);
+  }
 </script>
 
 <style>
   .buttons {
-    justify-content: end;
+    justify-content: start;
   }
 </style>
