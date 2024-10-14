98d2f12eaad1bdff72b5a3c71a0f2754ec4fd1dc
Jeremy Chang <jeremy.chang@beonex.com>
2024-10-14 16:00:01 -0600

    Contacts: Use SQLStorage class

--- app/logic/Contacts/Store/setStorage.ts
+++ app/logic/Contacts/Store/setStorage.ts
@@ -1,8 +1,8 @@
 import type { Addressbook } from "../Addressbook";
-import { SQLAddressbook } from "../SQL/SQLAddressbook";
+import { SQLStorage } from "../SQL/SQLStorage";
 
 export function setStorage(acc: Addressbook) {
   if (!acc.storage) {
-    acc.storage = new SQLAddressbook();
+    acc.storage = new SQLStorage();
   }
 }
