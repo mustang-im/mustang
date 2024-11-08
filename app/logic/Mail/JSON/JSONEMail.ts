03c9550ba3def3db474e634457df352293fbda97
Jeremy Chang <jeremy.chang@beonex.com>
2024-11-08 10:18:47 -0600

    JSONEmail: fix property names

--- app/logic/Mail/JSON/JSONEMail.ts
+++ app/logic/Mail/JSON/JSONEMail.ts
@@ -203,7 +203,7 @@ export class JSONEMail {
       a.mimeType = sanitize.nonemptystring(json.mimeType, "application/octet-stream");
       a.contentID = sanitize.nonemptystring(json.contentID, "" + fallbackID);
       a.filename = sanitize.nonemptystring(json.filename, "attachment-" + fallbackID + "." + fileExtensionForMIMEType(a.mimeType));
-      let filepathLocal = sanitize.string(json.filepathLocal, null)
+      let filepathLocal = sanitize.string(json.filepath, null)
       if (filepathLocal) {
         a.filepathLocal = this.filesDir + "/" + filepathLocal;
       }
