a0a80fc3d05e430fbbf54e111550dfff6f90435a
Ben Bucksch <ben.bucksch@beonex.com>
2024-11-21 01:47:21 +0100

    UI: Make component for Paper

--- app/frontend/Mail/Message/MessageDisplay.svelte
+++ app/frontend/Mail/Message/MessageDisplay.svelte
@@ -4,9 +4,9 @@
   {#if $message.event || $message.scheduling}
     <Invitation {message} />
   {/if}
-  <vbox flex class="paper">
+  <Paper>
     <MessageBody {message} />
-  </vbox>
+  </Paper>
 </vbox>
 
 <script lang="ts">
@@ -15,6 +15,7 @@
   import MessageAttachments from "./AttachmentsUI.svelte";
   import MessageBody from "./MessageBody.svelte";
   import Invitation from "../../Calendar/Invitation.svelte";
+  import Paper from "../../Shared/Paper.svelte";
 
   export let message: EMail;
 </script>
@@ -23,12 +24,5 @@
 .message-display {
   background-color: var(--leftbar-bg);
   color: var(--leftbar-fg);
-}
-.paper {
-  background-color: var(--main-bg);
-  color: var(--main-fg);
-  border-radius: 5px;
-  box-shadow: -1px 0px 5px 0.5px rgb(0, 0, 0, 10%);
-  margin: 0px 0px 2px 8px;
 }
 </style>
