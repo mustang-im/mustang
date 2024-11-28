a673d6b293b719317a4fd610b310f0158a9ccf43
Ben Bucksch <ben.bucksch@beonex.com>
2024-11-28 13:14:59 +0100

    SQL: Replace dummy email with empty email

--- app/logic/Mail/SQL/SQLEMail.ts
+++ app/logic/Mail/SQL/SQLEMail.ts
@@ -278,7 +278,7 @@ export class SQLEMail {
     email.threadID = sanitize.string(row.threadID ?? row.parentMsgID, null);
     email.downloadComplete = sanitize.boolean(!!row.downloadComplete);
 
-    email.contact = findOrCreatePersonUID("foo45@example.com", sanitize.label(row.contactName, null));
+    email.contact = findOrCreatePersonUID("must@n.g", sanitize.label(row.contactName, null));
   }
 
   static async readWritableProps(email: EMail, row?: any) {
