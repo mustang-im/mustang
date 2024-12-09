a07dac545d88d0b5a90dface5f704a361fd32384
Ben Bucksch <ben.bucksch@beonex.com>
2024-12-09 21:29:33 +0100

    Setup: Bottom buttons: Secondard less prominent, more consistent

--- app/frontend/Setup/Shared/ButtonsBottom.svelte
+++ app/frontend/Setup/Shared/ButtonsBottom.svelte
@@ -46,6 +46,13 @@
   .buttons :global(> *) {
     margin-inline-end: 8px;
   }
+  .buttons :global(button.secondary .label) {
+    margin-left: 5px;
+    margin-right: 5px;
+  }
+  .buttons :global(button.disabled) {
+    opacity: 40%;
+  }
   .buttons :global(button.secondary) {
     background-color: inherit;
     padding: 3px 8px;
@@ -53,5 +60,13 @@
   }
   .buttons :global(button.secondary:not(.disabled)) {
     opacity: 85%;
+  }
+  .buttons :global(button.secondary) {
+    border-color: #BBBBBB;
+  }
+  @media (prefers-color-scheme: dark) {
+    .buttons :global(button.secondary) {
+      border-color: #222222;
+    }
   }
 </style>
