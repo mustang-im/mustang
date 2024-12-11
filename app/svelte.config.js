79d58e64897960dec3ecc6239a85727d4db8be1b
Ben Bucksch <ben.bucksch@beonex.com>
2024-12-11 21:20:46 +0100

    svelte-check: Silence autofocus and click warning

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -4,4 +4,16 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  // <https://github.com/sveltejs/language-tools/issues/650#issuecomment-1181354795>
+  onwarn: (warning, handler) => {
+    if (ignoreSvelteCompilerWarnings.includes(warning.code)) {
+      return;
+    }
+    handler(warning);
+  },
 }
+
+const ignoreSvelteCompilerWarnings = [
+  "a11y-autofocus",
+  "a11y-mouse-events-have-key-events",
+];
