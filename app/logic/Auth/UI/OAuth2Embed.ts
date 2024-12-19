a21e116f99f2ee3b1bbc3127b63461d6d62bae88
Ben Bucksch <ben.bucksch@beonex.com>
2024-12-19 02:12:23 +0100

    OAuth2: Implement embedded login window class

--- app/logic/Auth/UI/OAuth2Embed.ts
+++ app/logic/Auth/UI/OAuth2Embed.ts
@@ -1,7 +1,5 @@
 import { OAuth2UI } from "./OAuth2UI";
-import type { LoginDialogMustangApp } from "../../../frontend/Mail/MailMustangApp";
-import type { URLString } from "../../util/util";
-import { ArrayColl } from "svelte-collections";
+import { assert, type URLString } from "../../util/util";
 
 /**
  * Opens the OAuth2 login webpage in a dialog within the app main window,
@@ -11,7 +9,9 @@ export class OAuth2Embed extends OAuth2UI {
   startURL: URLString;
   protected doneFunc: (authCode: string) => void;
   protected failFunc: (ex: Error) => void;
-  mustangApp: LoginDialogMustangApp;
+  /** The caller needs to set this to the <webview> element
+   * where he wants the login webpage to appear. */
+  webviewE: HTMLIFrameElement;
 
   /**
    * Does an interactive login
@@ -20,33 +20,38 @@ export class OAuth2Embed extends OAuth2UI {
    */
   async login(): Promise<string> {
     this.startURL = await this.oAuth2.getAuthURL();
-    // console.log("OAuth2 start url", dialog.startURL);
-    oAuth2DialogOpen.add(this);
+    console.log("OAuth2 start url", this.startURL);
+    this.webviewE.hidden = false;
+    this.webviewE.setAttribute("url", this.startURL);
     return new Promise((resolve, reject) => {
       this.doneFunc = resolve;
       this.failFunc = reject;
     });
   }
   urlChange(url: URLString) {
-    // console.log("OAuth2 page change to", url);
-    if (this.oAuth2.isAuthDoneURL(url) && this.doneFunc) {
-      try {
-        this.doneFunc(this.oAuth2.getAuthCodeFromDoneURL(url));
-        oAuth2DialogOpen.remove(this);
-      } catch (ex) {
-        this.failed(ex);
-      }
+    console.log("OAuth2 page change to", url);
+    if (this.oAuth2.isAuthDoneURL(url)) {
+      this.success(url);
+    }
+  }
+  close() {
+    this.webviewE.hidden = true;
+    this.webviewE.setAttribute("url", "");
+  }
+  success(finalURL: URLString) {
+    this.close();
+    assert(this.doneFunc, "Need doneFunc");
+    try {
+      this.doneFunc(this.oAuth2.getAuthCodeFromDoneURL(finalURL));
+    } catch (ex) {
+      this.failed(ex);
     }
   }
   failed(ex: Error) {
-    if (!this.failFunc) {
-      return;
-    }
-    oAuth2DialogOpen.remove(this);
+    this.close();
+    assert(this.failFunc, "Need failFunc");
     this.failFunc(ex);
     this.failFunc = null;
     this.doneFunc = null;
   }
 }
-
-export const oAuth2DialogOpen = new ArrayColl<OAuth2Embed>();
