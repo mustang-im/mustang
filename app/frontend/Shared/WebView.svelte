8fa2ab3c47384e44b5165b1f24261b57b220a20d
Jeremy Chang <jeremy.chang@beonex.com>
2025-01-02 16:59:14 -0600

    Block all other resources except for CSS and IMG

--- app/frontend/Shared/WebView.svelte
+++ app/frontend/Shared/WebView.svelte
@@ -74,9 +74,10 @@
         over-flow: visible !important;
       }
     </style>`;
-    const head = headHTML; /*`<meta http-equiv="Content-Security-Policy" content="default-src '${
-      allowServerCalls === true ? "*" : allowServerCalls === false ? "none" : allowServerCalls
-    }'">\n\n` + headHTML + `\n\n`; */
+    let servers = allowServerCalls === true ? "*" :
+      allowServerCalls === false ? "'none'" : allowServerCalls;
+    const head = `<meta http-equiv="Content-Security-Policy" content="default-src 'none';
+      style-src ${servers}; img-src ${servers}">\n\n` + headHTML + `\n\n`;
     let displayHTML = html;
     let headPos = displayHTML.indexOf("<head>");
     headPos = headPos < 0 ? 0 : headPos + 6;
