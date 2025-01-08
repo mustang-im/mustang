df0d8c7e0c9745311707d7cb98da63b21dc0ba0a
Ben Bucksch <ben.bucksch@beonex.com>
2025-01-08 14:56:08 +0100

    All accounts: Fix Write button

--- app/frontend/Mail/LeftPane/WriteButton.svelte
+++ app/frontend/Mail/LeftPane/WriteButton.svelte
@@ -8,13 +8,17 @@
 <script lang="ts">
   import type { MailAccount } from "../../../logic/Mail/MailAccount";
   import { mailMustangApp } from "../MailMustangApp";
+  import { appGlobal } from "../../../logic/app";
   import RoundButton from "../../Shared/RoundButton.svelte";
   import WriteIcon from "lucide-svelte/icons/pencil";
-  import { t } from "../../../l10n/l10n";
+  import { assert } from "../../../logic/util/util";
+  import { gt, t } from "../../../l10n/l10n";
 
   export let selectedAccount: MailAccount; /* in/out */
 
   function newMail() {
+    let account = selectedAccount ?? appGlobal.emailAccounts.first;
+    assert(account, gt`Please set up an email account first`);
     let email = selectedAccount.newEMailFrom();
     mailMustangApp.writeMail(email);
   }
