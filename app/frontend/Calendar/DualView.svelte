6adfd1ddd4d460fff1061d6bd09e3c13e4247848
Neil Rashbrook <neil.rashbrook@beonex.com>
2025-01-09 21:32:35 +0000

    Week views should not scroll to clicked date

--- app/frontend/Calendar/DualView.svelte
+++ app/frontend/Calendar/DualView.svelte
@@ -1,8 +1,8 @@
 <hbox flex class="dual-view">
-  <WeekView bind:start {events} showDays={2}>
+  <WeekView bind:start={$selectedDate} {events} showDays={2}>
     <slot name="top-left" slot="top-left" />
   </WeekView>
-  <MonthView start={monthStart} {events} showDays={35}>
+  <MonthView {start} {events} showDays={35}>
     <slot name="top-right" slot="top-right" />
   </MonthView>
 </hbox>
@@ -12,16 +12,10 @@
   import type { Collection } from "svelte-collections";
   import WeekView from "./WeekView.svelte";
   import MonthView from "./MonthView.svelte";
+  import { selectedDate } from "./selected";
 
   export let start: Date;
   export let events: Collection<Event>;
-
-  let monthStart: Date;
-
-  $: start, syncMonth();
-  function syncMonth() {
-    monthStart = start;
-  }
 </script>
 
 <style>
--- app/frontend/Calendar/selected.ts
+++ app/frontend/Calendar/selected.ts
@@ -6,6 +6,7 @@ import { writable, type Writable } from "svelte/store";
 export let selectedEvent: Writable<Event> = writable(null);
 
 // TODO Move into observable prefs?
+export let startDate = writable(new Date());
 export let selectedDate = writable(new Date());
 export type DateInterval = 0 | 1 | 2 | 7 | 31 | 28;
 export let selectedDateInterval = writable(2 as DateInterval);
