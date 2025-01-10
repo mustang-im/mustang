4d764b50341c0d56d2888109442c7db34cbb2f13
Ben Bucksch <1907525+benbucksch@users.noreply.github.com>
2025-01-10 19:35:29 +0100

    Move down

--- app/frontend/Calendar/selected.ts
+++ app/frontend/Calendar/selected.ts
@@ -5,9 +5,8 @@ import { writable, type Writable } from "svelte/store";
 /** Which event is highlighted right now. */
 export let selectedEvent: Writable<Event> = writable(null);
 
-// TODO Move into observable prefs?
-export let startDate = writable(new Date());
 export let selectedDate = writable(new Date());
 export type DateInterval = 0 | 1 | 2 | 7 | 31 | 28;
 export let selectedDateInterval = writable(2 as DateInterval);
+export let startDate = writable(new Date());
 export let selectedCalendar = writable<Calendar | null>(null);
