3551eadded5a47f57d397511c47da60429f9cb41
Jeremy Chang <jeremy.chang@beonex.com>
2025-01-10 18:24:15 -0600

    Add comment about Delagotor object purpose

--- backend/backend.ts
+++ backend/backend.ts
@@ -342,6 +342,21 @@ function addEventListenerWebContents(webContentsID: number, webviewEvent: string
   });
 }
 
+/**
+ * Delegator object
+ * This is for security purposes only because `win.session.setPreloads()`
+ * sets the preload script which is dangerous and `win.session.loadExtensions()`
+ * loads extensions which could load also malicious extensions. There maybe other
+ * methods that may also be dangerous so it's best to just expose the necessary ones.
+ * 
+ * Preferably, only the methods that appear in <webview> should be exposed
+ * because Electron likely exposes only those because they are safe for the renderer
+ * process.
+ * 
+ * - https://www.electronjs.org/docs/latest/api/webview-tag
+ * - https://www.electronjs.org/docs/latest/api/session#sessetpreloadspreloads
+ * - https://www.electronjs.org/docs/latest/api/session#sesloadextensionpath-options
+ */
 class WebContents {
   private _win: Electron.WebContents;
 
