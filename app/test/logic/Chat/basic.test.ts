99d936e4d126cc6c57967df3d9c3c9c67fbd7e25
Ben Bucksch <ben.bucksch@beonex.com>
2025-01-16 05:06:25 +0100

    Test: Chat: Integration tests for chat account login

---
+++ app/test/logic/Chat/basic.test.ts
@@ -0,0 +1,20 @@
+import { newChatAccountForProtocol } from '../../../logic/Chat/AccountsList/ChatAccounts';
+import { kChatAccounts } from './logins';
+import { connectToBackend, stopBackend } from '../util/backend.test';
+import { afterAll, beforeAll, expect, test } from 'vitest';
+
+beforeAll(connectToBackend);
+
+test.each(kChatAccounts)("Test $protocol account $username", async (config) => {
+  if (config.disabled) { // <https://github.com/jestjs/jest/issues/7695>
+    return;
+  }
+  let acc = newChatAccountForProtocol(config.protocol);
+  acc.username = config.username;
+  acc.password = config.password;
+  acc.hostname = acc.hostname;
+
+  await acc.login(false);
+});
+
+afterAll(stopBackend);
---
+++ app/test/logic/Chat/logins.ts
@@ -0,0 +1,11 @@
+/** Test accounts
+ * Format: As read by `JSONChatAccount` */
+export const kChatAccounts = [
+  {
+    protocol: "xmpp",
+    username: "test@example.com",
+    password: "test",
+    hostname: "xmpp.example.com",
+    disabled: true,
+  },
+];
