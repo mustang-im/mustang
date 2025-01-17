a48c1dfcbda7359d921ec7e10400f3d511251957
Neil Rashbrook <neil.rashbrook@beonex.com>
2025-01-17 21:33:21 +0000

    Set up both ends of JPC at the same time

--- app/test/logic/util/backend.test.ts
+++ app/test/logic/util/backend.test.ts
@@ -1,9 +1,11 @@
+import { startupBackend, shutdownBackend } from "../../../../backend/backend";
 import JPCWebSocket from '../../../../lib/jpc-ws/protocol';
 import { appGlobal } from '../../../logic/app';
 
 let gJPC: JPCWebSocket;
 
 export async function connectToBackend() {
+  await startupBackend();
   let jpc = gJPC = new JPCWebSocket(null);
   const kSecret = 'eyache5C';
   await jpc.connect(kSecret, "localhost", 5453);
@@ -12,4 +14,5 @@ export async function connectToBackend() {
 
 export async function stopBackend() {
   await gJPC.close();
+  await shutdownBackend();
 }
