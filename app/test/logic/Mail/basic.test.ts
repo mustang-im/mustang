0548e272dee4ec5bee58c6fb4ed5e54801a8832d
Ben Bucksch <ben.bucksch@beonex.com>
2025-01-21 00:11:07 +0100

    Test: OAuth2

--- app/test/logic/Mail/basic.test.ts
+++ app/test/logic/Mail/basic.test.ts
@@ -1,10 +1,14 @@
 import { kMailAccounts } from './logins';
 import { newAccountForProtocol } from '../../../logic/Mail/AccountsList/MailAccounts';
+import { Account } from '../../../logic/Abstract/Account';
 import { AuthMethod, TLSSocketType } from '../../../logic/Mail/MailAccount';
+import { OAuth2 } from '../../../logic/Auth/OAuth2';
+import { OAuth2URLs } from '../../../logic/Auth/OAuth2URLs';
 import { DummyMailStorage } from '../../../logic/Mail/Store/DummyMailStorage';
 import { kStandardPorts } from '../../../logic/Mail/AutoConfig/configInfo';
 import { getDomainForEmailAddress } from '../../../logic/util/netUtil';
 import { connectToBackend, stopBackend } from '../util/backend.test';
+import { assert } from '../../../logic/util/util';
 import { afterAll, beforeAll, expect, test } from 'vitest';
 
 beforeAll(connectToBackend);
@@ -19,10 +23,19 @@ test.each(kMailAccounts)("Test $protocol account $username", async (config) => {
   acc.name = config.username;
   acc.emailAddress = config.username;
   acc.userRealname = config.username;
-  acc.hostname = config.hostname ?? "imap." + getDomainForEmailAddress(acc.username);
+  let domain = getDomainForEmailAddress(acc.username);
+  acc.hostname = config.hostname ?? config.url ? new URL(config.url).hostname : "imap." + domain;
   acc.port = kStandardPorts.find(p => p.protocol == config.protocol && p.tls == TLSSocketType.TLS)?.port ?? 443;
   acc.url = config.url ?? "https://example.com";
-  acc.authMethod = AuthMethod.Password;
+  acc.authMethod = config.authMethod ?? AuthMethod.Password;
+  if (acc.authMethod == AuthMethod.OAuth2) {
+    let urls = OAuth2URLs.find(o => o.hostnames.includes(acc.hostname ?? "") || o.domains.includes(domain));
+    assert(urls, "Need OAuth2 config");
+    acc.oAuth2 = new OAuth2(acc as any as Account, urls.tokenURL, urls.authURL, urls.authDoneURL, urls.scope, urls.clientID, urls.clientSecret, urls.doPKCE);
+    if (config.accessToken) {
+      acc.oAuth2.accessToken = config.accessToken;
+    }
+  }
   acc.storage = new DummyMailStorage();
   acc.contentStorage.clear();
 
