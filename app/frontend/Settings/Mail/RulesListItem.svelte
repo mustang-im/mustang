91dc1a3daf2182b9dc73d64790fc2b5b54dfd29f
Ben Bucksch <ben.bucksch@beonex.com>
2025-02-17 08:38:08 +0100

    Filter Rules: Settings UI fixes - missing file

---
+++ app/frontend/Settings/Mail/RulesListItem.svelte
@@ -0,0 +1,10 @@
+<hbox class="rule-item">{$rule.name}</hbox>
+
+<script lang="ts">
+  import { FilterRuleAction } from "../../../logic/Mail/FilterRules/FilterRuleAction";
+
+  export let rule: FilterRuleAction;
+</script>
+
+<style>
+</style>
