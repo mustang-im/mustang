b331e18517fa2b3f517bfcc979ac2c472cacf39c
Ben Bucksch <ben.bucksch@beonex.com>
2025-03-13 05:29:34 +0100

    Starting version 0.8.10-dev

--- app/logic/build.ts
+++ app/logic/build.ts
@@ -1,7 +1,7 @@
 /** Quick and dirty way to change the build. */
 /* Changed by ../build/parula-brand.sh . That's also why we use '' quotes here. */
 export const production = false;
-export const appVersion: string = '0.8.9';
+export const appVersion: string = '0.8.10-dev';
 export const appName: string = 'Mustang';
 export const siteRoot: string = 'https://mustang.im';
 export const webMail = false;
