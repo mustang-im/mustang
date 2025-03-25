61d021f1b8d8790efe213964d3c13444e585c216
Ben Bucksch <ben.bucksch@beonex.com>
2025-03-25 03:23:57 +0100

    Mail: Add quick search for a word within the current folder / msg list only

--- app/frontend/Mail/LeftPane/FolderFooter.svelte
+++ app/frontend/Mail/LeftPane/FolderFooter.svelte
@@ -1,10 +1,10 @@
 <!-- Appears below the msg list -->
  {#if folder}
   <hbox class="folder-header">
+    {#if $account?.isLoggedIn}
+      <GetMailButton {folder} />
+    {/if}
     <hbox class="buttons">
-      {#if $account?.isLoggedIn}
-        <GetMailButton {folder} />
-      {/if}
       <hbox class="star button" class:starred={isShowStarred}>
         <Button
           icon={StarIcon}
@@ -25,30 +25,58 @@
           plain
           />
       </hbox>
-    </hbox>
-    <hbox flex />
-    <hbox class="msg-count">
-      {#if searchMessages}
-        {#if $searchMessages.hasItems}
-          {$t`${$searchMessages.length} search results`}
-        {:else}
-          {$t`No search results`}
-        {/if}
-      {:else}
-        {#if $folder.countTotal > 0}
-          {$t`${$folder.countNewArrived} new,
-              ${$folder.countUnread} unread,
-              ${$folder.countTotal} total`}
-        {/if}
-        {#if $folder.countTotal > 0 && $folder.countTotal != $messages.length}
-          {#if $messages.length == 0}
-            {$t`Loading...`}
-          {:else}
-            {$t`${$messages.length} displayed.`}
-          {/if}
-        {/if}
+      {#if !isShowSearchField}
+        <hbox class="show-search button">
+          <Button
+            icon={SearchIcon}
+            iconSize="16px"
+            iconOnly
+            label={$t`Search only the currently displayed folder for a keyword`}
+            onClick={toggleShowSearchField}
+            plain
+            />
+        </hbox>
       {/if}
     </hbox>
+    {#if isShowSearchField}
+      <SearchField bind:searchTerm
+        on:input={() => catchErrors(onSearchTermChanged)}
+        on:clear={toggleShowSearchField}
+        placeholder={$t`Search only this folder`}
+        showX={true}
+        autofocus={true} />
+      <hbox flex />
+      {#if searchMessages && $searchMessages?.hasItems}
+        <hbox class="msg-count">
+          {$searchMessages.length}
+        </hbox>
+      {/if}
+      <hbox flex />
+    {:else}
+      <hbox flex />
+      <hbox class="msg-count">
+        {#if searchMessages}
+          {#if $searchMessages.hasItems}
+            {$t`${$searchMessages.length} search results`}
+          {:else}
+            {$t`No search results`}
+          {/if}
+        {:else}
+          {#if $folder.countTotal > 0}
+            {$t`${$folder.countNewArrived} new,
+                ${$folder.countUnread} unread,
+                ${$folder.countTotal} total`}
+          {/if}
+          {#if $folder.countTotal > 0 && $folder.countTotal != $messages.length}
+            {#if $messages.length == 0}
+              {$t`Loading...`}
+            {:else}
+              {$t`${$messages.length} displayed.`}
+            {/if}
+          {/if}
+        {/if}
+      </hbox>
+    {/if}
     <hbox flex />
   </hbox>
 {/if}
@@ -57,10 +85,13 @@
   import type { Folder } from '../../../logic/Mail/Folder';
   import type { EMail } from '../../../logic/Mail/EMail';
   import { newSearchEMail } from '../../../logic/Mail/Store/setStorage';
+  import SearchField from '../../Shared/SearchField.svelte';
   import GetMailButton from './GetMailButton.svelte';
   import Button from '../../Shared/Button.svelte';
+  import SearchIcon from "lucide-svelte/icons/search";
   import StarIcon from "lucide-svelte/icons/star";
   import CircleIcon from "lucide-svelte/icons/circle";
+  import { catchErrors } from '../../Util/error';
   import type { ArrayColl } from 'svelte-collections';
   import { t } from '../../../l10n/l10n';
 
@@ -69,6 +100,19 @@
 
   $: account = folder?.account;
   $: messages = folder?.messages;
+
+  let searchTerm: string;
+  async function onSearchTermChanged() {
+    await startSearch();
+  }
+
+  let isShowSearchField = false;
+  async function toggleShowSearchField() {
+    isShowSearchField = !isShowSearchField;
+    if (!isShowSearchField) {
+      clearSearch();
+    }
+  }
 
   let isShowStarred = false;
   async function toggleStarred() {
@@ -84,21 +128,18 @@
 
   /** Filters the folder.messages array */
   async function startSearch() {
-    if (!isShowStarred && !isShowUnread) {
+    if (!isShowStarred && !isShowUnread && !searchTerm) {
       searchMessages = null;
       return;
     }
-    let search = newSearchEMail();
-    if (isShowStarred) { // undefined != false
-      search.isStarred = true;
-    }
-    if (isShowUnread) {
-      search.isRead = false;
-    }
-    search.folder = folder;
     searchMessages = await folder.messages.filter(msg =>
       (!isShowStarred || msg.isStarred === true) &&
-      (!isShowUnread || msg.isRead === false)
+      (!isShowUnread || msg.isRead === false) &&
+      (!searchTerm || searchTerm.length > 1) &&
+      (!searchTerm || msg.subject?.toLowerCase().includes(searchTerm) ||
+        msg.from?.name?.toLowerCase().includes(searchTerm) ||
+        msg.to?.some(to => to.name?.toLowerCase().includes(searchTerm)) ||
+        msg.rawText?.toLowerCase().includes(searchTerm))
     );
   }
 
@@ -117,6 +158,14 @@
     }
     search.folder = folder;
     searchMessages = await search.startSearch();
+  }
+
+  function clearSearch() {
+    searchTerm = undefined;
+    isShowSearchField = false;
+    isShowStarred = false;
+    isShowUnread = false;
+    searchMessages = null;
   }
 </script>
 
@@ -138,6 +187,13 @@
     height: 20px;
     width: 20px;
     border: none;
+  }
+  .folder-header :global(.search) {
+    height: 18px;
+    margin-inline-start: 8px;
+  }
+  .folder-header :global(input[type="search"]) {
+    height: 18px;
   }
   .msg-count {
     padding-inline-start: 4px;
