e153efaa85df32a8f3e703b5889f072fd2f0722b
Ben Bucksch <ben.bucksch@beonex.com>
2025-03-26 06:51:48 +0100

    UI: AccountDropDown: Icon-only mode

--- app/frontend/Shared/AccountDropDown.svelte
+++ app/frontend/Shared/AccountDropDown.svelte
@@ -1,5 +1,10 @@
 <hbox class="account-selector">
-  <select bind:value={selectedAccount}>
+  {#if withIcon}
+    <hbox class="icon" style="--account-color: {$selectedAccount?.color ?? "black"}">
+      <AccountIcon />
+    </hbox>
+  {/if}
+  <select bind:value={selectedAccount} class:withLabel>
     {#each $accounts.each as account }
       <option value={account}>
         {account.name}
@@ -11,9 +16,12 @@
 <script lang="ts">
   import type { Account } from "../../logic/Abstract/Account";
   import type { Collection } from "svelte-collections";
+  import AccountIcon from "lucide-svelte/icons/rabbit";
 
   export let selectedAccount: Account; /* in/out */
   export let accounts: Collection<Account>;
+  export let withIcon: boolean = false;
+  export let withLabel: boolean = true;
 
   $: defaultSelection(selectedAccount);
   function defaultSelection(_dummy: any) {
@@ -31,9 +39,19 @@
 </script>
 
 <style>
-  .account-selector :global(select) {
+  .account-selector {
+    align-items: center;
+  }
+  select {
     border: none;
     color: inherit;
     background-color: transparent;
+  }
+  select:not(.withLabel) {
+    width: 16px;
+    height: 16px;
+  }
+  .account-selector .icon {
+    color: var(--account-color);
   }
 </style>
