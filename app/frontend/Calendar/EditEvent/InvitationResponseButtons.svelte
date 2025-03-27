a31b83368dab32cdc7942b5d447e64ceabd9c00d
Ben Bucksch <ben.bucksch@beonex.com>
2025-03-27 03:29:54 +0100

    Invitation: Respond buttons in event in calendar: Fix async, Fix labels

--- app/frontend/Calendar/EditEvent/InvitationResponseButtons.svelte
+++ app/frontend/Calendar/EditEvent/InvitationResponseButtons.svelte
@@ -2,9 +2,9 @@
   {#if event.response == ResponseType.Unknown || event.response == ResponseType.Organizer}
     <Button label={$t`Accept`} onClick={onAccept} />
     <hbox class="spacer" />
-    <Button label={$t`Decline`} onClick={onDecline} />
+    <Button label={$t`Reject`} onClick={onDecline} />
     <hbox class="spacer" />
-    <Button label={$t`Tentative`} onClick={onTentative} />
+    <Button label={$t`Maybe`} onClick={onTentative} />
   {/if}
 </hbox>
 
@@ -12,24 +12,20 @@
   import type { Event } from "../../../logic/Calendar/Event";
   import { ResponseType, type Responses } from "../../../logic/Calendar/Invitation";
   import Button from "../../Shared/Button.svelte";
-  import { backgroundError } from "../../Util/error";
   import { t } from "../../../l10n/l10n";
 
   export let event: Event;
 
-  function respond(response: Responses) {
-    event.respondToInvitation(response).catch(backgroundError);
+  async function respond(response: Responses) {
+    await event.respondToInvitation(response);
   }
-  function onAccept() {
-    respond(ResponseType.Accept);
+  async function onAccept() {
+    await respond(ResponseType.Accept);
   }
-  function onTentative() {
-    respond(ResponseType.Tentative);
+  async function onTentative() {
+    await respond(ResponseType.Tentative);
   }
-  function onDecline() {
-    respond(ResponseType.Decline);
+  async function onDecline() {
+    await respond(ResponseType.Decline);
   }
 </script>
-
-<style>
-</style>
