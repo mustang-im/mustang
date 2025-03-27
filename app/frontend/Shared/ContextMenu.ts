76f7cce057c4ea1debbd0a90bbc3e23f5c98b110
Jeremy Chang <jeremy.chang@beonex.com>
2025-03-27 00:43:09 -0700

    Context Menu: Fix Save image (#508) (#515)

--- app/frontend/Shared/ContextMenu.ts
+++ app/frontend/Shared/ContextMenu.ts
@@ -1,7 +1,8 @@
 import { ArrayColl } from "svelte-collections";
-import { NotImplemented, type URLString } from "../../logic/util/util";
+import { blobToDataURL, NotImplemented, type URLString } from "../../logic/util/util";
 import { gt } from "../../l10n/l10n";
 import { appGlobal } from "../../logic/app";
+import { sanitize } from "../../../lib/util/sanitizeDatatypes";
 
 /**
  * Handles the Electron `<webview>` `"context-menu"` event.
@@ -64,8 +65,7 @@ export async function buildContextMenu(context: ContextInfo, win: any): Promise<
   if (context.mediaType == "image") {
     add("copyImage", gt`Copy image`, null, copyImage);
     add("copyImageURL", gt`Copy image address`, null, copyImageURL);
-    add("saveImage", gt`Save image`, null, saveImage);
-    add("saveImageAs", gt`Save image as…`, null, saveImageAs);
+    add("saveImage", gt`Save image...`, null, saveImage);
   }
   if (context.mediaType == "video") {
     add("copyVideoURL", gt`Copy video address`, null, copyVideoURL);
@@ -180,50 +180,46 @@ export interface ContextInfo {
   };
 }
 
-export function copyText(context: ContextInfo, win: any) {
+function copyText(context: ContextInfo, win: any) {
   win.copy();
   // or: copyToClipboard(context.selectionText);
 }
 
-export function cutText(context: ContextInfo, win: any) {
+function cutText(context: ContextInfo, win: any) {
   win.cut();
 }
 
-export function pasteText(context: ContextInfo, win: any) {
+function pasteText(context: ContextInfo, win: any) {
   win.paste();
 }
 
-export function selectAll(context: ContextInfo, win: any) {
+function selectAll(context: ContextInfo, win: any) {
   win.selectAll();
 }
 
-export function searchWeb(context: ContextInfo, win: any) {
+function searchWeb(context: ContextInfo, win: any) {
   const url = new URL('https://www.google.com/search');
   url.searchParams.set('q', context.selectionText);
   openBrowser(url.toString());
 }
 
-export function lookUpSelection(context: ContextInfo, win: any) {
+function lookUpSelection(context: ContextInfo, win: any) {
   win.showDefinitionForSelection();
 }
 
-export function saveImage(context: ContextInfo, win: any) {
+function saveImage(context: ContextInfo, win: any) {
   download(context.srcURL, win, false);
 }
 
-export function saveImageAs(context: ContextInfo, win: any) {
-  download(context.srcURL, win, true);
-}
-
-export function saveVideo(context: ContextInfo, win: any) {
+function saveVideo(context: ContextInfo, win: any) {
   download(context.srcURL, win, false);
 }
 
-export function saveVideoAs(context: ContextInfo, win: any) {
+function saveVideoAs(context: ContextInfo, win: any) {
   download(context.srcURL, win, true);
 }
 
-export async function copyLink(context: ContextInfo, win: any) {
+async function copyLink(context: ContextInfo, win: any) {
   await copyToClipboard(context.linkURL);
   /* or:
   copyToClipboard({
@@ -233,35 +229,35 @@ export async function copyLink(context: ContextInfo, win: any) {
   */
 }
 
-export function saveLinkAs(context: ContextInfo, win: any) {
+function saveLinkAs(context: ContextInfo, win: any) {
   download(context.linkURL, win, true, context.suggestedFilename);
 }
 
-export async function copyImage(context: ContextInfo, win: any) {
+async function copyImage(context: ContextInfo, win: any) {
   win = await appGlobal.remoteApp.getWebContents(win.getWebContentsId());
   win.copyImageAt(context.x, context.y);
 }
 
-export async function copyImageURL(context: ContextInfo, win: any) {
+async function copyImageURL(context: ContextInfo, win: any) {
   await copyToClipboard(context.srcURL);
 }
 
-export async function copyVideoURL(context: ContextInfo, win: any) {
+async function copyVideoURL(context: ContextInfo, win: any) {
   await copyToClipboard(context.srcURL);
 }
 
-export function learnSpelling(context: ContextInfo, win: any) {
+function learnSpelling(context: ContextInfo, win: any) {
   win.session.addWordToSpellCheckerDictionary(context.misspelledWord);
 }
 
-export function inspect(context: ContextInfo, win: any) {
+function inspect(context: ContextInfo, win: any) {
   win.inspectElement(context.x, context.y);
   if (win.isDevToolsOpened()) {
     win.devToolsWebContents.focus();
   }
 }
 
-export async function copyToClipboard(text: string | Object) {
+async function copyToClipboard(text: string | Object) {
   if (typeof (text) == "string") {
     await appGlobal.remoteApp.writeTextToClipboard(text);
   } else {
@@ -269,17 +265,22 @@ export async function copyToClipboard(text: string | Object) {
   }
 }
 
-export async function openBrowser(url: URLString) {
+async function openBrowser(url: URLString) {
   await appGlobal.remoteApp.openExternalURL(url);
 }
 
-export function download(url: URLString, window: any, howSaveAsDialog: boolean, filename?: string) {
+async function download(url: URLString, window: any, howSaveAsDialog: boolean, filename?: string) {
+  let urlObj = new URL(url);
+  if (urlObj.protocol == "http:" || urlObj.protocol == "https:") {
+    let blob = await (await fetch(url)).blob();
+    url = await blobToDataURL(blob);
+    filename = filename ?? urlObj.pathname.split("/").pop();
+  }
+
   let a = document.createElement("a");
   a.href = url;
   a.setAttribute("target", "_blank"); // for non-same-origin images
-  if (filename) {
-    a.setAttribute("download", filename); // TODO sanitize filename
-  }
+  a.setAttribute("download", sanitize.filename(filename, ""));
   a.click();
   a.href = "";
 }
