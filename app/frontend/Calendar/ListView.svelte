3cc9f927518715e080951eb7c7d52e8a4a28cd60
Ben Bucksch <ben.bucksch@beonex.com>
2025-04-02 19:30:13 +0200

    Calendar: Make Search results clearer

--- app/frontend/Calendar/ListView.svelte
+++ app/frontend/Calendar/ListView.svelte
@@ -3,13 +3,19 @@
   <hbox flex />
   <slot name="top-right" />
 </hbox>
+
+{#if $globalSearchTerm}
+  <vbox class="search">
+    <hbox>
+      <SearchField bind:searchTerm={$globalSearchTerm} />
+    </hbox>
+    <hbox class="result-count">{$t`Found ${filteredEvents.length} events`}</hbox>
+  </vbox>
+{/if}
+
 <vbox flex class="list-view">
   <FastList items={filteredEvents}
     columns="auto 1fr">
-    <svelte:fragment slot="header">
-      <hbox class="time">{$t`Time`}</hbox>
-      <hbox>{$t`Title`}</hbox>
-    </svelte:fragment>
     <EventLine slot="row" let:item event={item} />
   </FastList>
 </vbox>
@@ -18,6 +24,7 @@
   import type { Event } from "../../logic/Calendar/Event";
   import EventLine from "./MonthView/EventLine.svelte";
   import FastList from "../Shared/FastList.svelte";
+  import SearchField from "../Shared/SearchField.svelte";
   import type { Collection } from "svelte-collections";
   import { globalSearchTerm } from "../AppsBar/selectedApp";
   import { t } from "../../l10n/l10n";
@@ -31,7 +38,24 @@
 </script>
 
 <style>
+  .search {
+    margin-block-start: 24px;
+    margin-block-end: 18px;
+    margin-inline-start: 12px;
+  }
+  .result-count {
+    margin-block-start: 8px;
+    margin-inline-start: 4px;
+    font-size: 15px;
+    opacity: 80%;
+  }
+  .list-view {
+    margin-inline-start: 12px;
+  }
   .list-view :global(.event) {
     display: contents;
+  }
+  .list-view :global(.title) {
+    margin-inline-start: 12px;
   }
 </style>
