ad3fef9c3c77c469beebd83bd206de3dd0d1d7d4
Ben Bucksch <ben.bucksch@beonex.com>
2025-04-03 02:43:30 +0200

    Calendar: WeekView: Multi-day events - Forgot to add file

---
+++ app/frontend/Calendar/DayView/AllDayEvent.svelte
@@ -0,0 +1,55 @@
+<hbox class="event" on:click on:click={onSelect} on:dblclick={onOpen}
+  title={eventAsText}
+  style="--account-color: {event.calendar?.color}"
+  class:selected={$selectedEvent == event}>
+  <hbox class="title">{event.title}</hbox>
+</hbox>
+
+<script lang="ts">
+  import type { Event } from "../../../logic/Calendar/Event";
+  import { calendarMustangApp } from "../CalendarMustangApp";
+  import { selectedEvent } from "../selected";
+
+  export let event: Event;
+  /** Time where the cell (not the event) starts */
+  export let start: Date;
+
+  $: eventAsText = event.title;
+
+  function onSelect() {
+    $selectedEvent = event;
+  }
+
+  function onOpen() {
+    $selectedEvent = event;
+    calendarMustangApp.editEvent(event);
+  }
+</script>
+
+<style>
+  .event {
+    position: relative;
+    padding: 4px;
+    margin: 2px 0px;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    font-size: 14px;
+
+    background-color: var(--account-color);
+    color: lch(from var(--account-color) calc((49.44 - l) * infinity) 0 0);
+  }
+  @media (prefers-color-scheme: dark) {
+    .event {
+      background-image:
+        linear-gradient(var(--account-color), var(--account-color)),
+        linear-gradient(#000000BB, #000000BB);
+      background-blend-mode: overlay;
+      background-color: unset;
+      color: unset;
+    }
+  }
+  .event:hover {
+    background-color: #20AF9E70;
+  }
+</style>
