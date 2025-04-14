5efe3c0083fe9fb05610750dcfdd7385cb160de9
Ben Bucksch <ben.bucksch@beonex.com>
2025-04-14 23:54:08 +0200

    App menu: Go to mail account

--- app/frontend/AppsBar/AppMenuM/Apps/MailAppMenu.svelte
+++ app/frontend/AppsBar/AppMenuM/Apps/MailAppMenu.svelte
@@ -1,11 +1,14 @@
-<AccountButton account={appGlobal.emailAccounts.get(1)} page={acc => `/mail/folder/${acc.id}/${acc.inbox.id}/message-list`} defaultIcon={AccountIcon} />
-<AccountButton account={appGlobal.emailAccounts.get(0)} page={acc => `/mail/folder/${acc.id}/${acc.inbox.id}/message-list`} defaultIcon={AccountIcon} />
+<AccountButton account={appGlobal.emailAccounts.get(1)} {goToAccount} defaultIcon={AccountIcon} />
+<AccountButton account={appGlobal.emailAccounts.get(0)} {goToAccount} defaultIcon={AccountIcon} />
 <AppButton app={mailMustangApp} page="/mail/" />
 <CombinedButton icon1={mailMustangApp.icon} icon2={SearchIcon} page="/mail/search" />
 <CombinedButton icon1={mailMustangApp.icon} icon2={PencilIcon} page="/mail/compose" />
 
 <script lang="ts">
+  import { MailAccount } from "../../../../logic/Mail/MailAccount";
   import { mailMustangApp } from "../../../Mail/MailMustangApp";
+  import { selectedAccount, selectedFolder } from "../../../Mail/Selected";
+  import { goTo } from "../../selectedApp";
   import { appGlobal } from "../../../../logic/app";
   import AccountButton from "../AccountButton.svelte";
   import AppButton from "../AppButton.svelte";
@@ -13,4 +16,10 @@
   import SearchIcon from "lucide-svelte/icons/search";
   import PencilIcon from "lucide-svelte/icons/pencil";
   import AccountIcon from "lucide-svelte/icons/inbox";
+
+  function goToAccount(account: MailAccount) {
+    $selectedAccount = account;
+    $selectedFolder = account.inbox;
+    goTo(`/mail/folder/${account.id}/${account.inbox.id}/message-list`);
+  }
 </script>
