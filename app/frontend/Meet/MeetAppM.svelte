83af57b61620b84523e1ecfae034d9d4c264d93f
Ben Bucksch <ben.bucksch@beonex.com>
2025-04-16 04:01:13 +0200

    Meet app

--- app/frontend/Meet/MeetAppM.svelte
+++ app/frontend/Meet/MeetAppM.svelte
@@ -1,18 +1,12 @@
-{#if meeting && $meeting.state == MeetingState.Ongoing}
-  <InMeeting {meeting} />
-{:else if meeting && $meeting.state != MeetingState.Ended}
-  <Calling {meeting} />
-{:else}
-  <StartScreen />
-{/if}
+<Route path="history">
+  <CallHistory />
+</Route>
+<Route path="/">
+  <Main />
+</Route>
 
 <script lang="ts">
-  import { MeetingState } from "../../logic/Meet/VideoConfMeeting";
-  import { appGlobal } from "../../logic/app";
-  import InMeeting from "./InMeeting.svelte";
-  import Calling from "./Start/Calling.svelte";
-  import StartScreen from "./Start/StartScreen.svelte";
-
-  $: meetings = appGlobal.meetings;
-  $: meeting = $meetings.first;
+  import Main from "./Main.svelte";
+  import CallHistory from "./Start/CallHistory.svelte";
+  import { Route } from "svelte-navigator";
 </script>
