febf816d0738f2b1c38ead11368b7d50a05c6c8f
Ben Bucksch <ben.bucksch@beonex.com>
2025-04-20 22:32:53 +0200

    SQL: Migrate to accounts DB - untested

--- app/logic/Mail/SQL/SQLDatabase.ts
+++ app/logic/Mail/SQL/SQLDatabase.ts
@@ -1,6 +1,10 @@
 import { appGlobal } from "../../app";
 import sql, { type Database } from "../../../../lib/rs-sqlite/index";
 import { mailDatabaseSchema } from "./createDatabase";
+import { newAccountForProtocol } from "../AccountsList/MailAccounts";
+import { SQLMailAccount } from "./SQLMailAccount";
+import type { MailAccount } from "../MailAccount";
+import { SQLMailStorage } from "./SQLMailStorage";
 
 let mailDatabase: Database;
 
@@ -17,6 +21,7 @@ export async function getDatabase(): Promise<Database> {
   const getDatabase = appGlobal.remoteApp.getSQLiteDatabase;
   mailDatabase = await getDatabase("mail.db");
   await mailDatabase.migrate(mailDatabaseSchema);
+  await migrateToAccountsDB();
   await mailDatabase.pragma('foreign_keys = true');
   await mailDatabase.pragma('journal_mode = DELETE');
   return mailDatabase;
@@ -43,4 +48,42 @@ async function deleteDatabase(): Promise<void> {
     await mailDatabase.execute(sql`DROP TABLE IF EXISTS ${table};`);
   }
   await mailDatabase.pragma('user_version = 0');
+  mailDatabase = null;
+}
+
+async function migrateToAccountsDB(): Promise<void> {
+  let rows = await mailDatabase.all(sql`
+    SELECT
+      idStr, name, protocol, emailAddress,
+      username,
+      hostname, port, tls, authMethod, url,
+      outgoingAccountID,
+      userRealname, workspace, configJSON
+    FROM emailAccount
+    `) as any[];
+  if (!rows?.length) {
+    return;
+  }
+  rows = rows.filter(row => row.hostname || row.url);
+  if (!rows.length) {
+    return;
+  }
+  let accounts = rows.map(migrateAccount);
+
+  await deleteDatabase();
+  await getDatabase();
+
+  for (let account of accounts) {
+    account.storage = new SQLMailStorage();
+    await account.save();
+  }
+}
+
+function migrateAccount(row: any): MailAccount {
+  let account = newAccountForProtocol(row.protocol);
+  let json = row.configJSON;
+  Object.assign(json, row);
+  json.id = row.idStr;
+  account.fromConfigJSON(json);
+  return account;
 }
