3d9af55ae60dbe565138fdaf81fda71a66666668
Ben Bucksch <ben.bucksch@beonex.com>
2025-04-23 00:06:03 +0200

    POP3: Save sent msg

--- app/logic/Mail/POP3/POP3Account.ts
+++ app/logic/Mail/POP3/POP3Account.ts
@@ -1,6 +1,7 @@
 import { MailAccount } from "../MailAccount";
 import { POP3Folder } from "./POP3Folder";
 import type { EMail } from "../EMail";
+import { SpecialFolder } from "../Folder";
 import { assert, NotImplemented } from "../../util/util";
 
 export class POP3Account extends MailAccount {
@@ -14,7 +15,13 @@ export class POP3Account extends MailAccount {
   async send(email: EMail): Promise<void> {
     assert(this.outgoing, "SMTP server is not set up for POP3 account " + this.name);
     await this.outgoing.send(email);
+    await this.saveSent(email);
   };
+
+  protected async saveSent(email: EMail): Promise<void> {
+    let sentFolder = email.folder ?? this.getSpecialFolder(SpecialFolder.Sent);
+    await sentFolder.addMessage(email);
+  }
 
   newFolder(): POP3Folder {
     return new POP3Folder(this);
