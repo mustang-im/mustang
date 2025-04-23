15f4f59d371ddaa0b3f66944090a810d841ca808
Ben Bucksch <ben.bucksch@beonex.com>
2025-04-23 05:12:21 +0200

    Main window: Never larger than the screen - untested - fixes #577

--- app/frontend/main.ts
+++ app/frontend/main.ts
@@ -26,7 +26,9 @@ function loadWindowSettings() {
   let windowSize = getLocalStorage("window.size", [window.outerWidth, window.outerHeight]).value;
   let windowPosition = getLocalStorage("window.position", [window.screenX, window.screenY]).value;
   assert(windowSize.length == 2 && windowSize.every(i => sanitize.integer(i)), gt`Bad window size` + windowSize);
-  assert(windowPosition.length == 2 && windowPosition.every(i => sanitize.integer(i)),  gt`Bad window position` + windowPosition);
+  assert(windowPosition.length == 2 && windowPosition.every(i => sanitize.integer(i)), gt`Bad window position` + windowPosition);
+  windowSize[0] = Math.min(windowSize[0], screen.width);
+  windowSize[1] = Math.min(windowSize[1], screen.height);
   window.resizeTo(windowSize[0], windowSize[1]);
   window.moveTo(windowPosition[0], windowPosition[1]);
 }
