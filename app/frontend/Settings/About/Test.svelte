8266eb9916fdbe00bb7b5c3ec40fa099324ef9fd
Ben Bucksch <ben.bucksch@beonex.com>
2025-04-23 07:27:56 +0200

    About: Test: Show loading message

--- app/frontend/Settings/About/Test.svelte
+++ app/frontend/Settings/About/Test.svelte
@@ -1,18 +1,36 @@
 <hbox class="buttons">
-  <Button label={$t`Load test data`}
-    onClick={loadTest}
-    />
+  {#if isLoading}
+    {$t`Loading artificial test data for demo purposes. This may take a moment...`}
+  {:else}
+    <Button label={$t`Load test data`}
+      onClick={loadTest}
+      />
+  {/if}
 </hbox>
 
 <script lang="ts">
   import { getTestObjects } from "../../../logic/testData";
   import { openApp } from "../../AppsBar/selectedApp";
   import { chatMustangApp } from "../../Chat/ChatMustangApp";
+  import { appGlobal } from "../../../logic/app";
+  import { sleep } from "../../../logic/util/util";
   import Button from "../../Shared/Button.svelte";
   import { t } from "../../../l10n/l10n";
 
+  let isLoading = false;
+
   async function loadTest() {
+    isLoading = true;
+    appGlobal.emailAccounts.clear();
+    appGlobal.chatAccounts.clear();
+    appGlobal.meetAccounts.clear();
+    appGlobal.calendars.clear();
+    appGlobal.addressbooks.clear();
+    appGlobal.personalAddressbook = null;
+    appGlobal.collectedAddressbook = null;
+    await sleep(0.1);
     await getTestObjects();
+    isLoading = false;
     openApp(chatMustangApp);
   }
 </script>
