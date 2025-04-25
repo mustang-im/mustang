67adca68b241023c63f8b23604037206860413dd
Ben Bucksch <ben.bucksch@beonex.com>
2025-04-26 00:12:16 +0200

    Backend code compiled separately

--- mobile/backend/index.ts
+++ mobile/backend/index.ts
@@ -1,7 +1,8 @@
-import { startupBackend, shutdownBackend } from '../../../backend/backend';
+import { startupBackend, shutdownBackend } from '../../backend/backend';
 
 async function startup(): Promise<void> {
   try {
+    console.log("backend starting");
     await startupBackend();
   } catch (ex) {
     console.error(ex);
