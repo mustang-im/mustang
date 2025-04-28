3472219246e2c2e6ee374ed91d286d0bb3e77564
Neil Rashbrook <neil.rashbrook@beonex.com>
2025-04-28 21:32:20 +0100

    Implement `OutgoingInvitation.identity`

--- app/logic/Calendar/ActiveSync/ActiveSyncOutgoingInvitation.ts
+++ app/logic/Calendar/ActiveSync/ActiveSyncOutgoingInvitation.ts
@@ -1,6 +1,11 @@
 import type { ActiveSyncEvent } from "./ActiveSyncEvent";
 import OutgoingInvitation from "../Invitation/OutgoingInvitation";
+import type { MailIdentity } from "../../Mail/MailIdentity";
 
 export default class ActiveSyncOutgoingInvitation extends OutgoingInvitation {
   event: ActiveSyncEvent;
+
+  get identity(): MailIdentity {
+    return this.event.calendar.account.identities.first;
+  }
 }
--- app/logic/Calendar/Invitation/OutgoingInvitation.ts
+++ app/logic/Calendar/Invitation/OutgoingInvitation.ts
@@ -2,13 +2,23 @@ import type { Event } from "../Event";
 import type { Participant } from "../Participant";
 import { InvitationResponse, type iCalMethod } from "./InvitationStatus";
 import type { MailAccount } from "../../Mail/MailAccount";
+import type { MailIdentity } from "../../Mail/MailIdentity";
 import type { PersonUID } from "../../Abstract/PersonUID";
+import { appGlobal } from "../../app";
+import { assert } from "../../util/util";
+import { gt } from "../../../l10n/l10n";
 
 export default class OutgoingInvitation {
   event: Event;
 
   constructor(event: Event) {
     this.event = event;
+  }
+
+  get identity(): MailIdentity {
+    let account = appGlobal.emailAccounts.find(account => account.canSendInvitations);
+    assert(account, gt`No suitable identity to organise this meeting`);
+    return account.identities.first;
   }
 
   async sendInvitations(account: MailAccount, from?: PersonUID) {
