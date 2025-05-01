6a885a270e5967c196631774f328a737a535c0cc
Ben Bucksch <ben.bucksch@beonex.com>
2025-05-02 01:22:39 +0200

    LiveKit: Video and audio are 2 tracks, but must be in the same MediaStream

--- app/logic/Meet/LiveKit/LiveKitRemoteParticipant.ts
+++ app/logic/Meet/LiveKit/LiveKitRemoteParticipant.ts
@@ -1,14 +1,16 @@
 import { MeetingParticipant, ParticipantRole } from "../Participant";
-import { ParticipantVideo, ScreenShare } from "../VideoStream";
+import { ParticipantVideo, ScreenShare, VideoStream } from "../VideoStream";
 import { sanitize } from "../../../../lib/util/sanitizeDatatypes";
 import { catchErrors } from "../../../frontend/Util/error";
 import { assert } from "../../util/util";
 import type { LiveKitConf } from "./LiveKitConf";
-import { ParticipantEvent, RemoteParticipant, Track } from "livekit-client";
+import { ParticipantEvent, RemoteParticipant, Track, TrackPublication } from "livekit-client";
 
 export class LiveKitRemoteParticipant extends MeetingParticipant {
   rp: RemoteParticipant;
   conf: LiveKitConf;
+  video: ParticipantVideo;
+  screenShare: ScreenShare;
 
   constructor(rp: RemoteParticipant, conf: LiveKitConf) {
     super();
@@ -22,11 +24,11 @@ export class LiveKitRemoteParticipant extends MeetingParticipant {
     rp.on(ParticipantEvent.ParticipantMetadataChanged, () => catchErrors(() => this.updateProps(), this.conf.errorCallback));
     rp.on(ParticipantEvent.TrackPublished, () => catchErrors(() => this.updateProps(), this.conf.errorCallback));
     rp.on(ParticipantEvent.TrackUnpublished, () => catchErrors(() => this.updateProps(), this.conf.errorCallback));
-    rp.on(ParticipantEvent.TrackMuted, () => catchErrors(() => this.updateProps(), this.conf.errorCallback));
-    rp.on(ParticipantEvent.TrackUnmuted, () => catchErrors(() => this.updateProps(), this.conf.errorCallback));
     rp.on(ParticipantEvent.IsSpeakingChanged, () => catchErrors(() => this.speakingChanged(), this.conf.errorCallback));
     rp.on(ParticipantEvent.TrackSubscribed, track => catchErrors(() => this.addTrack(track), this.conf.errorCallback));
     rp.on(ParticipantEvent.TrackUnsubscribed, track => catchErrors(() => this.removeTrack(track), this.conf.errorCallback));
+    rp.on(ParticipantEvent.TrackMuted, trackPub => catchErrors(() => this.trackMuted(trackPub), this.conf.errorCallback));
+    rp.on(ParticipantEvent.TrackUnmuted, trackPub => catchErrors(() => this.trackUnmuted(trackPub), this.conf.errorCallback));
 
     for (let trackPub of rp.trackPublications.values()) {
       if (!trackPub.isSubscribed || !trackPub.track) {
@@ -54,24 +56,55 @@ export class LiveKitRemoteParticipant extends MeetingParticipant {
   }
 
   async addTrack(track: Track) {
-    console.log("Participant", this.rp.identity, "added a track", track.mediaStream);
+    console.log("Participant", this.rp.identity, "added a track", track.mediaStream, track.mediaStream.getTracks());
     assert(track.mediaStream, "Need mediaStream for Track");
     let isScreen = track.source == Track.Source.ScreenShare || track.source == Track.Source.ScreenShareAudio;
-    let video = isScreen
-      ? new ScreenShare(track.mediaStream, this)
-      : new ParticipantVideo(track.mediaStream, this);
-    this.conf.videos.add(video);
+    let video: VideoStream;
+    if (isScreen) {
+      if (!this.screenShare) {
+        this.screenShare = new ScreenShare(new MediaStream(), this);
+        this.conf.videos.add(this.screenShare);
+      }
+      video = this.screenShare;
+    } else {
+      if (!this.video) {
+        this.video = new ParticipantVideo(new MediaStream(), this);
+        this.conf.videos.add(this.video);
+      }
+      video = this.video;
+    }
+
+    video.stream.addTrack(track.mediaStreamTrack);
   }
   async removeTrack(track: Track) {
     console.log("Participant", this.rp.identity, "removed a track");
     let isScreen = track.source == Track.Source.ScreenShare || track.source == Track.Source.ScreenShareAudio;
-    let video = this.conf.videos.find(v =>
-      (!isScreen && v instanceof ParticipantVideo ||
-        isScreen && v instanceof ScreenShare) &&
-      v.participant == this);
+    let video = isScreen ? this.screenShare : this.video;
     if (!video) {
       return;
     }
-    this.conf.videos.remove(video);
+
+    video.stream.removeTrack(track.mediaStreamTrack);
+
+    if (!video.stream.getTracks().length) {
+      this.conf.videos.remove(video);
+      if (isScreen) {
+        this.screenShare = null;
+      } else {
+        this.video = null;
+      }
+    }
+  }
+  async trackMuted(trackPub: TrackPublication) {
+    this.updateProps();
+    if (trackPub.videoTrack) {
+      this.removeTrack(trackPub.videoTrack);
+    }
+  }
+  async trackUnmuted(trackPub: TrackPublication) {
+    this.updateProps();
+    if (trackPub.videoTrack) {
+      this.addTrack(trackPub.videoTrack);
+    }
   }
 }
