2d68277ea30847b1531d078ce27c6d9fa68b7a98
Jeremy Chang <jeremy.chang@beonex.com>
2025-04-30 18:34:08 -0600

    Mobile Backend: Make capacitor-nodejs start an ESM module (#583) (#590)
    * Try to make capacitor-nodejs start an ESM module - breaks gradle build
    * Mobile: Vite: Copy backend code to webDir
    * Android: Allow cleartext communications

--- mobile/backend/vite.config.ts
+++ mobile/backend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import nodeExternals from 'rollup-plugin-node-externals';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   ssr: { noExternal: true },
@@ -7,12 +8,11 @@ export default defineConfig({
     target: 'node18',
     lib: {
       name: 'index',
-      fileName: () => 'indexs.js',
       formats: ['es'],
       entry: './index.ts'
     },
-    outDir: '../android/app/src/main/assets/public/nodejs',
-    emptyOutDir: false,
+    outDir: 'dist',
+    emptyOutDir: true,
     minify: false,
     ssr: true,
   },
@@ -20,6 +20,20 @@ export default defineConfig({
     nodeExternals({
       deps: false,
       devDeps: true, // Use node.js internal modules
-    })
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.js',
+          dest: '../../../app/dist/nodejs',
+          rename: 'index.mjs',
+        },
+        {
+          src: 'package-deploy.json',
+          dest: '../../../app/dist/nodejs',
+          rename: 'package.json',
+        },
+      ]
+    }),
   ],
 });
