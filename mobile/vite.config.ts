388266923fdaa2249a8c32fb35c74d142e81ceaa
Jeremy Chang <jeremy.chang@beonex.com>
2025-05-08 11:59:32 -0600

    Mobile: Vite build config

--- mobile/vite.config.ts
+++ mobile/vite.config.ts
@@ -1,11 +1,35 @@
 import { defineConfig } from 'vite';
+import conditionalCompile from 'vite-plugin-conditional-compile';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
+import { includeProprietary, production, webMail } from '../app/logic/build';
 
 export default defineConfig({
   root: './src',
-  publicDir: './njs',
+  publicDir: '../app/public',
   build: {
     outDir: '../dist',
     minify: false,
     emptyOutDir: true,
+    sourcemap: production,
   },
+  plugins: [
+    conditionalCompile({
+      // <https://github.com/LZS911/vite-plugin-conditional-compile/blob/master/README.md>
+      env: {
+        // For conditional `// #if [FOO]` statements in the code
+        WEBMAIL: webMail && includeProprietary ? webMail : undefined,
+        PROPRIETARY: includeProprietary ? true : undefined,
+      },
+    }),
+    nodePolyfills({include: ['buffer'], globals: {global: false, process: false}}),
+    svelte({configFile: '../svelte.config.mjs'}),
+    sentryVitePlugin({
+      org: "mustang-jq",
+      project: "mustang",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      disable: !production,
+    }),
+  ],
 });
