036c4802340f73f054b1876d87efaac2e6c38561
Jeremy Chang <jeremy.chang@beonex.com>
2025-05-09 13:37:53 -0600

    Mobile: Vite: Revert changes

--- mobile/vite.config.ts
+++ mobile/vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite';
-import conditionalCompile from 'vite-plugin-conditional-compile';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import { sentryVitePlugin } from '@sentry/vite-plugin';
-import { includeProprietary, production, webMail } from '../app/logic/build';
 
 export default defineConfig({
   root: './src',
-  publicDir: '../../app/public',
+  publicDir: './njs',
   build: {
     outDir: '../dist',
     minify: false,
     emptyOutDir: true,
-    sourcemap: production,
   },
 });
