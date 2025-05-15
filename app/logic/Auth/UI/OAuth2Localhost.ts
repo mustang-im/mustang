d574207e584619f3a92b6d06e1cf562f1b2735d1
Ben Bucksch <ben.bucksch@beonex.com>
2025-05-15 11:19:08 +0200

    OAuth2: Error to user when closing with 15 min timeout

--- app/logic/Auth/UI/OAuth2Localhost.ts
+++ app/logic/Auth/UI/OAuth2Localhost.ts
@@ -1,6 +1,8 @@
 import { OAuth2UI } from "./OAuth2UI";
 import { appGlobal } from "../../app";
-import { assert, type URLString } from "../../util/util";
+import { k1MinuteMS } from "../../../frontend/Util/date";
+import { assert, UserError, type URLString } from "../../util/util";
+import { gt } from "../../../l10n/l10n";
 
 /**
  * Starts a local web server on http://localhost with a random port,
@@ -21,15 +23,17 @@ export class OAuth2Localhost extends OAuth2UI {
     let port = 1024 + Math.ceil(Math.random() * 64000);
     let server = await appGlobal.remoteApp.newHTTPServer();
     await server.start(port);
-    // Close after 15 mins no result
-    let killTimeout = setTimeout(() => {
-      server.close();
-    }, 15 * 60 * 1000);
     let doneURL = `http://localhost:${port}/login-success`;
     let url = await this.oAuth2.getAuthURL(doneURL);
     // console.log("OAuth2: Localhost: Load URL", url, "and done URL", doneURL);
     await this.loginURLCallback(url);
     return new Promise((resolve, reject) => {
+      // Close after 15 mins no result
+      const minutes = 15;
+      let killTimeout = setTimeout(() => {
+        server.close();
+        reject(new UserError(gt`Authentication page timed out after ${minutes} minutes`));
+      }, minutes * k1MinuteMS);
       server.get("/login-success", (urlPath: URLString) => {
         try {
           // console.log("OAuth2: Login finished", url);
