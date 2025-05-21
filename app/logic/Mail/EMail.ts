4d91f4207f9b93bd9af7f3aa0ab3485bc705b81e
Ben Bucksch <ben.bucksch@beonex.com>
2025-05-21 18:14:40 +0200

    EMail: Fix empty emails displayed

--- app/logic/Mail/EMail.ts
+++ app/logic/Mail/EMail.ts
@@ -372,9 +372,6 @@ export class EMail extends Message {
   }
 
   async loadBody() {
-    if (this._sanitizedHTML) {
-      return;
-    }
     if (!this._rawHTML && !this._text) {
       if (this.dbID) {
         await this.storage.readMessageBody(this);
