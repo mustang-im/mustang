fac2dd88222e074cbeceaea04bc5ca04d2fcb523
Jeremy Chang <jeremy.chang@beonex.com>
2025-05-26 18:56:19 -0600

    Test: Calendar: ICalEMailProcessor: Fix test errors and TS errors (#638)
    Add missing descriptionText empty string

--- app/test/logic/Calendar/ICal/ICalEMailProcessor.test.ts
+++ app/test/logic/Calendar/ICal/ICalEMailProcessor.test.ts
@@ -1,5 +1,7 @@
 import { expect, test } from "vitest";
 import { appGlobal } from "../../../../logic/app.ts"; // defeats circular import
+import type { Event } from "../../../../logic/Calendar/Event.ts";
+import type { EMail } from "../../../../logic/Mail/EMail.ts";
 import { ICalEMailProcessor } from "../../../../logic/Calendar/ICal/ICalEMailProcessor";
 import * as fs from "node:fs/promises";
 
@@ -26,9 +28,9 @@ const allFiles = await fs.readdir(dataDir);
 const testFiles = allFiles.filter(name => name.endsWith(".ics")).map(name => name.slice(0, -4));
 test.each(testFiles)("Parse %s", async name => {
   const calendar = await fs.readFile(new URL(name + ".ics", dataDir), { encoding: 'utf-8' });
-  const [invitationMessage, event] = JSON.parse(await fs.readFile(new URL(name + ".json", dataDir, { encoding: 'utf-8' })));
+  const [invitationMessage, event] = JSON.parse(await fs.readFile(new URL(name + ".json", dataDir), { encoding: 'utf-8' }));
   const processor = new ICalEMailProcessor();
-  const email = {
+  const email: EMail = {
     attachments: [{
       mimeType: "text/calendar",
       content: {
@@ -40,5 +42,5 @@ test.each(testFiles)("Parse %s", async name => {
   };
   await processor.process(email, null);
   expect(email.invitationMessage).toEqual(invitationMessage);
-  expect(toJSON(email.event)).toEqual(event);
+  expect(toJSON(email.event as Event)).toEqual(event);
 });
