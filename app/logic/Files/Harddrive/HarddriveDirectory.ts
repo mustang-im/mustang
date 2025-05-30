7f4046616e363f29e180c8a106e5b94b94d7d52f
Ben Bucksch <ben.bucksch@beonex.com>
2025-05-30 12:41:31 +0200

    Files: Harddrive: Lock when listing dir

--- app/logic/Files/Harddrive/HarddriveDirectory.ts
+++ app/logic/Files/Harddrive/HarddriveDirectory.ts
@@ -1,14 +1,16 @@
 import { Directory } from "../Directory";
 import type { File } from "../File";
 import type { HarddriveAccount } from "./HarddriveAccount";
-import { AbstractFunction, assert } from "../../util/util";
-import type { ArrayColl, Collection } from "svelte-collections";
 import { appGlobal } from "../../app";
+import { Lock } from "../../util/Lock";
+import { AbstractFunction } from "../../util/util";
+import type { ArrayColl, Collection } from "svelte-collections";
 
 export class HarddriveDirectory extends Directory {
   declare account: HarddriveAccount;
   readonly files: ArrayColl<File>;
   readonly subDirs: ArrayColl<HarddriveDirectory>;
+  protected readonly listLock = new Lock();
 
   newDirectory(name: string): HarddriveDirectory {
     let dir = new HarddriveDirectory();
@@ -19,22 +21,27 @@ export class HarddriveDirectory extends Directory {
   }
 
   async listContents() {
-    this.files.clear();
-    this.subDirs.clear();
-    let entries = await appGlobal.remoteApp.listDirectoryContents(this.path, true, false);
-    for (let entry of entries) {
-      if (await entry.isDirectory) {
-        let dir = this.newDirectory(entry.name);
-        dir.path = dir.filepathLocal = entry.path;
-        dir.lastMod = entry.lastMod;
-        this.subDirs.add(dir);
-      } else {
-        let file = this.newFile(entry.name);
-        file.path = file.filepathLocal = entry.path;
-        file.size = entry.size;
-        file.lastMod = entry.lastMod;
-        this.files.add(file);
+    let lock = await this.listLock.lock();
+    try {
+      this.files.clear();
+      this.subDirs.clear();
+      let entries = await appGlobal.remoteApp.listDirectoryContents(this.path, true, false);
+      for (let entry of entries) {
+        if (await entry.isDirectory) {
+          let dir = this.newDirectory(entry.name);
+          dir.path = dir.filepathLocal = entry.path;
+          dir.lastMod = entry.lastMod;
+          this.subDirs.add(dir);
+        } else {
+          let file = this.newFile(entry.name);
+          file.path = file.filepathLocal = entry.path;
+          file.size = entry.size;
+          file.lastMod = entry.lastMod;
+          this.files.add(file);
+        }
       }
+    } finally {
+      lock.release();
     }
   }
 
