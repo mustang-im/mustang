0d5686c9c8f1e27920cab5c301dd4c6b5b7841f4
Jeremy Chang <jeremy.chang@beonex.com>
2025-06-06 11:44:19 -0600

    Change the MIME

--- app/logic/Mail/ComposeActions.ts
+++ app/logic/Mail/ComposeActions.ts
@@ -1,5 +1,6 @@
 import type { EMail } from "./EMail";
 import { SpecialFolder } from "./Folder";
+import { CreateMIME } from "./SMTP/CreateMIME";
 import { Attachment, ContentDisposition } from "../Abstract/Attachment";
 import { PersonUID } from "../Abstract/PersonUID";
 import { MailIdentity, findIdentityForEMailAddress } from "./MailIdentity";
@@ -257,6 +258,7 @@ export class ComposeActions {
     let previousDrafts = this.getDrafts();
 
     this.email.isDraft = true;
+    this.email.mime = await CreateMIME.getMIME(this.email);
     await draftFolder.addMessage(this.email);
 
     await this.deleteDrafts(previousDrafts);
