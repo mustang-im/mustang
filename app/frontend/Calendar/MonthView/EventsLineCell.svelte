010a1ca693d9d90e627f45d382f07c38249e90aa
Ben Bucksch <ben.bucksch@beonex.com>
2025-07-10 04:40:19 +0200

    Calendar: Month view: Avoid that events are on top of the menu

--- app/frontend/Calendar/MonthView/EventsLineCell.svelte
+++ app/frontend/Calendar/MonthView/EventsLineCell.svelte
@@ -64,7 +64,7 @@
     bottom: 0px;
     left: 0px;
     overflow: hidden;
-    z-index: 0;
+    z-index: -1;
 
     width: 100%;
     aspect-ratio: 1/1;
@@ -76,8 +76,5 @@
     letter-spacing: 0.378px;
     text-transform: uppercase;
     font-weight: 300;
-  }
-  .events :global(.event) {
-    z-index: 1;
   }
 </style>
