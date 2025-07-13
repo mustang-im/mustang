776b32f9fa1b4196e9f26b86fdd5eaadb083391a
Ben Bucksch <ben.bucksch@beonex.com>
2025-07-13 18:28:14 +0200

    Add code comment; Use NotReached

--- app/logic/Calendar/Invitation/InvitationEvent.ts
+++ app/logic/Calendar/Invitation/InvitationEvent.ts
@@ -1,25 +1,27 @@
 import { Event } from "../Event";
+import { NotReached } from "../../util/util";
 
+/** This is the event information included in an invitation email.
+ * This event is not in any calendar. To put the event in a calendar, you need
+ * to call calendar.newEvent()` to create a protocol-specific calendar event
+ * implementation and then clone into info from here into that calendar event. */
 export class InvitationEvent extends Event {
-  constructor() {
-    super();
-  }
   async save(): Promise<never> {
-    throw new Error("InvitationEvent holds a temporary copy of event data for incoming and outgoing invitations only");
+    throw new NotReached("InvitationEvent holds a temporary copy of event data for incoming and outgoing invitations only");
   }
   async saveLocally(): Promise<never> {
-    throw new Error("InvitationEvent holds a temporary copy of event data for incoming and outgoing invitations only");
+    throw new NotReached("InvitationEvent holds a temporary copy of event data for incoming and outgoing invitations only");
   }
   async saveToServer(): Promise<never> {
-    throw new Error("InvitationEvent holds a temporary copy of event data for incoming and outgoing invitations only");
+    throw new NotReached("InvitationEvent holds a temporary copy of event data for incoming and outgoing invitations only");
   }
   async deleteIt(): Promise<never> {
-    throw new Error("InvitationEvent holds a temporary copy of event data for incoming and outgoing invitations only");
+    throw new NotReached("InvitationEvent holds a temporary copy of event data for incoming and outgoing invitations only");
   }
   async deleteLocally(): Promise<never> {
-    throw new Error("InvitationEvent holds a temporary copy of event data for incoming and outgoing invitations only");
+    throw new NotReached("InvitationEvent holds a temporary copy of event data for incoming and outgoing invitations only");
   }
   async deleteFromServer(): Promise<never> {
-    throw new Error("InvitationEvent holds a temporary copy of event data for incoming and outgoing invitations only");
+    throw new NotReached("InvitationEvent holds a temporary copy of event data for incoming and outgoing invitations only");
   }
 }
