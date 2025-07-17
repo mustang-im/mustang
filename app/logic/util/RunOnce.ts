e1846fa18f62e8f9cdd5a666c28cdde26d3d47d0
Neil Rashbrook <neil.rashbrook@beonex.com>
2025-07-17 21:42:08 +0100

    OWA: Calendar, Contacts: Prevent slow functions from being reentered (#781)

---
+++ app/logic/util/RunOnce.ts
@@ -0,0 +1,26 @@
+/**
+ * Wrapper around a slow async function to prevent it from being re-entered.
+ * Call maybeRun() to attempt to invoke the async function.
+ * Unlike Semaphore, attempting re-entrancy does nothing.
+ * XXX need better name
+ */
+export class RunOnce {
+  func: () => void;
+  running: boolean = false;
+
+  constructor(func) {
+    this.func = func;
+  }
+
+  async maybeRun() {
+    if (this.running) {
+      return;
+    }
+    try {
+      this.running = true;
+      await this.func();
+    } finally {
+      this.running = false;
+    }
+  }
+}
