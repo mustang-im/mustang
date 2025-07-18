484cb1724ec7cddc32a98b2778b601c25e3b7833
Ben Bucksch <ben.bucksch@beonex.com>
2025-07-18 19:31:57 +0200

    Demo: Files: Person View

--- app/frontend/Files/selected.ts
+++ app/frontend/Files/selected.ts
@@ -1,13 +1,13 @@
 import type { FileOrDirectory } from "../../logic/Files/FileOrDirectory";
 import type { Directory } from "../../logic/Files/Directory";
 import type { FileSharingAccount } from "../../logic/Files/FileSharingAccount";
-import { production } from "../../logic/build";
+import { isDemo } from "../AppsBar/demo";
 import { FilesView } from "./LeftPane/PaneViewSwitcher.svelte";
-import { writable, type Writable } from "svelte/store";
+import { writable, type Writable, get } from "svelte/store";
 import { ArrayColl } from "svelte-collections";
 
 export let selectedFile: Writable<FileOrDirectory> = writable(null);
 export let selectedFiles = new ArrayColl<FileOrDirectory>();
 export let selectedFolder: Writable<Directory> = writable(null);
 export let selectedAccount: Writable<FileSharingAccount> = writable(null);
-export let selectedLeftTab: Writable<FilesView> = writable(production ? FilesView.Person : FilesView.Harddrive);
+export let selectedLeftTab: Writable<FilesView> = writable(get(isDemo) ? FilesView.Harddrive : FilesView.Person);
