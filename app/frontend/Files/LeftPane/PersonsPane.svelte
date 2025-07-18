b0ea1259cc12255dde8240033fd2fe62f6fc52d4
Ben Bucksch <ben.bucksch@beonex.com>
2025-07-18 19:32:14 +0200

    Files: Persons: Show only persons with email attachments

--- app/frontend/Files/LeftPane/PersonsPane.svelte
+++ app/frontend/Files/LeftPane/PersonsPane.svelte
@@ -1,24 +1,62 @@
 <vbox flex class="persons-file-pane">
+  {#await filterPersons()}
+    <hbox class="progress">{$t`Searching...`}</hbox>
+  {:then}
   <PersonsList {persons} bind:selected={$selectedPerson} size="small" />
+  {:catch ex}
+    <ErrorMessageInline {ex} />
+  {/await}
 </vbox>
 
 <script lang="ts">
   import { File } from "../../../logic/Files/File";
   import { Directory } from "../../../logic/Files/Directory";
-  import type { Person } from "../../../logic/Abstract/Person";
+  import { Person } from "../../../logic/Abstract/Person";
+  import { PersonUID } from "../../../logic/Abstract/PersonUID";
   import { newSearchEMail } from "../../../logic/Mail/Store/setStorage";
   import { selectedPerson } from "../../Contacts/Person/Selected";
-  import { appGlobal } from "../../../logic/app";
   import PersonsList from "../../Contacts/Person/PersonsList.svelte";
+  import ErrorMessageInline from "../../Shared/ErrorMessageInline.svelte";
   import { catchErrors } from "../../Util/error";
-  import { ArrayColl, Collection } from 'svelte-collections';
+  import { ArrayColl, Collection, MapColl, SetColl } from 'svelte-collections';
+  import { t } from "../../../l10n/l10n";
 
   /** The list of files and folders to show on the right pane
    * in/out only */
   export let listFiles: Collection<File>;
   export let listDirs: Collection<Directory>;
 
-  let persons = appGlobal.persons;
+  let persons = new SetColl<Person>();
+
+  // onMount(() => catchErrors(filterPersons));
+
+  /** Show only those persons that have files*/
+  async function filterPersons(): Promise<Collection<Person>> {
+    persons.clear();
+    let uids = new MapColl<string, PersonUID>();
+    let search = newSearchEMail();
+    search.hasAttachment = true;
+    let emails = await search.startSearch();
+    for (let email of emails) {
+      let hasAttachment = email.attachments.hasItems && email.attachments.some(a => !a.hidden);
+      if (!hasAttachment) {
+        continue;
+      }
+      if (email.contact instanceof Person) {
+        persons.add(email.contact);
+      } else if (email.contact instanceof PersonUID) {
+        uids.set(email.contact.emailAddress, email.contact);
+      }
+    }
+    for (let uid of uids.contents) {
+      let person = uid.findPerson();
+      if (!person) {
+        continue;
+      }
+      persons.add(person);
+    }
+    return persons;
+  }
 
   // $: personFolders = appGlobal.files.filter(file => file.sentToFrom == $selectedPerson);
   // $: displayFiles = personFolders?.first?.files;
@@ -43,6 +81,9 @@
     let emails = await search.startSearch();
     for (let email of emails) {
       for (let attachment of email.attachments) {
+        if (attachment.hidden) {
+          continue;
+        }
         let file = new File();
         file.setFileName(attachment.filename);
         file.filepathLocal = attachment.filepathLocal;
@@ -60,5 +101,9 @@
 <style>
   .persons-file-pane {
     margin-block-start: 8px;
+  }
+  .progress {
+    align-self: center;
+    justify-self: center;
   }
 </style>
