6fb31f97f1000b22868ca321561a1b1a1ce1bc62
Ben Bucksch <ben.bucksch@beonex.com>
2025-07-22 10:22:51 +0200

    Mail: Get mail button: Use Refresh icon instead of Download icon

--- app/frontend/Mail/LeftPane/GetMailButton.svelte
+++ app/frontend/Mail/LeftPane/GetMailButton.svelte
@@ -20,7 +20,7 @@
 <script lang="ts">
   import type { Folder } from "../../../logic/Mail/Folder";
   import RoundButton from "../../Shared/RoundButton.svelte";
-  import DownloadIcon from "lucide-svelte/icons/download";
+  import DownloadIcon from "lucide-svelte/icons/refresh-cw";
   import FetchingIcon from "lucide-svelte/icons/arrow-big-down-dash";
   import DoneIcon from "lucide-svelte/icons/check";
   import NewIcon from "lucide-svelte/icons/sparkle";
