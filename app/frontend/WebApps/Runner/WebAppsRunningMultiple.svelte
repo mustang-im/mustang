60d1c5c29ba6e3af5d9ac7a1ff52245376960670
Ben Bucksch <ben.bucksch@beonex.com>
2025-07-27 21:26:23 +0200

    WebApps: Launcher bar on left, to switch between apps

---
+++ app/frontend/WebApps/Runner/WebAppsRunningMultiple.svelte
@@ -0,0 +1,10 @@
+<vbox class="running-apps" flex>
+  {#each webAppsRunning.each as webApp}
+    <WebAppRunner app={webApp} hidden={$showingWebApp != webApp} />
+  {/each}
+</vbox>
+
+<script lang="ts">
+  import { webAppsRunning, showingWebApp } from "./WebAppsRunning";
+  import WebAppRunner from "./WebAppRunner.svelte";
+</script>
