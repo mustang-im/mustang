b8236537a120350d340376b80e24791930322ad5
Jeremy Chang <jeremy.chang@beonex.com>
2025-07-30 16:24:07 -0600

    Build: Add quotes

--- mobile/build.sh
+++ mobile/build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ../mobile/dist
 
-export NODE_OPTIONS=--max-old-space-size=32768
+export NODE_OPTIONS="--max-old-space-size=32768"
 
 (cd ../app && npm run build && cp -R ./dist/* ../mobile/dist) &&
 (cd backend && npm run build) &&
