92d1499e0ea273f6b266caa535a3d1ec0f6286a0
Ben Bucksch <ben.bucksch@beonex.com>
2025-08-06 23:40:13 +0200

    Meet: Show screen share video

--- app/frontend/Meet/InMeeting.svelte
+++ app/frontend/Meet/InMeeting.svelte
@@ -30,8 +30,8 @@
   let selectedParticipant: MeetingParticipant = null;
   $: me = meeting.myParticipant;
   $: allStreams = meeting.videos;
-  $: videos = $allStreams.filter(video => video.hasVideo);
-  $: audioOnlyStreams = $allStreams.filter(video => !video.hasVideo);
+  $: videos = $allStreams.filterObservable(video => video.hasVideo || video.isScreenShare);
+  $: audioOnlyStreams = $allStreams.filterObservable(video => !video.hasVideo);
 
   // Show sidebar only when there are no participants, and
   // close it when the first person joins
