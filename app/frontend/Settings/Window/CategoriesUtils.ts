488357fb34265dbdddbb33ea0a045e5428bb0049
Ben Bucksch <ben.bucksch@beonex.com>
2025-08-07 05:27:57 +0200

    Settings: openCategoryForApp()

--- app/frontend/Settings/Window/CategoriesUtils.ts
+++ app/frontend/Settings/Window/CategoriesUtils.ts
@@ -1,6 +1,11 @@
 import type { SettingsCategory } from "./SettingsCategory";
 import { settingsCategories } from "../SettingsCategories";
 import type { MustangApp } from "../../AppsBar/MustangApp";
+import type { Account } from "../../../logic/Abstract/Account";
+import { settingsMustangApp } from "./SettingsMustangApp";
+import { openApp } from "../../AppsBar/selectedApp";
+import { selectedAccount, selectedCategory } from "./selected";
+import { assert } from "../../../logic/util/util";
 
 export function getAllSettingsCategories(): SettingsCategory[] {
   let results: SettingsCategory[] = []
@@ -22,4 +27,10 @@ export function getSettingsCategoryByID(id: string): SettingsCategory {
 
 export function getSettingsCategoryForApp(app: MustangApp) {
   return getAllSettingsCategories().find(cat => cat.forApp == app);
+}
+
+export function openSettingsCategoryForApp(app: MustangApp) {
+  let cat = getSettingsCategoryForApp(app);
+  selectedCategory.set(cat);
+  openApp(settingsMustangApp);
 }
