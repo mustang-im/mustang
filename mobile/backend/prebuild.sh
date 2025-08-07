5b44334e166950ae402dcbbad245af46f300ebee
Jeremy Chang <jeremy.chang@beonex.com>
2025-08-07 17:21:40 -0600

    Backend build

--- mobile/backend/prebuild.sh
+++ mobile/backend/prebuild.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd node_modules;
-(cd better-sqlite3 && npx prebuild-for-nodejs-mobile $MOBILE_ARCH)
-(cd bufferutil && npx prebuild-for-nodejs-mobile $MOBILE_ARCH)
+(cd better-sqlite3 && npx prebuild-for-nodejs-mobile $MOBILE_ARCH $ANDROID_SDK)
+(cd bufferutil && npx prebuild-for-nodejs-mobile $MOBILE_ARCH $ANDROID_SDK)
