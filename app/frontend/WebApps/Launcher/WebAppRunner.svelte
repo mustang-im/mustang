0f71628bba842c1616d903f4feeca161f359dfb2
Ben Bucksch <ben.bucksch@beonex.com>
2025-08-07 07:43:27 +0200

    WebApps: Remove old launcher

--- app/frontend/WebApps/Launcher/WebAppRunner.svelte
+++
@@ -1,8 +0,0 @@
-<WebView url={runningApp.start} title={runningApp.nameTranslated} />
-
-<script lang="ts">
-  import type { WebAppListed } from "../../../logic/WebApps/WebAppListed";
-  import WebView from "../../Shared/WebView.svelte";
-
-  export let runningApp: WebAppListed;
-</script>
