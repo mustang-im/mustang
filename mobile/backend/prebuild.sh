46186d921ebcbfa8f0670f31c24b27d09972ac7b
Jeremy Chang <jeremy.chang@beonex.com>
2025-08-19 17:33:55 -0600

    Set android lib url

--- mobile/backend/prebuild.sh
+++ mobile/backend/prebuild.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export ANDROID_LIBNODE="https://github.com/mustang-im/nodejs-mobile/releases/download/v22.9.0/nodejs-mobile-v22.9.0-android.zip"
+
 cd node_modules;
-(cd better-sqlite3 && npx prebuild-for-nodejs-mobile $MOBILE_ARCH $ANDROID_SDK)
+(cd better-sqlite3 && npx prebuild-for-nodejs-mobile $MOBILE_ARCH $ANDROID_SDK) &&
 (cd bufferutil && npx prebuild-for-nodejs-mobile $MOBILE_ARCH $ANDROID_SDK)
