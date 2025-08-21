75e3f09e04a30129345c090aa0bfa023507518d2
Ben Bucksch <ben.bucksch@beonex.com>
2025-08-21 02:27:08 +0200

    Android: CI: Set isMobile = true in build.ts

--- app/logic/build.ts
+++ app/logic/build.ts
@@ -4,6 +4,6 @@ export const production = false;
 export const appVersion: string = '0.9.4-dev';
 export const appName: string = 'Mustang';
 export const siteRoot: string = 'https://mustang.im';
-export const isMobile = false;
+export const isMobile = true;
 export const webMail = false;
 export const includeProprietary = true;
--- mobile/hooks/android/update-project.ts
+++ mobile/hooks/android/update-project.ts
@@ -4,29 +4,28 @@ import config from "../../capacitor.config";
 import { readFile, writeFile } from "node:fs/promises";
 
 const __dirname = import.meta.dirname;
-const androidProjectFile = path.join(__dirname, "../../android/app/build.gradle");
 
 async function updateProjectSettings() {
   if (process.env.CAPACITOR_PLATFORM_NAME != "android") return;
 
-  const appId = config.appId;
-  const appName = config.appName;
-  const version = packageJSON.version;
+  let appId = config.appId;
+  let appName = config.appName;
+  let version = packageJSON.version;
 
-  // Update the applicationId in build.gradle
+  // Update build.gradle
+  let androidProjectFile = path.join(__dirname, "../../android/app/build.gradle");
   let gradleContent = await readFile(androidProjectFile, { encoding: 'utf-8' });
   gradleContent = gradleContent.replace(/applicationId "[^"]+"/, `applicationId "${appId}"`);
-
-  // Update the version in build.gradle
   gradleContent = gradleContent.replace(/versionName "[^"]+"/, `versionName "${version}"`);
-
-  // Update the namespace in build.gradle
   gradleContent = gradleContent.replace(/namespace "[^"]+"/, `namespace "${appId}"`);
-
-  // Update the appName in build.gradle
   gradleContent = gradleContent.replace(/def appName = "[^"]+"/, `def appName = "${appName}"`);
-
   await writeFile(androidProjectFile, gradleContent);
+
+  // Update build.ts
+  let appConfigFile = path.join(__dirname, "../../../app/logic/build.ts");
+  let appConfigContent = await readFile(appConfigFile, { encoding: 'utf-8' });
+  appConfigContent = appConfigContent.replace(/ isMobile = false;/, ` isMobile = true;`);
+  await writeFile(appConfigFile, appConfigContent);
 }
 
 async function main() {
