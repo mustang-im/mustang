01972e0de27cded1671ef0fee4309b686782fb5a
Jeremy Chang <jeremy.chang@beonex.com>
2025-08-22 15:01:55 -0600

    Force CMake version

--- mobile/build.sh
+++ mobile/build.sh
@@ -4,7 +4,9 @@ mkdir -p ../mobile/dist
 
 export NODE_OPTIONS="--max-old-space-size=32768"
 
-(cd ../app && npm run build && cp -R ./dist/* ../mobile/dist) &&
+(cd ../app && npm run build) &&
+(cd ./dist && rm -rf * .[!.]* ..?*) &&
+(cd ../app && cp -R ./dist/* ../mobile/dist) &&
 (cd backend && npm run build) &&
 npx cap copy
 
