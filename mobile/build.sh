1a0453a3eac20fbc785545b40173fbe7fe1d3057
Jeremy Chang <jeremy.chang@beonex.com>
2025-08-22 20:00:31 -0600

    Place code in ./build.sh

--- mobile/build.sh
+++ mobile/build.sh
@@ -4,7 +4,11 @@ mkdir -p ../mobile/dist
 
 export NODE_OPTIONS="--max-old-space-size=32768"
 
-bash ./hooks/common/mobile-ui.sh &&
+# Setup mobile UI
+perl -p -i \
+  -e "s|isMobile = false;$|isMobile = true;|;" \
+  ../app/logic/build.ts
+
 (cd ../app && npm run build && cp -R ./dist/* ../mobile/dist) &&
 (cd backend && npm run build) &&
 npx cap copy
--- mobile/hooks/common/mobile-ui.sh
+++
@@ -1,5 +0,0 @@
-# Run from /mobile
-echo Setting UI to mobile
-perl -p -i \
-  -e "s|isMobile = false;$|isMobile = true;|;" \
-  ../app/logic/build.ts
