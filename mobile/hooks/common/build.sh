7c864714898b0cad5d2cff0829bdf431779ac6b5
Jeremy Chang <jeremy.chang@beonex.com>
2025-08-25 12:07:02 -0600

    Mobile: Set mobile UI build.ts flag before build (#840)

--- mobile/hooks/common/build.sh
+++ mobile/hooks/common/build.sh
@@ -4,6 +4,11 @@ mkdir -p ../mobile/dist
 
 export NODE_OPTIONS="--max-old-space-size=32768"
 
+# Setup mobile UI
+perl -p -i \
+  -e "s|isMobile = false;$|isMobile = true;|;" \
+  ../app/logic/build.ts
+
 (cd ../app && npm run build && cp -R ./dist/* ../mobile/dist) &&
 (cd backend && npm run build) &&
 npx cap copy
