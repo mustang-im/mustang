2182a1fdf7957a988ed34937c6d7be5bef1a6474
Jeremy Chang <jeremy.chang@beonex.com>
2025-08-25 12:33:11 -0600

    Move env into sh script

--- mobile/backend/prebuild.sh
+++ mobile/backend/prebuild.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 export ANDROID_LIBNODE="https://github.com/mustang-im/nodejs-mobile/releases/download/v22.9.0/nodejs-mobile-v22.9.0-android.zip"
+export ANDROID_SDK="--sdk35"
 
 cd node_modules;
 (cd better-sqlite3 && npx prebuild-for-nodejs-mobile $MOBILE_ARCH $ANDROID_SDK) &&
 (cd bufferutil && npx prebuild-for-nodejs-mobile $MOBILE_ARCH $ANDROID_SDK)
+
+unset ANDROID_LIBNODE
+unset ANDROID_SDK
