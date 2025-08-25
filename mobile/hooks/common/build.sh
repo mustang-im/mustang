49ed67d172ac1a27165dc1ccdb97afac8cdda47a
Jeremy Chang <jeremy.chang@beonex.com>
2025-08-25 15:36:24 -0600

    Clear assets cache and libnode cache for release build

--- mobile/hooks/common/build.sh
+++ mobile/hooks/common/build.sh
@@ -4,13 +4,31 @@ mkdir -p ../mobile/dist
 
 export NODE_OPTIONS="--max-old-space-size=32768"
 
+# Clear LIBNODE cache for release build
+release_found=false
+
+for arg in "$@"; do
+  if [[ "$arg" == "--release" ]]; then
+    release_found=true
+    break
+  fi
+done
+
+if "$release_found"; then
+  export NO_LIBNODE_CACHE=true
+fi
+
 # Setup mobile UI
 perl -p -i \
   -e "s|isMobile = false;$|isMobile = true;|;" \
   ../app/logic/build.ts
 
-(cd ../app && npm run build && cp -R ./dist/* ../mobile/dist) &&
+# Build and copy assets
+(cd ../app && npm run build) &&
+(cd ./dist && rm -rf * .[!.]* ..?*) &&
+(cd ../app && cp -R ./dist/* ../mobile/dist) &&
 (cd backend && npm run build) &&
 npx cap copy
 
 unset NODE_OPTIONS
+unset NO_LIBNODE_CACHE
