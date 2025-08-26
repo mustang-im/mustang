b803bc01fa9c00889f845d0d081b4ffa0f48721f
Jeremy Chang <jeremy.chang@beonex.com>
2025-08-26 11:59:44 -0600

    Undo isMobile

--- app/logic/build.ts
+++ app/logic/build.ts
@@ -4,6 +4,6 @@ export const production = false;
 export const appVersion: string = '0.9.5-dev';
 export const appName: string = 'Mustang';
 export const siteRoot: string = 'https://mustang.im';
-export const isMobile = true;
+export const isMobile = false;
 export const webMail = false;
 export const includeProprietary = true;
