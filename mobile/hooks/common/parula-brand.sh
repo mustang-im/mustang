dc9cb0dafc44e972dcf2c062dde65a3643c93a58
Jeremy Chang <jeremy.chang@beonex.com>
2025-08-26 11:05:14 -0600

    Parula brand

---
+++ mobile/hooks/common/parula-brand.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Run from /mobile
+
+# Replace name in all apps
+(cd ../app/build && ./parula-brand.sh)
+
+# Replace Android icons
+cp -R parula/android android/app/src/main/res
+
+# Relace all appName in /mobile
+perl -p -i \
+  -e "s|im.mustang.capa|im.parula.capa|;" \
+  -e "s|"Mustang"|"Parula"|;" \
+  ./capacitor.config.ts
