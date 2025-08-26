7aa14196ca0ee77dcf9f4ae602521b126c7618e4
Jeremy Chang <jeremy.chang@beonex.com>
2025-08-26 11:58:39 -0600

    Review comments

--- mobile/hooks/common/build.sh
+++ mobile/hooks/common/build.sh
@@ -1,23 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p ../mobile/dist
-
 export NODE_OPTIONS="--max-old-space-size=32768"
-
-# Clear LIBNODE cache for release build
-is_release=false
-
-for arg in "$@"; do
-  if [[ "$arg" == "--release" ]]; then
-    is_release=true
-    break
-  fi
-done
-
-if "$is_release"; then
-  rm -rf backend/node_modules/prebuild-for-nodejs-mobile/android/libnode
-  rm -rf node_modules/capacitor-nodejs/android/libnode
-fi
 
 # Setup mobile UI
 perl -p -i \
@@ -26,7 +9,7 @@ perl -p -i \
 
 # Build and copy assets
 (cd ../app && npm run build) &&
-(cd ./dist && rm -rf * .[!.]* ..?*) &&
+(rm -rf ./dist; mkdir -p ./dist) &&
 (cd ../app && cp -R ./dist/* ../mobile/dist) &&
 (cd backend && npm run build) &&
 npx cap copy
