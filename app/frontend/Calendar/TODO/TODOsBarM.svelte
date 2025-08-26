d348c16a96809f0dd54d4c8807b54d134e9bd247
Ben Bucksch <ben.bucksch@beonex.com>
2025-08-26 02:09:11 +0200

    Calendar: Mobile: TODO: Add button to go back to calendar

--- app/frontend/Calendar/TODO/TODOsBarM.svelte
+++ app/frontend/Calendar/TODO/TODOsBarM.svelte
@@ -1,22 +1,38 @@
 <hbox class="buttons">
   <AppBarM>
     <!-- left -->
-    <hbox class="empty">
+    <hbox class="calendar">
+      <Button
+        label={$t`Calendar`}
+        onClick={goToCalendar}
+        iconSize="24px" iconOnly plain>
+        <CalendarIcon slot="icon" />
+      </Button>
+    </hbox>
 
     <!-- left middle -->
-    <hbox class="empty">
+    <hbox class="empty" />
 
     <AppMenuButton />
 
     <!-- right middle -->
-    <hbox class="empty">
+    <hbox class="empty" />
 
     <!-- right -->
-    <hbox class="empty">
+    <hbox class="empty" />
   </AppBarM>
 </hbox>
 
 <script lang="ts">
+  import { goTo, globalSearchTerm } from "../../AppsBar/selectedApp";
   import AppBarM from "../../AppsBar/AppBarM.svelte";
   import AppMenuButton from "../../AppsBar/AppMenuM/AppMenuButton.svelte";
+  import Button from "../../Shared/Button.svelte";
+  import CalendarIcon from "../CalendarIcon.svelte";
+  import { t } from "../../../l10n/l10n";
+
+  function goToCalendar() {
+    $globalSearchTerm = null;
+    goTo(`/calendar`, {});
+  }
 </script>
