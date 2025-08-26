9915e7bb5c862eb0738f83e46b1bba60365d319b
Ben Bucksch <ben.bucksch@beonex.com>
2025-08-26 03:03:24 +0200

    Mobile: Calendar: Show event: Add Bar

--- app/frontend/Calendar/DisplayEvent/ShowEvent.svelte
+++ app/frontend/Calendar/DisplayEvent/ShowEvent.svelte
@@ -4,10 +4,16 @@
   <EditEvent {event} />
 {/if}
 
+{#if appGlobal.isMobile}
+  <ShowEventBarM />
+{/if}
+
 <script lang="ts">
   import type { Event } from "../../../logic/Calendar/Event";
+  import { appGlobal } from "../../../logic/app";
   import EditEvent from "../EditEvent/EditEvent.svelte";
   import InvitationInCalendar from "./InvitationInCalendar.svelte";
+  import ShowEventBarM from "./ShowEventBarM.svelte";
 
   export let event: Event;
 </script>
---
+++ app/frontend/Calendar/DisplayEvent/ShowEventBarM.svelte
@@ -0,0 +1,37 @@
+<hbox class="buttons">
+  <AppBarM>
+    <!-- left -->
+    <hbox class="calendar">
+      <Button
+        label={$t`Calendar`}
+        onClick={goToCalendar}
+        iconSize="24px" iconOnly plain>
+        <CalendarIcon slot="icon" />
+      </Button>
+    </hbox>
+
+    <!-- left middle -->
+    <hbox class="empty" />
+
+    <AppMenuButton />
+
+    <!-- right middle -->
+    <hbox class="empty" />
+
+    <!-- right -->
+    <hbox class="empty" />
+  </AppBarM>
+</hbox>
+
+<script lang="ts">
+  import { goTo } from "../../AppsBar/selectedApp";
+  import AppBarM from "../../AppsBar/AppBarM.svelte";
+  import AppMenuButton from "../../AppsBar/AppMenuM/AppMenuButton.svelte";
+  import Button from "../../Shared/Button.svelte";
+  import CalendarIcon from "../CalendarIcon.svelte";
+  import { t } from "../../../l10n/l10n";
+
+  function goToCalendar() {
+    goTo(`/calendar`, {});
+  }
+</script>
