91ff962f85b830a51200609893d72b807e90d84e
Jeremy Chang <jeremy.chang@beonex.com>
2025-09-10 11:53:17 -0600

    Zip file name and remove uneccessary step

--- mobile/hooks/ios/setup.sh
+++ mobile/hooks/ios/setup.sh
@@ -4,13 +4,13 @@ mkdir -p ./resources
 cd ./resources
 
 DOWNLOAD_URL="https://github.com/mustang-im/nodejs-mobile/releases/download/v24.5.0/nodejs-mobile-v24.5.0-ios.zip"
+TMP_ZIP_FILE="tmp.zip"
 
 # Download the file
 echo "Downloading $DOWNLOAD_URL"
-curl -L -o latest_release.zip "$DOWNLOAD_URL"
+curl -L -o $TMP_ZIP_FILE "$DOWNLOAD_URL"
 
-unzip -o latest_release.zip -d ./
-rm -r latest_release.zip ./*/include
-mv ./*/NodeMobile.xcframework ./
+unzip -o $TMP_ZIP_FILE -d ./
+rm -r $TMP_ZIP_FILE ./include
 
 echo "Download and extraction complete."
