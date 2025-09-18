9e975e9e38eb1154e3f606ff1cd1242958dfa3fa
Jeremy Chang <jeremy.chang@beonex.com>
2025-09-18 11:45:02 -0600

    Revert "Capacitor Config dynamically configure for debug"
    This reverts commit fe15286940b0df05b8ebc483f752dfbc8a256619.

--- mobile/capacitor.config.ts
+++ mobile/capacitor.config.ts
@@ -1,34 +1,22 @@
 import type { CapacitorConfig } from '@capacitor/cli';
-import { production } from '../app/logic/build.ts';
-import { env } from 'node:process';
 
-function config(): CapacitorConfig {
-  return {
-    appId: "im.mustang.capa",
-    appName: "Mustang",
-    webDir: "./dist",
-    plugins: {
-      CapacitorNodeJS: {
-        nodeDir: "nodejs",
-        androidLibNode: "https://github.com/mustang-im/nodejs-mobile/releases/download/v22.9.0/nodejs-mobile-v22.9.0-android.zip",
-        androidArchitectures: ["arm64"],
-      },
-      SplashScreen: {
-        "launchAutoHide": true
-      },
-      EdgeToEdge: {
-        backgroundColor: "#494558", // --appbar-bg, see app.css
-      },
+const config: CapacitorConfig = {
+  appId: "im.mustang.capa",
+  appName: "Mustang",
+  webDir: "./dist",
+  plugins: {
+    CapacitorNodeJS: {
+      nodeDir: "nodejs",
+      androidLibNode: "https://github.com/mustang-im/nodejs-mobile/releases/download/v22.9.0/nodejs-mobile-v22.9.0-android.zip",
+      androidArchitectures: ["arm64"],
     },
-    ios: {
-      buildOptions: {
-        signingStyle: production ? 'manual' : 'automatic',
-        exportMethod: production ? 'app-store-connect' : 'debugging',
-        signingCertificate: env.IOS_SIGNING_CERTIFICATE,
-        provisioningProfile: env.IOS_PROVISIONING_PROFILE,
-      },
-    }
-  }
+    SplashScreen: {
+      launchAutoHide: true,
+    },
+    EdgeToEdge: {
+      backgroundColor: "#494558", // --appbar-bg, see app.css
+    },
+  },
 }
 
-export default config();
+export default config;
