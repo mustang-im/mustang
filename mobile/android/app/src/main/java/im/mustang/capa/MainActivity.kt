061a80a14e16e2c0b58222d8cb0bbd9604633e6b
Jeremy Chang <jeremy.chang@beonex.com>
2025-09-19 14:13:46 -0600

    Don't copy every startup

--- mobile/android/app/src/main/java/im/mustang/capa/MainActivity.kt
+++ mobile/android/app/src/main/java/im/mustang/capa/MainActivity.kt
@@ -1,13 +1,24 @@
 package im.mustang.capa
 
+import android.os.Bundle
 import com.getcapacitor.BridgeActivity
 
 class MainActivity : BridgeActivity() {
     private lateinit var nodeJS: NodeJS
 
-    override fun onStart() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         this.nodeJS = NodeJS(this.applicationContext)
         this.nodeJS.start()
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        this.nodeJS.start()
         super.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        this.nodeJS.stop()
     }
 }
