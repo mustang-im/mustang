752460d089b4b559a7fbcb662355dadada051301
Jeremy Chang <jeremy.chang@beonex.com>
2025-09-18 18:15:42 -0600

    NodeJS run directly

--- mobile/android/app/src/main/java/im/mustang/capa/MainActivity.kt
+++ mobile/android/app/src/main/java/im/mustang/capa/MainActivity.kt
@@ -2,4 +2,12 @@ package im.mustang.capa
 
 import com.getcapacitor.BridgeActivity
 
-class MainActivity : BridgeActivity()
+class MainActivity : BridgeActivity() {
+    private lateinit var nodeJS: NodeJS
+
+    override fun onStart() {
+        this.nodeJS = NodeJS(this.applicationContext)
+        this.nodeJS.start()
+        super.onStart()
+    }
+}
