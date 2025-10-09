f34c9b0b0fe39c4c6fbd158732d09697f0fe8013
Jeremy Chang <jeremy.chang@beonex.com>
2025-10-09 17:03:53 -0600

    iOS build script

---
+++ mobile/ios/build/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Run from /mobile/ios
+
+# Install iOS provisioning profile
+if [ -f "./apple-dist.mobileprovision" ]; then
+    mkdir -p ~/Library/Developer/Xcode/UserData/Provisioning\ Profiles
+    cp ./apple-dist.mobileprovision ~/Library/Developer/Xcode/UserData/Provisioning\ Profiles/
+    echo "Installed provisioning profile"
+else
+    echo "Warning: apple-dist.mobileprovision not found in mobile/ios"
+fi
+
+(cd ../ && ./hooks/common/build.sh)
