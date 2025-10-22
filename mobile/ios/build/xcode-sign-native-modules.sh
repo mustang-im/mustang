c80bb51e71917f5bf056e89dda52905c90726a4e
Jeremy Chang <jeremy.chang@beonex.com>
2025-10-22 17:28:08 -0600

    Get the node path

--- mobile/ios/build/xcode-sign-native-modules.sh
+++ mobile/ios/build/xcode-sign-native-modules.sh
@@ -1,3 +1,5 @@
+$(which node) ../build/ios-create-plists-and-dlopen-override.js ../App/App/nodejs-project
+
 # Embed every resulting .framework in the application and delete them afterwards.
 embed_framework()
 {
