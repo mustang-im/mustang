e44ad4f4dfa144fc8a3b3ce5d199e3ee2e08de5b
Jeremy Chang <jeremy.chang@beonex.com>
2025-10-23 12:36:57 -0600

    Delete framework from their build paths

--- mobile/ios/build/xcode-sign-native-modules.sh
+++ mobile/ios/build/xcode-sign-native-modules.sh
@@ -10,3 +10,7 @@ embed_framework()
     /usr/bin/codesign --force --sign $EXPANDED_CODE_SIGN_IDENTITY --preserve-metadata=identifier,entitlements,flags --timestamp=none "$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH/$FRAMEWORK_NAME"
 }
 find "$CODESIGNING_FOLDER_PATH/nodejs-project/" -name "*.framework" -type d | while read frmwrk_path; do embed_framework "$frmwrk_path"; done
+
+#Delete frameworks from their build paths
+find "$CODESIGNING_FOLDER_PATH/nodejs-project/" -path "*/*.framework/*" -delete
+find "$CODESIGNING_FOLDER_PATH/nodejs-project/" -name "*.framework" -type d -delete
