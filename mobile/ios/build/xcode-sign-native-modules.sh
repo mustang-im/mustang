bbd543d1ec2366bc3ee681e8ed1b5c99a328194e
Jeremy Chang <jeremy.chang@beonex.com>
2025-10-23 13:36:08 -0600

    Squashed commit of the following:
    commit bfd70d44b8bbd7ffcc3566882665556d0bc01815
    Author: Jeremy Chang <jeremy.chang@beonex.com>
    Date:   Thu Oct 23 13:35:24 2025 -0600
    
        Use the correct path
    
    commit e44ad4f4dfa144fc8a3b3ce5d199e3ee2e08de5b
    Author: Jeremy Chang <jeremy.chang@beonex.com>
    Date:   Thu Oct 23 12:36:57 2025 -0600
    
        Delete framework from their build paths
    
    commit 894af9552d977ee3d7a66f4a634212c7c48a39b5
    Author: Jeremy Chang <jeremy.chang@beonex.com>
    Date:   Thu Oct 23 12:08:24 2025 -0600
    
        Add docs
    
    commit 9f1b10c7d0841a05d2bde54c34358da702b12860
    Author: Jeremy Chang <jeremy.chang@beonex.com>
    Date:   Thu Oct 23 11:53:08 2025 -0600
    
        Fix path
    
    commit b279909e4a4c5cb446e7fe3be889290fc14d967d
    Author: Jeremy Chang <jeremy.chang@beonex.com>
    Date:   Thu Oct 23 11:44:59 2025 -0600
    
        Preload override script
    
    commit 1fd07b530ac635974c46d04f7fc5dd7684bb5902
    Author: Jeremy Chang <jeremy.chang@beonex.com>
    Date:   Thu Oct 23 11:12:26 2025 -0600
    
        Fix building iOS Framework
    
    commit c80bb51e71917f5bf056e89dda52905c90726a4e
    Author: Jeremy Chang <jeremy.chang@beonex.com>
    Date:   Wed Oct 22 17:28:08 2025 -0600
    
        Get the node path
    
    commit b75cc802102c9e12c274efd0fa203793e8df73a4
    Author: Jeremy Chang <jeremy.chang@beonex.com>
    Date:   Wed Oct 22 17:04:32 2025 -0600
    
        Sign Node Native modules
    
    commit 9fcf0145bd4acedcb925ac2dec940a1718e4140b
    Author: Jeremy Chang <jeremy.chang@beonex.com>
    Date:   Wed Oct 22 16:06:54 2025 -0600
    
        Convert .node to .framework
    
    commit f2aa0f51c8e3ebcae1fc43c313a2504141b0596b
    Author: Jeremy Chang <jeremy.chang@beonex.com>
    Date:   Wed Oct 22 12:25:07 2025 -0600
    
        Remove capacitor-nodejs

--- mobile/ios/build/xcode-sign-native-modules.sh
+++ mobile/ios/build/xcode-sign-native-modules.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-node ../build/ios-create-plists-and-dlopen-override.js ../App/App/nodejs-project
+NODEJS_PROJECT_DIR="$( cd "$CODESIGNING_FOLDER_PATH" && cd nodejs-project && pwd )"
+
+node ../build/ios-create-plists-and-dlopen-override.js "$NODEJS_PROJECT_DIR"
 
 # Embed every resulting .framework in the application and delete them afterwards.
 embed_framework()
@@ -10,3 +12,7 @@ embed_framework()
     /usr/bin/codesign --force --sign $EXPANDED_CODE_SIGN_IDENTITY --preserve-metadata=identifier,entitlements,flags --timestamp=none "$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH/$FRAMEWORK_NAME"
 }
 find "$CODESIGNING_FOLDER_PATH/nodejs-project/" -name "*.framework" -type d | while read frmwrk_path; do embed_framework "$frmwrk_path"; done
+
+#Delete frameworks from their build paths
+find "$CODESIGNING_FOLDER_PATH/nodejs-project/" -path "*/*.framework/*" -delete
+find "$CODESIGNING_FOLDER_PATH/nodejs-project/" -name "*.framework" -type d -delete
