b49623c1713287637fdb03313f043da9bb89c1ed
Jeremy Chang <jeremy.chang@beonex.com>
2025-10-30 16:44:44 -0600

    Remove background process

--- app/frontend/Settings/About/LogHistory.ts
+++ app/frontend/Settings/About/LogHistory.ts
@@ -1,3 +1,15 @@
 import { ArrayColl } from "svelte-collections";
 
 export const logHistory = new ArrayColl<string>();
+
+const originalLog = console.log;
+const originalError = console.error;
+
+console.log = function(...args: any[]) {
+  logHistory.push(args);
+  originalLog.apply(console, args);
+};
+console.error = function(...args: any[]) {
+  logHistory.push(args);
+  originalError.apply(console, args);
+};
