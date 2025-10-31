7b6ec7c7870865e5f2fafa62a4449e54cb8695a7
Ben Bucksch <ben.bucksch@beonex.com>
2025-10-31 14:02:21 +0100

    Mobile: Mail: Accounts: Add header; and workspace warning

--- app/frontend/Mail/LeftPane/AccountsM.svelte
+++ app/frontend/Mail/LeftPane/AccountsM.svelte
@@ -1,14 +1,26 @@
 <vbox flex class="accounts-pane">
   <FolderList {folders} bind:selectedFolder bind:selectedFolders on:click={() => catchErrors(onFolderSelected)} />
-  <vbox class="accounts">
-    <AccountSelectorRound {accounts} bind:selectedAccount iconDefault={AccountIcon} iconSize="32px" large />
-  </vbox>
+
+  <header class="accounts-header font-small">
+    {$t`Accounts`}
+  </header>
+  {#if accounts.hasItems}
+    <vbox class="accounts">
+      <AccountSelectorRound {accounts} bind:selectedAccount iconDefault={AccountIcon} iconSize="32px" />
+    </vbox>
+  {:else if $selectedWorkspace}
+    <div class="warning no-accounts font-small">
+      {$t`No accounts in workspace ${$selectedWorkspace.name}`}
+    </div>
+  {/if}
+
   <AccountsBarM {selectedAccount} {selectedFolder} />
 </vbox>
 
 <script lang="ts">
   import type { MailAccount } from "../../../logic/Mail/MailAccount";
   import { type Folder } from "../../../logic/Mail/Folder";
+  import { selectedWorkspace } from "../../MainWindow/Selected";
   import { goTo } from "../../AppsBar/selectedApp";
   import AccountSelectorRound from "../../Shared/AccountSelectorRound.svelte";
   import FolderList from "./FolderList.svelte";
@@ -18,6 +30,7 @@
   import { catchErrors } from "../../Util/error";
   import { sleep, assert } from "../../../logic/util/util";
   import type { ArrayColl, Collection } from 'svelte-collections';
+  import { t } from "../../../l10n/l10n";
 
   export let accounts: Collection<MailAccount>; /** in */
   export let folders: Collection<Folder>; /** in */
@@ -47,11 +60,25 @@
     margin-block-start: -4px;
   }
 
+  header {
+    margin-inline-start: 18px;
+    color: grey;
+  }
+  .accounts-header {
+    margin-block-start: 6px;
+  }
+
   .accounts {
     align-items: center;
     margin-block: 20px;
   }
   .accounts :global(.account) {
-    margin-inline-end: 12px;
+    padding-inline: 16px;
+  }
+
+  .warning.no-accounts {
+    text-align: center;
+    color: grey;
+    height: 110px;
   }
 </style>
