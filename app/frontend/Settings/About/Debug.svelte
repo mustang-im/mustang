3640c137522ab6073da0bcb4e5bbdd0335976242
Ben Bucksch <ben.bucksch@beonex.com>
2025-11-06 16:51:03 +0100

    Mobile: Settings: Debug log: Make output readable

--- app/frontend/Settings/About/Debug.svelte
+++ app/frontend/Settings/About/Debug.svelte
@@ -1,20 +1,20 @@
 <vbox>
-  <h1>{$t`Debug`}</h1>
-  <div class="log-history selectable" bind:this={logHistoryEl}>
+  <h1>{$t`Debug log`}</h1>
+  <div class="log-history" bind:this={logHistoryEl}>
     {#each $logHistory.each as log, i}
-      <div>{i}: {ensureArray(log).map(v => safeStringify(v)).join(" ")}</div>
+      <div class="line selectable">{i}: {ensureArray(log).map(v => safeStringify(v)).join(" ")}</div>
     {/each}
   </div>
-  <vbox class="buttons">
-    <Button
-      label={$t`Clear log history`}
-      onClick={() => logHistory.clear()}
-    />
+  <hbox class="buttons">
     <Button
       label={$t`Copy to clipboard`}
       onClick={copyLogHistory}
     />
-    </vbox>
+    <Button
+      label={$t`Clear log history`}
+      onClick={() => logHistory.clear()}
+    />
+  </hbox>
 </vbox>
 
 <script lang="ts">
@@ -29,9 +29,10 @@
       return JSON.stringify(obj, (key, value) => {
         if (key && key.startsWith("_")) {
           return undefined;
-        }
-        if (value == null) { // or undefined
+        } else if (value == null) { // or undefined
           return "";
+        } else if (["nextTime"].includes(key)) { // noise useless properties
+          return "(ignored)";
         } else if (typeof value === "object") {
           if (value instanceof Error) {
             return value.toString();
@@ -48,7 +49,7 @@
           seen.add(value);
         }
         return value;
-      });
+      }, 2);
     } catch (error) {
       try {
         return String(obj);
@@ -60,7 +61,7 @@
 
   let logHistoryEl: HTMLDivElement;
   async function copyLogHistory() {
-    navigator.clipboard.writeText(logHistoryEl.innerText);
+    navigator.clipboard.writeText(logHistoryEl.innerText + "\n");
   }
 </script>
 
@@ -68,6 +69,9 @@
   .log-history {
     max-height: 500px;
     overflow-y: auto;
+  }
+  .line {
+    white-space: pre-wrap;
   }
   .buttons {
     margin-block-start: 16px;
