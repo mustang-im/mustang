dab2aec28c945371b75381e6e8616e4b036c4fa5
Jeremy Chang <jeremy.chang@beonex.com>
2025-11-07 11:02:51 -0600

    Fix code formatting: add spaces around operators and after commas

--- mobile/ios/build/ios-create-plists-and-dlopen-override.js
+++ mobile/ios/build/ios-create-plists-and-dlopen-override.js
@@ -16,7 +16,7 @@ export function visitEveryFramework(projectPath) {
   function recursivelyFindFrameworks(currentPath) {
     let currentFiles = fs.readdirSync(currentPath);
     for (let i = 0; i < currentFiles.length; i++) {
-      let currentFilename = path.normalize(path.join(currentPath,currentFiles[i]));
+      let currentFilename = path.normalize(path.join(currentPath, currentFiles[i]));
       if (fs.lstatSync(currentFilename).isDirectory()) {
         if (currentFilename.endsWith(".node")) {
           let frameworkContents = fs.readdirSync(currentFilename);
@@ -26,10 +26,9 @@ export function visitEveryFramework(projectPath) {
             countInvalidFrameworks++;
           } else {
             let currentBinaryName = frameworkContents[0];
-            let checkFileType = spawnSync('file', [path.join(currentFilename,currentBinaryName)]);
+            let checkFileType = spawnSync('file', [path.join(currentFilename, currentBinaryName)]);
             // File inside a .framework should be a dynamically linked shared library.
-            if(checkFileType.stdout.toString().indexOf('dynamically linked shared library') > -1)
-            {
+            if (checkFileType.stdout.toString().indexOf('dynamically linked shared library') > -1) {
               let newFrameworkObject = {
                 originalFileName: currentFilename,
                 originalRelativePath: '',
@@ -52,7 +51,7 @@ export function visitEveryFramework(projectPath) {
   recursivelyFindFrameworks(projectPath);
 
   console.log("Found " + countValidFrameworks + " valid frameworks and " + countInvalidFrameworks + " invalid frameworks after rebuilding the native modules for iOS.");
-  if (foundFrameworks.length<1) {
+  if (foundFrameworks.length < 1) {
     console.log("No valid framework native modules were found. Skipping integrating them into the App.");
     return;
   }
@@ -60,13 +59,13 @@ export function visitEveryFramework(projectPath) {
   for (let i = 0; i < foundFrameworks.length; i++) {
     // Fill the helper fields for each framework.
     let currentFramework = foundFrameworks[i];
-    currentFramework.originalRelativePath = path.relative(projectPath,currentFramework.originalFileName);
+    currentFramework.originalRelativePath = path.relative(projectPath, currentFramework.originalFileName);
 
     // To make each framework name unique while embedding, use a digest of the relative path.
     let hash = crypto.createHash('sha1');
     hash.update(currentFramework.originalRelativePath);
     currentFramework.newFrameworkName = 'node' + hash.digest('hex');
-    currentFramework.newFrameworkFileName = path.join(path.dirname(currentFramework.originalFileName),currentFramework.newFrameworkName+'.framework');
+    currentFramework.newFrameworkFileName = path.join(path.dirname(currentFramework.originalFileName), currentFramework.newFrameworkName + '.framework');
   }
 
   for (let i = 0; i < foundFrameworks.length; i++) {
@@ -74,12 +73,12 @@ export function visitEveryFramework(projectPath) {
     let currentFramework = foundFrameworks[i];
     fs.renameSync(currentFramework.originalFileName, currentFramework.newFrameworkFileName);
     fs.renameSync(
-      path.join(currentFramework.newFrameworkFileName,currentFramework.originalBinaryName),
-      path.join(currentFramework.newFrameworkFileName,currentFramework.newFrameworkName)
+      path.join(currentFramework.newFrameworkFileName, currentFramework.originalBinaryName),
+      path.join(currentFramework.newFrameworkFileName, currentFramework.newFrameworkName)
     );
 
     // Read template Info.plist
-    let plistXmlContents = fs.readFileSync(path.join(__dirname,'plisttemplate.xml')).toString();
+    let plistXmlContents = fs.readFileSync(path.join(__dirname, 'plisttemplate.xml')).toString();
 
     // Replace values with the new bundle name and XCode environment variables.
     plistXmlContents = plistXmlContents
@@ -94,32 +93,32 @@ export function visitEveryFramework(projectPath) {
       .replace(/\{ENV_XCODE_PRODUCT_BUILD_VERSION\}/g, process.env.XCODE_PRODUCT_BUILD_VERSION);
 
     // Use plutil to generate the plist in the binary format.
-    let plistGeneration = spawnSync('plutil',[
+    let plistGeneration = spawnSync('plutil', [
       '-convert',
       'binary1', // Will convert the xml plist to binary.
       '-o',
-      path.join(currentFramework.newFrameworkFileName,'Info.plist'), // target Info.plist path.
+      path.join(currentFramework.newFrameworkFileName, 'Info.plist'), // target Info.plist path.
       '-' // read the input from the process stdin.
     ], {
       input: plistXmlContents
     });
   }
 
-  var frameworkOverrideContents = []
+  var frameworkOverrideContents = [];
   for (let i = 0; i < foundFrameworks.length; i++) {
     // Generate the contents of a JSON file for overriding dlopen calls at runtime.
     let currentFramework = foundFrameworks[i];
     frameworkOverrideContents.push(
       {
         originalpath: currentFramework.originalRelativePath.split(path.sep),
-        newpath: ['..', 'Frameworks', currentFramework.newFrameworkName+'.framework', currentFramework.newFrameworkName]
+        newpath: ['..', 'Frameworks', currentFramework.newFrameworkName + '.framework', currentFramework.newFrameworkName]
       }
     );
   }
   fs.writeFileSync(path.join(projectPath, 'override-dlopen-paths-data.json'), JSON.stringify(frameworkOverrideContents));
 
   // Copy runtime script that will override dlopen paths.
-  fs.copyFileSync(path.join(__dirname,'override-dlopen-paths-preload.js'),path.join(projectPath,'override-dlopen-paths-preload.js'));
+  fs.copyFileSync(path.join(__dirname, 'override-dlopen-paths-preload.js'), path.join(projectPath, 'override-dlopen-paths-preload.js'));
 
   for (let i = 0; i < foundFrameworks.length; i++) {
     // Put an empty file in each of the .node original locations, since some modules check their existence.
@@ -129,7 +128,7 @@ export function visitEveryFramework(projectPath) {
 }
 
 
-if (process.argv.length >=3) {
+if (process.argv.length >= 3) {
   if (fs.existsSync(process.argv[2])) {
     visitEveryFramework(path.normalize(process.argv[2]));
   }
--- mobile/ios/build/override-dlopen-paths-preload.js
+++ mobile/ios/build/override-dlopen-paths-preload.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-var substitutionDataFile = path.join(__dirname,'override-dlopen-paths-data.json');
+var substitutionDataFile = path.join(__dirname, 'override-dlopen-paths-data.json');
 // If the json file exists, override dlopen to load the specified framework paths instead.
 if (fs.existsSync(substitutionDataFile)) {
   var pathSubstitutionData = JSON.parse(fs.readFileSync(substitutionDataFile, 'utf8'));
@@ -23,9 +23,9 @@ if (fs.existsSync(substitutionDataFile)) {
   var old_dlopen = process.dlopen;
   // Override process.dlopen
   process.dlopen = function(_module, _filename) {
-    if( pathSubstitutionDictionary[path.normalize(_filename)] ) {
+    if (pathSubstitutionDictionary[path.normalize(_filename)]) {
       _filename = pathSubstitutionDictionary[path.normalize(_filename)];
     }
-    old_dlopen(_module,_filename);
+    old_dlopen(_module, _filename);
   }
 }
