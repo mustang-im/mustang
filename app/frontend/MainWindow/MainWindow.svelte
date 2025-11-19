937d14aef9f27ecff5de71cfdab203f50d38d3d4
Ben Bucksch <ben.bucksch@beonex.com>
2025-11-19 05:49:18 +0100

    Windows: URL pathname  fails in Router - fixes #854

--- app/frontend/MainWindow/MainWindow.svelte
+++ app/frontend/MainWindow/MainWindow.svelte
@@ -32,7 +32,7 @@
           <NavigationM />
         </Router>
       {:else if $selectedApp}
-        <Router primary={false}>
+        <Router {basepath} primary={false}>
           <Splitter name="sidebar" initialRightRatio={0.25} hasRight={!!sidebar}>
             <AppContentRoutes slot="left"/>
             <vbox flex class="sidebar" slot="right">
@@ -96,6 +96,10 @@
   $: $sidebarApp?.sidebar, setSidebarDebounced();
   $: rtl = rtlLocales.includes(getUILocale()) ? 'rtl' : null;
   categoriesLoaded; /* make sure to import the file, so that that categories load */
+  let basepath;
+  if (window.location.pathname[2] == ":") { // #854 Windows in production mode has URLs `/D:/mail/`
+    basepath = window.location.pathname.substr(0, 3);
+  }
 
   onMount(() => catchErrors(onLoad));
 
