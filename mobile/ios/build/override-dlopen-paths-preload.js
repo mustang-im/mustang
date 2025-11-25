e9fddc8da520be9c1d5047736abab932adf1cf2f
Jeremy Chang <jeremy.chang@beonex.com>
2025-11-25 09:12:01 -0600

    iOS: Build Node Natives as Frameworks (#917)
    * Convert .node to .framework
    * Sign Node Native modules
    * Get the node path
    * Fix building iOS Framework
    * Preload override script
    * Fix path
    * Add docs
    * Delete framework from their build paths
    * Use the correct path
    * Upgrade min OS version to 14.0
    * Rename to embed
    * Format docs
    * Replace var with let/const
    * Add copyright information

---
+++ mobile/ios/build/override-dlopen-paths-preload.js
@@ -0,0 +1,38 @@
+/**
+ * This was imported from
+ * https://github.com/nodejs-mobile/nodejs-mobile-cordova/blob/6cd38b0abc86f0acdbc3e899b36937abf906b119/install/helper-scripts/override-dlopen-paths-preload.js
+ * and modified
+ *
+ * MIT license
+ * Copyright (c) 2018 Janea Systems, Inc. and Contributors
+ */
+
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const substitutionDataFile = path.join(__dirname, 'override-dlopen-paths-data.json');
+// If the json file exists, override dlopen to load the specified framework paths instead.
+if (fs.existsSync(substitutionDataFile)) {
+  const pathSubstitutionData = JSON.parse(fs.readFileSync(substitutionDataFile, 'utf8'));
+
+  const pathSubstitutionDictionary = {};
+  // Build a dictionary to convert paths at runtime, taking current sandboxed paths into account.
+  for (let i = 0; i < pathSubstitutionData.length; i++) {
+    pathSubstitutionDictionary[
+      path.normalize(path.join.apply(null, [__dirname].concat(pathSubstitutionData[i].originalpath)))
+    ] = path.normalize(path.join.apply(null, [__dirname].concat(pathSubstitutionData[i].newpath)));
+  }
+
+  const old_dlopen = process.dlopen;
+  // Override process.dlopen
+  process.dlopen = function(_module, _filename) {
+    if (pathSubstitutionDictionary[path.normalize(_filename)]) {
+      _filename = pathSubstitutionDictionary[path.normalize(_filename)];
+    }
+    old_dlopen(_module, _filename);
+  }
+}
