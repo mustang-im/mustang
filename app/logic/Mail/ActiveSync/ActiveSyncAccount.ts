mustang-im/mustang

commit 535dad46969c21cb86b0064d5c18e5dc3f66ead0

Addressbook/Calendar: Dependent account creation

app/logic/Mail/ActiveSync/ActiveSyncAccount.ts
@@ -7,9 +7,9 @@ import { kMaxCount, ActiveSyncFolder, FolderType } from "./ActiveSyncFolder";
 import { ActiveSyncError } from "./ActiveSyncError";
 import { CreateMIME } from "../SMTP/CreateMIME";
 import { newAddressbookForProtocol} from "../../Contacts/AccountsList/Addressbooks";
-import { ActiveSyncAddressbook } from "../../Contacts/ActiveSync/ActiveSyncAddressbook";
+import type { ActiveSyncAddressbook } from "../../Contacts/ActiveSync/ActiveSyncAddressbook";
 import { newCalendarForProtocol} from "../../Calendar/AccountsList/Calendars";
-import { ActiveSyncCalendar } from "../../Calendar/ActiveSync/ActiveSyncCalendar";
+import type { ActiveSyncCalendar } from "../../Calendar/ActiveSync/ActiveSyncCalendar";
 import { OAuth2 } from "../../Auth/OAuth2";
 import { OAuth2URLs } from "../../Auth/OAuth2URLs";
 import { request2WBXML, WBXML2JSON } from "./WBXML";
@@ -106,12 +106,18 @@ export class ActiveSyncAccount extends MailAccount {
 
     await this.listFolders();
 
-    // `listFolders` will subscribe to new user-added address books and calendars
-    for (let account of this.dependentAccounts()) {
-      if (account instanceof ActiveSyncAddressbook) {
-        account.listContacts();
-      } else if (account instanceof ActiveSyncCalendar) {
-        account.listEvents();
+    // `listFolders()` will subscribe to new user-added addressbooks and calendars
+
+    for (let addressbook of appGlobal.addressbooks) {
+      if (addressbook.mainAccount == this) {
+        addressbook.listContacts()
+          .catch(this.errorCallback);
+      }
+    }
+    for (let calendar of appGlobal.calendars) {
+      if (calendar.mainAccount == this) {
+        calendar.listEvents()
+          .catch(this.errorCallback);
       }
     }
 
@@ -426,13 +432,14 @@ export class ActiveSyncAccount extends MailAccount {
       for (let change of ensureArray(response.Changes?.Add).concat(ensureArray(response.Changes?.Update))) {
         try {
           url.searchParams.set("serverID", change.ServerId);
+          let folderURL = url.toString();
           switch (change.Type) {
-          case FolderType.OtherSpecialFolder:
           case FolderType.Inbox:
           case FolderType.Drafts:
           case FolderType.Trash:
           case FolderType.Sent:
           case FolderType.Outbox:
+          case FolderType.OtherSpecialFolder:
           case FolderType.UserFolder:
             let folder = this.findFolderById(change.ServerId) || this.newFolder();
             folder.fromWBXML(change);
@@ -445,53 +452,41 @@ export class ActiveSyncAccount extends MailAccount {
             await folder.save();
             missingFolders.remove(folder);
             break;
-          case FolderType.Tasks:
-          case FolderType.UserTasks:
-            // Mustang doesn't support tasks yet, fortunately.
+          case FolderType.Contacts:
+          case FolderType.UserContacts:
+            let haveAddressbook = appGlobal.addressbooks.find(addressbook => addressbook.mainAccount == this);
+            let isMainAddressbook = !haveAddressbook; // TODO Determine default addressbook
+            if (!haveAddressbook) {
+              let addressbook = newAddressbookForProtocol("addressbook-activesync") as ActiveSyncAddressbook;
+              addressbook.initFromMainAccount(this);
+              if (!isMainAddressbook) {
+                addressbook.name = sanitize.nonemptylabel(change.DisplayName, addressbook.name);
+              }
+              addressbook.url = sanitize.url(folderURL);
+              addressbook.serverID = sanitize.nonemptystring(change.ServerId);
+              await addressbook.save();
+              appGlobal.addressbooks.add(addressbook);
+            }
             break;
           case FolderType.Calendar:
           case FolderType.UserCalendar:
-            let calendar = appGlobal.calendars.find((calendar: ActiveSyncCalendar) => calendar.mainAccount == this && (calendar.url == url.toString() || calendar.serverID == change.ServerId)) as ActiveSyncCalendar | null;
-            console.log("found the ActS cal again", calendar?.name);
-            if (calendar) {
-              calendar.name = change.DisplayName;
-              calendar.serverID ??= change.ServerId;
-              calendar.icon ??= this.icon; // Migration, remove later
-              calendar.color ??= this.color;
-            } else {
-              calendar = newCalendarForProtocol("calendar-activesync") as ActiveSyncCalendar;
-              calendar.name = change.DisplayName;
-              calendar.serverID = change.ServerId;
-              calendar.url = url.toString();
-              calendar.username = this.username;
-              calendar.workspace = this.workspace;
-              calendar.icon = this.icon;
-              calendar.color = this.color;
-              calendar.mainAccount = this;
+            let haveCalendar = appGlobal.calendars.find(calendar => calendar.mainAccount == this);
+            let isMainCalendar = !haveCalendar; // TODO Determine default calendar
+            if (!haveCalendar) {
+              let calendar = newCalendarForProtocol("calendar-activesync") as ActiveSyncCalendar;
+              calendar.initFromMainAccount(this);
+              if (!isMainCalendar) {
+                calendar.name = sanitize.nonemptylabel(change.DisplayName, calendar.name);
+              }
+              calendar.url = sanitize.url(folderURL);
+              calendar.serverID = sanitize.nonemptystring(change.ServerId);
+              await calendar.save();
               appGlobal.calendars.add(calendar);
             }
             break;
-          case FolderType.Contacts:
-          case FolderType.UserContacts:
-            let addressbook = appGlobal.addressbooks.find((addressbook: ActiveSyncAddressbook) => addressbook.mainAccount == this && (addressbook.url == url.toString() || addressbook.serverID == change.ServerId)) as ActiveSyncAddressbook | null;
-            console.log("found the ActS AB again", addressbook?.name);
-            if (addressbook) {
-              addressbook.name = change.DisplayName;
-              addressbook.serverID ??= change.ServerId;
-              addressbook.icon ??= this.icon; // Migration, remove later
-              addressbook.color ??= this.color;
-            } else {
-              addressbook = newAddressbookForProtocol("addressbook-activesync") as ActiveSyncAddressbook;
-              addressbook.name = change.DisplayName;
-              addressbook.serverID = change.ServerId;
-              addressbook.url = url.toString();
-              addressbook.username = this.username;
-              addressbook.workspace = this.workspace;
-              addressbook.icon = this.icon;
-              addressbook.color = this.color;
-              addressbook.mainAccount = this;
-              appGlobal.addressbooks.add(addressbook);
-            }
+          case FolderType.Tasks:
+          case FolderType.UserTasks:
+            // Mustang doesn't support tasks yet, fortunately.
             break;
           }
         } catch (ex) {
@@ -508,15 +503,15 @@ export class ActiveSyncAccount extends MailAccount {
           }
           let url = new URL(this.url);
           url.searchParams.set("serverID", deletion.ServerId);
-          let addressbook = appGlobal.addressbooks.find((addressbook: ActiveSyncAddressbook) => addressbook.mainAccount == this && (addressbook.url == url.toString() || addressbook.serverID == deletion.ServerId)) as ActiveSyncAddressbook | undefined;
+          let addressbook = appGlobal.addressbooks.find((addressbook: ActiveSyncAddressbook) => addressbook.mainAccount == this && addressbook.serverID == deletion.ServerId) as ActiveSyncAddressbook | undefined;
           if (addressbook) {
             this.removePingable(addressbook);
-            addressbook.deleteIt();
+            await addressbook.deleteIt();
           }
-          let calendar = appGlobal.calendars.find((calendar: ActiveSyncCalendar) => calendar.mainAccount == this && (calendar.url == url.toString() || calendar.serverID == deletion.ServerId)) as ActiveSyncCalendar | undefined;
+          let calendar = appGlobal.calendars.find((calendar: ActiveSyncCalendar) => calendar.mainAccount == this && calendar.serverID == deletion.ServerId) as ActiveSyncCalendar | undefined;
           if (calendar) {
             this.removePingable(calendar);
-            calendar.deleteIt();
+            await calendar.deleteIt();
           }
         } catch (ex) {
           this.errorCallback(ex);

app/logic/Abstract/Account.ts
@@ -104,6 +104,15 @@ export class Account extends Observable {
   dependentAccounts(): Collection<Account> {
     return getAllAccounts().filter(acc => acc.mainAccount == this);
   }
+  initFromMainAccount(main: Account) {
+    this.mainAccount = main;
+    this.name = main.name;
+    this.url = main.url;
+    this.color = main.color;
+    this.icon = main.icon;
+    this.workspace = main.workspace;
+    this.username = main.username;
+  }
 
   /** The cookie store to use when loading this account. For `<webview partition="persist:...">` */
   get webSessionID(): string | null {

app/logic/Chat/Graph/GraphChatAccount.ts
@@ -8,8 +8,11 @@ import { MapColl } from 'svelte-collections';
 
 export class GraphChatAccount extends ChatAccount {
   readonly protocol: string = "chat-graph";
-  account: GraphAccount;
   readonly chats = new MapColl<ChatPerson | Group, GraphChatRoom>;
+
+  get account(): GraphAccount {
+    return this.mainAccount as GraphAccount;
+  }
 
   /** Login to this account on the server. Opens network connection.
    * You must call this after creating the object and having set its properties.

app/logic/Mail/AutoConfig/saveConfig.ts
@@ -50,14 +50,14 @@ export async function saveConfig(config: MailAccount, emailAddress: string, pass
   await config.saveAll();
 }
 
-export async function saveChildService(config: Account, parent: Account): Promise<void> {
-  config.realname = parent.realname;
-  config.username = parent.username;
-  config.password = parent.password;
-  config.name = parent.name;
-  config.color = parent.color;
-  config.icon = parent.icon;
-  config.mainAccount = parent;
+export async function saveChildService(config: Account, main: Account): Promise<void> {
+  config.realname = main.realname;
+  config.username = main.username;
+  config.password = main.password;
+  config.name = main.name;
+  config.color = main.color;
+  config.icon = main.icon;
+  config.mainAccount = main;
   addAccountToGlobal(config);
   await config.save();
 }

app/logic/Mail/EWS/EWSAccount.ts
@@ -49,6 +49,21 @@ export class EWSAccount extends MailAccount {
 
   async verifyLogin(): Promise<void> {
     await this.loginCommon(true);
+
+    let query = {
+      m$FindFolder: {
+        Traversal: "Shallow",
+        m$FolderShape: {
+          t$BaseShape: "IdOnly",
+        },
+        m$ParentFolderIds: {
+          t$DistinguishedFolderId: [{
+            Id: "msgfolderroot",
+          }],
+        },
+      },
+    };
+    await this.callEWS(query);
   }
 
   protected async loginCommon(interactive: boolean): Promise<void> {
@@ -62,7 +77,6 @@ export class EWSAccount extends MailAccount {
       this.oAuth2.subscribe(() => this.notifyObservers());
       await this.oAuth2.login(interactive);
     }
-    await this.listFolders();
   }
 
   async login(interactive: boolean): Promise<void> {
@@ -70,15 +84,20 @@ export class EWSAccount extends MailAccount {
     await super.login(interactive);
     await this.loginCommon(interactive);
 
+    await this.listFolders();
+
+    // `listFolders()` will subscribe to new user-added addressbooks and calendars
+
     for (let addressbook of appGlobal.addressbooks) {
       if (addressbook.mainAccount == this) {
-        await addressbook.listContacts();
+        addressbook.listContacts()
+          .catch(this.errorCallback);
       }
     }
-
     for (let calendar of appGlobal.calendars) {
       if (calendar.mainAccount == this) {
-        await calendar.listEvents();
+        calendar.listEvents()
+          .catch(this.errorCallback);
       }
     }
 
@@ -538,23 +557,19 @@ export class EWSAccount extends MailAccount {
       // Link (until #155) or create the default address book.
       // TODO: Support user-added address books. Compare FolderId.
       // FolderClass will be IPF.Contacts but some internal folders exist
-      if (folder.DistinguishedFolderId == "contacts") {
-        let addressbook = appGlobal.addressbooks.find(addressbook => addressbook.mainAccount == this) as EWSAddressbook | null;
-        if (!addressbook) {
-          addressbook = newAddressbookForProtocol("addressbook-ews") as EWSAddressbook;
-          addressbook.name = this.name;
-          addressbook.url = this.url;
-          addressbook.username = this.username;
-          addressbook.workspace = this.workspace;
-          addressbook.icon = this.icon;
-          addressbook.color = this.color;
-          addressbook.mainAccount = this;
+      let isMainAddressbook = folder.DistinguishedFolderId == "contacts";
+      if (isMainAddressbook) {
+        let haveAddressbook = appGlobal.addressbooks.find(addressbook => addressbook.mainAccount == this);
+        if (!haveAddressbook) {
+          let addressbook = newAddressbookForProtocol("addressbook-ews") as EWSAddressbook;
+          addressbook.initFromMainAccount(this);
+          if (!isMainAddressbook) {
+            addressbook.name = sanitize.nonemptylabel(folder.DisplayName, addressbook.name);
+          }
+          addressbook.folderID = sanitize.nonemptystring(folder.FolderId.Id);
+          await addressbook.save();
           appGlobal.addressbooks.add(addressbook);
         }
-        addressbook.icon ??= this.icon; // Migration, remove later
-        addressbook.color ??= this.color;
-        addressbook.folderID ??= folder.FolderId.Id;
-        await addressbook.save();
       }
     }
     for (let folder of ensureArray(result.RootFolder.Folders.CalendarFolder)) {
@@ -564,23 +579,19 @@ export class EWSAccount extends MailAccount {
       // FolderClass will be IPF.Appointment except for Birthdays calendar
       // which is IPF.Appointment.Birthdays
       // Holidays calendar is read-only but it's hard to tell from the API...
-      if (folder.DistinguishedFolderId == "calendar") {
-        let calendar = appGlobal.calendars.find(calendar => calendar.mainAccount == this) as EWSCalendar | null;
-        if (!calendar) {
-          calendar = newCalendarForProtocol("calendar-ews") as EWSCalendar;
-          calendar.name = this.name;
-          calendar.url = this.url;
-          calendar.username = this.username;
-          calendar.workspace = this.workspace;
-          calendar.icon = this.icon;
-          calendar.color = this.color;
-          calendar.mainAccount = this;
+      let isMainCalendar = folder.DistinguishedFolderId == "calendar";
+      if (isMainCalendar) {
+        let haveCalendar = appGlobal.calendars.find(calendar => calendar.mainAccount == this);
+        if (!haveCalendar) {
+          let calendar = newCalendarForProtocol("calendar-ews") as EWSCalendar;
+          calendar.initFromMainAccount(this);
+          if (!isMainCalendar) {
+            calendar.name = sanitize.nonemptylabel(folder.DisplayName, calendar.name);
+          }
+          calendar.folderID = sanitize.nonemptystring(folder.FolderId.Id);
+          await calendar.save();
           appGlobal.calendars.add(calendar);
         }
-        calendar.icon ??= this.icon; // Migration, remove later
-        calendar.color ??= this.color;
-        calendar.folderID ??= folder.FolderId.Id;
-        await calendar.save();
       }
     }
   }

app/logic/Mail/EWS/EWSFolder.ts
@@ -22,7 +22,7 @@ export class EWSFolder extends Folder {
 
   fromXML(xmljs: any) {
     this.id = sanitize.nonemptystring(xmljs.FolderId.Id);
-    this.name = sanitize.nonemptystring(xmljs.DisplayName);
+    this.name = sanitize.nonemptylabel(xmljs.DisplayName);
     this.countTotal = sanitize.integer(xmljs.TotalCount);
     this.countUnread = sanitize.integer(xmljs.UnreadCount);
     switch (xmljs.DistinguishedFolderId) { // allowed to be null

app/logic/Mail/Graph/GraphAccount.ts
@@ -54,46 +54,51 @@ export class GraphAccount extends MailAccount {
     inbox.startPolling();
 
     /*
-    let addressbook = appGlobal.addressbooks.find((addressbook: GraphAddressbook) => addressbook.protocol == "addressbook-graph" && addressbook.url == this.url && addressbook.username == this.username) as GraphAddressbook | undefined;
-    if (!addressbook) {
-      addressbook = newAddressbookForProtocol("addressbook-graph") as GraphAddressbook;
+    let haveAddressbook = appGlobal.addressbooks.find(acc => acc.mainAccount == this);
+    if (!haveAddressbook) {
+      let addressbook = newAddressbookForProtocol("addressbook-graph") as GraphAddressbook;
       addressbook.name = this.name;
       addressbook.url = this.url;
-      addressbook.username = this.username;
-      addressbook.workspace = this.workspace;
-      addressbook.icon = this.icon;
-      addressbook.color = this.color;
+      addressbook.initFromMainAccount(this);
+      await addressbook.save();
       appGlobal.addressbooks.add(addressbook);
     }
-    addressbook.account = this;
-    await addressbook.listContacts();
 
-    let calendar = appGlobal.calendars.find((calendar: GraphCalendar) => calendar.protocol == "calendar-graph" && calendar.url == this.url && calendar.username == this.username) as GraphCalendar | undefined;
-    if (!calendar) {
-      calendar = newCalendarForProtocol("calendar-graph") as GraphCalendar;
-      calendar.name = this.name;
-      calendar.url = this.url;
-      calendar.username = this.username;
-      calendar.workspace = this.workspace;
-      calendar.icon = this.icon;
-      calendar.color = this.color;
+    let haveCalendar = appGlobal.chatAccounts.find(acc => acc.mainAccount == this);
+    if (!haveCalendar) {
+      let calendar = newCalendarForProtocol("calendar-graph") as GraphCalendar;
+      calendar.initFromMainAccount(this);
+      await calendar.save();
       appGlobal.calendars.add(calendar);
     }
-    calendar.account = this;
-    await calendar.listEvents();
     */
 
-    let chatAccount = appGlobal.chatAccounts.find((calendar: GraphChatAccount) => calendar.protocol == "chat-graph" && calendar.url == this.url && calendar.username == this.username) as GraphChatAccount | undefined;
-    if (!chatAccount) {
-      chatAccount = newChatAccountForProtocol("chat-graph") as GraphChatAccount;
-      chatAccount.name = this.name;
-      chatAccount.url = this.url;
-      chatAccount.username = this.username;
-      chatAccount.workspace = this.workspace;
+    let haveChatAccount = appGlobal.chatAccounts.find(acc => acc.mainAccount == this);
+    if (!haveChatAccount) {
+      let chatAccount = newChatAccountForProtocol("chat-graph") as GraphChatAccount;
+      chatAccount.initFromMainAccount(this);
+      await chatAccount.save();
       appGlobal.chatAccounts.add(chatAccount);
     }
-    chatAccount.account = this;
-    await chatAccount.listChats();
+
+    for (let addressbook of appGlobal.addressbooks) {
+      if (addressbook.mainAccount == this) {
+        addressbook.listContacts()
+          .catch(this.errorCallback);
+      }
+    }
+    for (let calendar of appGlobal.calendars) {
+      if (calendar.mainAccount == this) {
+        await calendar.listEvents()
+          .catch(this.errorCallback);
+      }
+    }
+    for (let chatAccount of appGlobal.chatAccounts) {
+      if (chatAccount.mainAccount == this) {
+        await chatAccount.listChats()
+          .catch(this.errorCallback);
+      }
+    }
   }
 
   async verifyLogin(): Promise<void> {

app/logic/Mail/JMAP/JMAPAccount.ts
@@ -53,6 +53,17 @@ export class JMAPAccount extends MailAccount {
     let inbox = this.inbox as JMAPFolder;
     assert(inbox, "Inbox not found");
     inbox.startPolling();
+
+    for (let addressbook of appGlobal.addressbooks) {
+      if (addressbook.mainAccount == this) {
+        await addressbook.listContacts();
+      }
+    }
+    for (let calendar of appGlobal.calendars) {
+      if (calendar.mainAccount == this) {
+        await calendar.listEvents();
+      }
+    }
   }
 
   async verifyLogin(): Promise<void> {

commit 46fda6b21b0ca181b0ce3ddf83523a364bc447af

ActiveSync: Detect user calendars and addressbooks again
Regression from 535dad46969c21cb86b0064d5c18e5dc3f66ead0

app/logic/Mail/ActiveSync/ActiveSyncAccount.ts
@@ -428,11 +428,8 @@ export class ActiveSyncAccount extends MailAccount {
         // We're syncing from scratch, so we may have stale folders.
         missingFolders = this.getAllFolders();
       }
-      let url = new URL(this.url);
       for (let change of ensureArray(response.Changes?.Add).concat(ensureArray(response.Changes?.Update))) {
         try {
-          url.searchParams.set("serverID", change.ServerId);
-          let folderURL = url.toString();
           switch (change.Type) {
           case FolderType.Inbox:
           case FolderType.Drafts:
@@ -454,35 +451,33 @@ export class ActiveSyncAccount extends MailAccount {
             break;
           case FolderType.Contacts:
           case FolderType.UserContacts:
-            let haveAddressbook = appGlobal.addressbooks.find(addressbook => addressbook.mainAccount == this);
-            let isMainAddressbook = !haveAddressbook; // TODO Determine default addressbook
-            if (!haveAddressbook) {
-              let addressbook = newAddressbookForProtocol("addressbook-activesync") as ActiveSyncAddressbook;
+            let isMainAddressbook = change.Type == FolderType.Contacts;
+            let addressbook = appGlobal.addressbooks.find((addressbook: ActiveSyncAddressbook) => addressbook.mainAccount == this && addressbook.serverID == change.ServerId) as ActiveSyncAddressbook | null;
+            if (!addressbook) {
+              addressbook = newAddressbookForProtocol("addressbook-activesync") as ActiveSyncAddressbook;
               addressbook.initFromMainAccount(this);
-              if (!isMainAddressbook) {
-                addressbook.name = sanitize.nonemptylabel(change.DisplayName, addressbook.name);
-              }
-              addressbook.url = sanitize.url(folderURL);
               addressbook.serverID = sanitize.nonemptystring(change.ServerId);
-              await addressbook.save();
-              appGlobal.addressbooks.add(addressbook);
             }
+            if (!isMainAddressbook) {
+              addressbook.name = sanitize.nonemptylabel(change.DisplayName, addressbook.name);
+            }
+            await addressbook.save();
+            appGlobal.addressbooks.add(addressbook);
             break;
           case FolderType.Calendar:
           case FolderType.UserCalendar:
-            let haveCalendar = appGlobal.calendars.find(calendar => calendar.mainAccount == this);
-            let isMainCalendar = !haveCalendar; // TODO Determine default calendar
-            if (!haveCalendar) {
-              let calendar = newCalendarForProtocol("calendar-activesync") as ActiveSyncCalendar;
+            let isMainCalendar = change.Type == FolderType.Calendar;
+            let calendar = appGlobal.calendars.find((calendar: ActiveSyncCalendar) => calendar.mainAccount == this && calendar.serverID == change.ServerId) as ActiveSyncCalendar | null;
+            if (!calendar) {
+              calendar = newCalendarForProtocol("calendar-activesync") as ActiveSyncCalendar;
               calendar.initFromMainAccount(this);
-              if (!isMainCalendar) {
-                calendar.name = sanitize.nonemptylabel(change.DisplayName, calendar.name);
-              }
-              calendar.url = sanitize.url(folderURL);
               calendar.serverID = sanitize.nonemptystring(change.ServerId);
-              await calendar.save();
-              appGlobal.calendars.add(calendar);
             }
+            if (!isMainCalendar) {
+              calendar.name = sanitize.nonemptylabel(change.DisplayName, calendar.name);
+            }
+            await calendar.save();
+            appGlobal.calendars.add(calendar);
             break;
           case FolderType.Tasks:
           case FolderType.UserTasks:
@@ -501,8 +496,6 @@ export class ActiveSyncAccount extends MailAccount {
             await this.storage.deleteFolder(folder);
             folder.removeFromParent();
           }
-          let url = new URL(this.url);
-          url.searchParams.set("serverID", deletion.ServerId);
           let addressbook = appGlobal.addressbooks.find((addressbook: ActiveSyncAddressbook) => addressbook.mainAccount == this && addressbook.serverID == deletion.ServerId) as ActiveSyncAddressbook | undefined;
           if (addressbook) {
             this.removePingable(addressbook);