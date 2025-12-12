6b9ab9de82d77a65bbf56d083e1e8c57d4c80766
Jeremy Chang <jeremy.chang@beonex.com>
2025-12-12 13:51:22 -0600

    Remove comments from LiveKitEnum

--- app/logic/Meet/LiveKit/LiveKitEnums.ts
+++ app/logic/Meet/LiveKit/LiveKitEnums.ts
@@ -14,87 +14,14 @@ export namespace Track {
 }
 
 export enum ParticipantEvent {
-  /**
-   * When a new track is published to room *after* the local
-   * participant has joined. It will not fire for tracks that are already published.
-   *
-   * A track published doesn't mean the participant has subscribed to it. It's
-   * simply reflecting the state of the room.
-   *
-   * args: ([[RemoteTrackPublication]])
-   */
   TrackPublished = 'trackPublished',
-
-  /**
-   * Successfully subscribed to the [[RemoteParticipant]]'s track.
-   * This event will **always** fire as long as new tracks are ready for use.
-   *
-   * args: ([[RemoteTrack]], [[RemoteTrackPublication]])
-   */
   TrackSubscribed = 'trackSubscribed',
-
-  /**
-   * A [[RemoteParticipant]] has unpublished a track
-   *
-   * args: ([[RemoteTrackPublication]])
-   */
   TrackUnpublished = 'trackUnpublished',
-
-  /**
-   * A subscribed track is no longer available. Clients should listen to this
-   * event and ensure they detach tracks.
-   *
-   * args: ([[RemoteTrack]], [[RemoteTrackPublication]])
-   */
   TrackUnsubscribed = 'trackUnsubscribed',
-
-  /**
-   * A track that was muted, fires on both [[RemoteParticipant]]s and [[LocalParticipant]]
-   *
-   * args: ([[TrackPublication]])
-   */
   TrackMuted = 'trackMuted',
-
-  /**
-   * A track that was unmuted, fires on both [[RemoteParticipant]]s and [[LocalParticipant]]
-   *
-   * args: ([[TrackPublication]])
-   */
   TrackUnmuted = 'trackUnmuted',
-
-  /**
-   * Participant metadata is a simple way for app-specific state to be pushed to
-   * all users.
-   * When RoomService.UpdateParticipantMetadata is called to change a participant's
-   * state, *all*  participants in the room will fire this event.
-   * To access the current metadata, see [[Participant.metadata]].
-   *
-   * args: (prevMetadata: string)
-   *
-   */
   ParticipantMetadataChanged = 'participantMetadataChanged',
-
-  /**
-   * Participant's display name changed
-   *
-   * args: (name: string, [[Participant]])
-   *
-   */
   ParticipantNameChanged = 'participantNameChanged',
-
-  /**
-   * Has speaking status changed for the current participant
-   *
-   * args: (speaking: boolean)
-   */
   IsSpeakingChanged = 'isSpeakingChanged',
-
-  /**
-   * Participant attributes is an app-specific key value state to be pushed to
-   * all users.
-   * When a participant's attributes changed, this event will be emitted with the changed attributes
-   * args: (changedAttributes: [[Record<string, string]])
-   */
   AttributesChanged = 'attributesChanged',
-
 }
