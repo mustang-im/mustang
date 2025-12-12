ae023db01815f4175b42cf41638d3324cfb831bf
Jeremy Chang <jeremy.chang@beonex.com>
2025-12-12 13:49:32 -0600

    Removed unused Enum

--- app/logic/Meet/LiveKit/LiveKitEnums.ts
+++ app/logic/Meet/LiveKit/LiveKitEnums.ts
@@ -34,13 +34,6 @@ export enum ParticipantEvent {
   TrackSubscribed = 'trackSubscribed',
 
   /**
-   * Could not subscribe to a track
-   *
-   * args: (track sid)
-   */
-  TrackSubscriptionFailed = 'trackSubscriptionFailed',
-
-  /**
    * A [[RemoteParticipant]] has unpublished a track
    *
    * args: ([[RemoteTrackPublication]])
@@ -70,40 +63,6 @@ export enum ParticipantEvent {
   TrackUnmuted = 'trackUnmuted',
 
   /**
-   * A local track was published successfully. This event is helpful to know
-   * when to update your local UI with the newly published track.
-   *
-   * args: ([[LocalTrackPublication]])
-   */
-  LocalTrackPublished = 'localTrackPublished',
-
-  /**
-   * A local track was unpublished. This event is helpful to know when to remove
-   * the local track from your UI.
-   *
-   * When a user stops sharing their screen by pressing "End" on the browser UI,
-   * this event will also fire.
-   *
-   * args: ([[LocalTrackPublication]])
-   */
-  LocalTrackUnpublished = 'localTrackUnpublished',
-
-  /**
-   * A local track has been constrained by cpu.
-   * This event is useful to know when to reduce the capture resolution of the track.
-   *
-   * This event is emitted on the local participant.
-   *
-   * args: ([[LocalVideoTrack]], [[LocalTrackPublication]])
-   */
-  LocalTrackCpuConstrained = 'localTrackCpuConstrained',
-
-  /**
-   * @internal
-   */
-  LocalSenderCreated = 'localSenderCreated',
-
-  /**
    * Participant metadata is a simple way for app-specific state to be pushed to
    * all users.
    * When RoomService.UpdateParticipantMetadata is called to change a participant's
@@ -124,92 +83,11 @@ export enum ParticipantEvent {
   ParticipantNameChanged = 'participantNameChanged',
 
   /**
-   * Data received from this participant as sender.
-   * Data packets provides the ability to use LiveKit to send/receive arbitrary payloads.
-   * All participants in the room will receive the messages sent to the room.
-   *
-   * args: (payload: Uint8Array, kind: [[DataPacket_Kind]])
-   */
-  DataReceived = 'dataReceived',
-
-  /**
-   * SIP DTMF tones received from this participant as sender.
-   *
-   * args: (dtmf: [[DataPacket_Kind]])
-   */
-  SipDTMFReceived = 'sipDTMFReceived',
-
-  /**
-   * Transcription received from this participant as data source.
-   * @beta
-   */
-  TranscriptionReceived = 'transcriptionReceived',
-
-  /**
    * Has speaking status changed for the current participant
    *
    * args: (speaking: boolean)
    */
   IsSpeakingChanged = 'isSpeakingChanged',
-
-  /**
-   * Connection quality was changed for a Participant. It'll receive updates
-   * from the local participant, as well as any [[RemoteParticipant]]s that we are
-   * subscribed to.
-   *
-   * args: (connectionQuality: [[ConnectionQuality]])
-   */
-  ConnectionQualityChanged = 'connectionQualityChanged',
-
-  /**
-   * StreamState indicates if a subscribed track has been paused by the SFU
-   * (typically this happens because of subscriber's bandwidth constraints)
-   *
-   * When bandwidth conditions allow, the track will be resumed automatically.
-   * TrackStreamStateChanged will also be emitted when that happens.
-   *
-   * args: (pub: [[RemoteTrackPublication]], streamState: [[Track.StreamState]])
-   */
-  TrackStreamStateChanged = 'trackStreamStateChanged',
-
-  /**
-   * One of subscribed tracks have changed its permissions for the current
-   * participant. If permission was revoked, then the track will no longer
-   * be subscribed. If permission was granted, a TrackSubscribed event will
-   * be emitted.
-   *
-   * args: (pub: [[RemoteTrackPublication]],
-   *        status: [[TrackPublication.SubscriptionStatus]])
-   */
-  TrackSubscriptionPermissionChanged = 'trackSubscriptionPermissionChanged',
-
-  /**
-   * One of the remote participants publications has changed its subscription status.
-   *
-   */
-  TrackSubscriptionStatusChanged = 'trackSubscriptionStatusChanged',
-
-  /**
-   * a local track has been constrained by cpu
-   */
-  TrackCpuConstrained = 'trackCpuConstrained',
-
-  // fired only on LocalParticipant
-  /** @internal */
-  MediaDevicesError = 'mediaDevicesError',
-
-  // fired only on LocalParticipant
-  /** @internal */
-  AudioStreamAcquired = 'audioStreamAcquired',
-
-  /**
-   * A participant's permission has changed.
-   * args: (prevPermissions: [[ParticipantPermission]])
-   */
-  ParticipantPermissionsChanged = 'participantPermissionsChanged',
-
-  /** @internal */
-  PCTrackAdded = 'pcTrackAdded',
 
   /**
    * Participant attributes is an app-specific key value state to be pushed to
@@ -219,16 +97,4 @@ export enum ParticipantEvent {
    */
   AttributesChanged = 'attributesChanged',
 
-  /**
-   * fired on local participant only, when the first remote participant has subscribed to the track specified in the payload
-   */
-  LocalTrackSubscribed = 'localTrackSubscribed',
-
-  /** only emitted on local participant */
-  ChatMessage = 'chatMessage',
-
-  /**
-   * Emitted when the participant's state changes to ACTIVE and is ready to send/receive data messages
-   */
-  Active = 'active',
 }
