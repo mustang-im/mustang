31196f5388bd1d2c82e3f4836a6018273bf079df
Jeremy Chang <jeremy.chang@beonex.com>
2025-12-17 16:46:17 -0600

    Initialize class

--- mobile/android/app/src/main/java/im/mustang/capa/Logger.kt
+++ mobile/android/app/src/main/java/im/mustang/capa/Logger.kt
@@ -4,14 +4,35 @@ import com.getcapacitor.JSObject
 import com.getcapacitor.Plugin
 import com.getcapacitor.annotation.CapacitorPlugin
 
-@CapacitorPlugin
-object Logger: Plugin() {
-    fun consoleLog(log: String) {
+@CapacitorPlugin(name = "Logger")
+class Logger : Plugin() {
+
+    // This method is called by Capacitor when the plugin is loaded.
+    // It's the perfect place to initialize.
+    override fun load() {
+        super.load()
+        // Save the initialized instance of this plugin to the companion object.
+        instance = this
+    }
+
+    companion object {
+        private var instance: Logger? = null
+
+        fun consoleLog(log: String) {
+            instance?.log(log)
+        }
+
+        fun consoleError(error: String) {
+            instance?.error(error);
+        }
+    }
+
+    private fun log(log: String) {
         val logJS = JSObject()
         logJS.put("log", log)
         notifyListeners("capLog", logJS)
     }
-    fun consoleError(error: String) {
+    private fun error(error: String) {
         val errorJS = JSObject()
         errorJS.put("error", error)
         notifyListeners("capError", errorJS)
