72987fdbecc2757f64571090367c7c6d48db5356
Jeremy Chang <jeremy.chang@beonex.com>
2025-12-21 15:46:22 -0600

    Download libnode

---
+++ mobile/android/scripts/download-libnode.sh
@@ -0,0 +1,11 @@
+# Run from mobile/android
+DOWNLOAD_URL="https://github.com/mustang-im/nodejs-mobile/releases/download/v22.9.0/nodejs-mobile-v22.9.0-android.zip"
+TMP_ZIP_FILE="libnode-download.zip"
+
+echo "Downloading $DOWNLOAD_URL"
+curl -L -o $TMP_ZIP_FILE "$DOWNLOAD_URL"
+
+unzip -o $TMP_ZIP_FILE -d ./app/src/main/cpp/libnode
+rm -r $TMP_ZIP_FILE ./include
+
+echo "Download and extraction complete."
