51597541f8d1fa47334bc072fee1f48a00400754
Jeremy Chang <jeremy.chang@beonex.com>
2025-12-21 16:08:32 -0600

    LoadLibrary in Coroutine

--- mobile/android/app/src/main/java/im/mustang/capa/nodeprocess/NodeProcess.kt
+++ mobile/android/app/src/main/java/im/mustang/capa/nodeprocess/NodeProcess.kt
@@ -1,4 +1,22 @@
 package im.mustang.capa.nodeprocess
 
-class NodeProcess {
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+
+class NodeProcess: ViewModel() {
+    companion object {
+        private const val TAG = "NodeProcess"
+        private var librariesLoaded = false
+    }
+
+    suspend fun loadLibraries() {
+        if (librariesLoaded) return
+        withContext(Dispatchers.IO) {
+            System.loadLibrary("node-process")
+            System.loadLibrary("node")
+            librariesLoaded = true
+        }
+    }
 }
