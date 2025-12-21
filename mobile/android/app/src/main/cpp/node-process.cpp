613a857bdbfc3492da95f55577da26d60702b90a
Jeremy Chang <jeremy.chang@beonex.com>
2025-12-21 15:56:10 -0600

    Add node to project

--- mobile/android/app/src/main/cpp/node-process.cpp
+++ mobile/android/app/src/main/cpp/node-process.cpp
@@ -1,17 +1,1 @@
-// Write C++ code here.
-//
-// Do not forget to dynamically load the C++ library into your application.
-//
-// For instance,
-//
-// In MainActivity.java:
-//    static {
-//       System.loadLibrary("capa");
-//    }
-//
-// Or, in MainActivity.kt:
-//    companion object {
-//      init {
-//         System.loadLibrary("capa")
-//      }
-//    }
+#include <node.h>
