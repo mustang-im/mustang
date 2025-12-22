a6217c279537bf1cd750634631a08e5100578639
Jeremy Chang <jeremy.chang@beonex.com>
2025-12-22 16:08:04 -0600

    C++ Node.js Starter

--- mobile/android/app/src/main/cpp/node-process.cpp
+++ mobile/android/app/src/main/cpp/node-process.cpp
@@ -1,1 +1,50 @@
+#include <jni.h>
 #include <node.h>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+jint startNode(JNIEnv *env,
+               jobject /* this */,
+               jobjectArray args) {
+    jsize argc = env->GetArrayLength(args);
+
+    vector<char*> argv(argc);
+
+    for (int i = 0; i < argc; i++) {
+        jstring arg = static_cast<jstring>(env->GetObjectArrayElement(args, i));
+        jsize argLen = env->GetStringUTFLength(arg);
+
+
+        argv[i] = new char[argLen + 1];
+
+        env->GetStringUTFRegion(arg, 0, argLen, argv[i]);
+        argv[i][argLen] = '\0';
+
+        env->DeleteLocalRef(arg);
+    }
+
+    return jint(node::Start(argc, argv.data()));
+}
+
+JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+    JNIEnv* env;
+    if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK) {
+        return JNI_ERR;
+    }
+
+    // Find the NodeProcess class
+    jclass c = env->FindClass("im/mustang/capa/nodeprocess/NodeProcess");
+    if (c == nullptr) return JNI_ERR;
+
+    // Register your class' native methods.
+    static const JNINativeMethod methods[] = {
+            {"startNode", "([Ljava/lang/String;)I", reinterpret_cast<jint*>(startNode)},
+    };
+
+    int rc = env->RegisterNatives(c, methods, sizeof(methods)/sizeof(JNINativeMethod));
+    if (rc != JNI_OK) return rc;
+
+    return JNI_VERSION_1_6;
+}
