aa685ea9656228646b8b861c65451602c7aea55c
Jeremy Chang <jeremy.chang@beonex.com>
2025-12-22 16:08:28 -0600

    Kotlin Node.js Starter

--- mobile/android/app/src/main/java/im/mustang/capa/MainActivity.kt
+++ mobile/android/app/src/main/java/im/mustang/capa/MainActivity.kt
@@ -1,5 +1,14 @@
 package im.mustang.capa
 
+import android.os.Bundle
 import com.getcapacitor.BridgeActivity
+import im.mustang.capa.nodeprocess.NodeProcess
 
-class MainActivity : BridgeActivity()
+class MainActivity : BridgeActivity() {
+    private lateinit var nodeProcess: NodeProcess
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        nodeProcess = NodeProcess(applicationContext)
+        nodeProcess.start()
+    }
+}
--- mobile/android/app/src/main/java/im/mustang/capa/nodeprocess/NodeProcess.kt
+++ mobile/android/app/src/main/java/im/mustang/capa/nodeprocess/NodeProcess.kt
@@ -1,13 +1,31 @@
 package im.mustang.capa.nodeprocess
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import android.content.res.AssetManager
+import android.util.Log
+import java.io.File
 
 
-class NodeProcess: ViewModel() {
+class NodeProcess(val context: Context): ViewModel() {
+    private val TAG = "NodeProcess"
+    private lateinit var job: Job
+    private val MainJS = "index.mjs"
+    private val nodeDir = "nodejs"
+    // Don't initialize until needed
+    private val assetManager: AssetManager by lazy {
+        context.assets
+    }
+    private val filesDir: File by lazy {
+        context.filesDir
+    }
+
     companion object {
-        private const val TAG = "NodeProcess"
         private var librariesLoaded = false
     }
 
@@ -17,6 +35,27 @@ class NodeProcess: ViewModel() {
             System.loadLibrary("node-process")
             System.loadLibrary("node")
             librariesLoaded = true
+        }
+    }
+
+    private external fun startNode(args: Array<String>): Int
+
+    fun start() {
+        if (this::job.isInitialized && job.isActive) return
+        job = viewModelScope.launch(Dispatchers.IO) {
+            try {
+                loadLibraries()
+                val from = "${filesDir.absoluteFile}/public/$nodeDir"
+                val to = "${filesDir.absoluteFile}/$nodeDir"
+                val copyResult = FileOperations.copyAssetsDir(assetManager, from, to)
+                if (!copyResult) {
+                    throw Exception("Error copying assets")
+                }
+                val mainJS = File(to, MainJS)
+                startNode(arrayOf("node", mainJS.absolutePath))
+            } catch (e: Exception) {
+                Log.e(TAG, "Error starting node", e)
+            }
         }
     }
 }
