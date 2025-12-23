8d0adbfc5afacea748da456cfd684928babb7583
Jeremy Chang <jeremy.chang@beonex.com>
2025-12-23 12:33:21 -0600

    Let File Operations throw errors

--- mobile/android/app/src/main/java/im/mustang/capa/nodeprocess/FileOperations.kt
+++ mobile/android/app/src/main/java/im/mustang/capa/nodeprocess/FileOperations.kt
@@ -1,7 +1,6 @@
 package im.mustang.capa.nodeprocess
 
 import android.content.res.AssetManager
-import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
@@ -11,67 +10,38 @@ import java.io.File
 import java.io.FileOutputStream
 
 object FileOperations {
-    private const val TAG = "FileOperations"
+    // Asynchronous copying throws error if any file fails to copy
+    suspend fun copyAssetsDir(assetManager: AssetManager, from: String, to: String): Unit = coroutineScope {
+        // Get the list of files in the assets directory
+        val files = assetManager.list(from)
 
-    // Asynchronous copying
-    suspend fun copyAssetsDir(assetManager: AssetManager, from: String, to: String): Boolean = coroutineScope {
-        try {
-            // Get the list of files in the assets directory
-            // if there's no files return
-            val files = assetManager.list(from)
-            val copyResults = ArrayList<Boolean>()
-
-            if (files == null) {
-                Log.e(TAG, "Error listing assets directory: $from")
-                return@coroutineScope false
-            }
-
-            if (files.isEmpty()) {
-                //If it's a file, it won't have any assets "inside" it.
-                copyResults.add(copyAssetFile(assetManager, from, to))
-            } else {
-                File(to).mkdirs()
-                // Copy files in parallel
-                val results = files.map { file ->
-                    async {
-                        val inputPath = "$from/$file"
-                        val outputPath = "$to/$file"
-                        // There may be nested directories
-                        val result = copyAssetsDir(assetManager, inputPath, outputPath)
-                        copyResults.add(result)
-                    }
+        if (files == null) {
+            throw Exception("Error copying assets directory $from")
+        } else if (files.isEmpty()) {
+            //If it's a file, it won't have any assets "inside" it.
+            copyAssetFile(assetManager, from, to)
+        } else {
+            File(to).mkdirs()
+            val copyTasks = files.map{ file ->
+                async {
+                    val fromPath = "$from/$file"
+                    val toPath = "$to/$file"
+                    copyAssetsDir(assetManager, fromPath, toPath)
                 }
-                // Wait for all copies to finish
-                results.awaitAll()
             }
-
-            // Fail if any copy failed
-            if (copyResults.any { !it }) {
-                throw Exception("Error copying assets")
-            }
-            // Regular returns don't work in coroutines
-            return@coroutineScope true
-        } catch (e: Exception) {
-            Log.e(TAG, "Error copying asset directory: $from", e)
-            return@coroutineScope false
+            copyTasks.awaitAll()
         }
+
     }
 
     // Copy files with IO coroutine context to prevent blocking
-    suspend fun copyAssetFile(assetManager: AssetManager, from: String, to: String) = withContext(Dispatchers.IO) {
-        try {
-            val inputStream = assetManager.open(from)
-            File(to).createNewFile()
-            val outputStream = FileOutputStream(to)
-            inputStream.copyTo(outputStream)
-            inputStream.close()
-            outputStream.flush()
-            outputStream.close()
-            // Regular returns don't work in coroutines
-            return@withContext true
-        } catch (e: Exception) {
-            Log.e(TAG, "Error copying asset file: $from", e)
-            return@withContext false
-        }
+    suspend fun copyAssetFile(assetManager: AssetManager, from: String, to: String): Unit = withContext(Dispatchers.IO) {
+        val inputStream = assetManager.open(from)
+        File(to).createNewFile()
+        val outputStream = FileOutputStream(to)
+        inputStream.copyTo(outputStream)
+        inputStream.close()
+        outputStream.flush()
+        outputStream.close()
     }
 }
