8e58da672be89322b88d5d21408fd766a45fa7cb
Ben Bucksch <ben.bucksch@beonex.com>
2025-12-23 03:43:21 +0100

    Simplify hostname condition

--- app/logic/Auth/OAuth2Util.ts
+++ app/logic/Auth/OAuth2Util.ts
@@ -3,14 +3,15 @@ import { OAuth2URLs } from "./OAuth2URLs";
 import { OAuth2 } from "./OAuth2";
 import { OWAAuth } from "./OWAAuth";
 import type { Account } from "../Abstract/Account";
+import type { TCPAccount } from "../Abstract/TCPAccount";
 import type { OWAAccount } from "../Mail/OWA/OWAAccount";
-import { TCPAccount } from "../Abstract/TCPAccount";
 
 export function getOAuth2BuiltIn(config: Account): WebBasedAuth | undefined {
   if (config.protocol == "owa") {
     return new OWAAuth(config as OWAAccount);
   }
-  let hostname = config instanceof TCPAccount ? config.hostname : config.url ? new URL(config.url).hostname : null;
+  let hostname = (config as TCPAccount).hostname ??
+    config.url ? new URL(config.url).hostname : null;
   let o = OAuth2URLs.find(o => o.hostnames.some(h => h == hostname));
   if (!o) {
     return undefined;
