4c054d6563360f32fd004b001a462b355005f28d
Jeremy Chang <jeremy.chang@beonex.com>
2026-01-06 15:05:15 -0600

    Mobile WebView Browser

--- app/frontend/Shared/Browser.svelte
+++ app/frontend/Shared/Browser.svelte
@@ -22,6 +22,8 @@
 
 // #if [!WEBMAIL && !MOBILE]
 <webview bind:this={webviewE} src={url} {title} {partition} />
+// #elif [!WEBMAIL && MOBILE]
+<webview bind:this={webviewE} />
 // #else
 <iframe bind:this={webviewE} src={url} {title} sandbox="allow-scripts allow-forms" />
 // #endif
@@ -30,6 +32,9 @@
   import { createEventDispatcher } from 'svelte';
   import RoundButton from './RoundButton.svelte';
   import Loader from './Loader.svelte';
+  // #if [!WEBMAIL && MOBILE]
+  import { InAppBrowser } from '@capgo/inappbrowser';
+  // #endif
   import CloseIcon from "lucide-svelte/icons/x";
   import { getBaseDomainFromHost } from '../../logic/util/netUtil';
   import type { URLString } from '../../logic/util/util';
@@ -63,6 +68,7 @@
 
   let webviewE: HTMLIFrameElement = null;
   $: webviewE && haveWebView();
+  // #if [!WEBMAIL && !MOBILE]
   function haveWebView () {
     webviewE.addEventListener("dom-ready", () => {
       currentURL = webviewE.src;
@@ -78,6 +84,34 @@
       }
     });
   }
+  // #else
+  async function haveWebView () {
+    await InAppBrowser.addListener("urlChangeEvent", () => {
+      currentURL = webviewE.src;
+      dispatch("page-change", webviewE.src);
+    });
+    await InAppBrowser.addListener("browserPageLoaded", async () => {
+      isLoading = false;
+      if (autofill) {
+        await InAppBrowser.executeScript({ code: autofill });
+      }
+    });
+    let x = webviewE.offsetLeft;
+    let y = webviewE.offsetTop;
+    let height = webviewE.height;
+    let width = webviewE.width;
+    await InAppBrowser.openWebView({
+      url: url,
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Mobile Safari/537.36",
+      },
+      x: x,
+      y: y,
+      width: parseInt(width),
+      height: parseInt(height),
+    });
+  };
+  // #endif
 
   function onClose() {
     dispatch("close");
