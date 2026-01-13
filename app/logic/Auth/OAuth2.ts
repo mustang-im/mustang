b782e6bff3adabaa902eb57e6182bf80bcc7a6f1
Ben Bucksch <1907525+benbucksch@users.noreply.github.com>
2026-01-13 02:10:46 +0100

    Wrong way around

--- app/logic/Auth/OAuth2.ts
+++ app/logic/Auth/OAuth2.ts
@@ -168,7 +168,7 @@ export class OAuth2 extends WebBasedAuth {
   }
 
   isExpired(): boolean {
-    return this.expiresAt.getTime() + 2000 > Date.now();
+    return this.expiresAt.getTime() + 2000 < Date.now();
   }
 
   /**
