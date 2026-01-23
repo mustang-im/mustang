aee5bb55d2fcfe4dfa6cb5abde1decfadfb09576
Ben Bucksch <ben.bucksch@beonex.com>
2026-01-23 03:48:02 +0100

    Mobile: Chat: Open room on tap

--- app/frontend/Chat/RoomsM.svelte
+++ app/frontend/Chat/RoomsM.svelte
@@ -1,40 +1,30 @@
 <vbox flex class="pane">
   <Header bind:selectedAccount={$selectedAccount} {accounts} />
-  <RoomList {rooms} bind:selectedRoom={$selectedRoom} {doSearch} />
+  <RoomList {rooms} bind:selectedRoom {doSearch} />
 </vbox>
 {#if $appGlobal.isMobile}
   <RoomsBarM />
 {/if}
 
 <script lang="ts">
-  import { Person } from "../../logic/Abstract/Person";
-  import { selectedAccount, selectedRoom } from "./selected";
+  import { ChatRoom } from "../../logic/Chat/ChatRoom";
+  import { openChatRoomFromOtherApp } from "./open";
+  import { selectedAccount } from "./selected";
   import { selectedWorkspace } from "../MainWindow/Selected";
-  import { selectedPerson } from "../Contacts/Person/Selected";
   import { appGlobal } from "../../logic/app";
   import Header from "./Header.svelte";
   import RoomList from "./RoomList.svelte";
   import RoomsBarM from "./RoomsBarM.svelte";
   import { mergeColls } from "svelte-collections";
-  import { onMount } from "svelte";
 
   export let doSearch = false;
 
   $: accounts = appGlobal.chatAccounts.filterObservable(acc => acc.workspace == $selectedWorkspace || !$selectedWorkspace);
   $: rooms = $selectedAccount ? $selectedAccount.rooms : mergeColls(accounts.map(a => a.rooms));
 
-  onMount(() => {
-    $selectedRoom = $selectedPerson && rooms.find(room => room.contact == $selectedPerson);
-  });
-  $: if ($selectedRoom?.contact instanceof Person) {
-    $selectedPerson = $selectedRoom.contact;
-  }
-  $: rooms, clearSelectedRoom()
-  function clearSelectedRoom() {
-    if (!rooms.contains($selectedRoom)) {
-      $selectedRoom = rooms.last;
-    }
-  }
+  let selectedRoom: ChatRoom;
+
+  $: selectedRoom && openChatRoomFromOtherApp(selectedRoom);
 </script>
 
 <style>
