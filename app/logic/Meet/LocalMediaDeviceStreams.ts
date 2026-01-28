mustang-im/mustang

commit 3da43bbefba52ebf3154f67de4059ed32cc5d434

Mac: Request camera and mic access #937 (#993)

app/logic/Meet/LocalMediaDeviceStreams.ts
@@ -1,4 +1,5 @@
 import { MediaDeviceStreams } from "./MediaDeviceStreams";
+import { appGlobal } from "../app";
 import { notifyChangedAccessor, notifyChangedProperty } from "../util/Observable";
 import { Lock } from "../util/Lock";
 import { assert } from "../util/util";
@@ -37,6 +38,19 @@ export class LocalMediaDeviceStreams extends MediaDeviceStreams {
     await this.setCameraMicOn(this._cameraOn, on, this._cameraDevice, device);
   }
   async setCameraMicOn(cameraOn: boolean, micOn: boolean, cameraDevice: string = this._cameraDevice, micDevice: string = this._micDevice) {
+    let platform = await appGlobal.remoteApp.platform();
+    const isMac = platform == "darwin";
+    if (isMac) {
+      if (cameraOn) {
+        let cameraAccess = await appGlobal.remoteApp.askForMediaAccess('camera');
+        assert(cameraAccess, gt`Camera access denied`);
+      }
+      if (micOn) {
+        let micAccess = await appGlobal.remoteApp.askForMediaAccess('microphone');
+        assert(micAccess, gt`Microphone access denied`);
+      }
+    }
+
     cameraDevice ??= this._cameraDevice;
     micDevice ??= this._micDevice;
     let lock = await this._lock.lock();

backend/backend.ts
@@ -8,7 +8,7 @@ import { ImapFlow } from 'imapflow';
 import { Database } from "@radically-straightforward/sqlite"; // formerly @leafac/sqlite
 import Zip from "adm-zip";
 import ky from 'ky';
-import { shell, nativeTheme, Notification, Tray, nativeImage, app, BrowserWindow, webContents, Menu, MenuItemConstructorOptions, clipboard, NativeImage, session, desktopCapturer, type DesktopCapturerSource, autoUpdater } from "electron";
+import { shell, nativeTheme, Notification, Tray, nativeImage, app, BrowserWindow, webContents, Menu, MenuItemConstructorOptions, clipboard, NativeImage, session, desktopCapturer, type DesktopCapturerSource, autoUpdater, systemPreferences } from "electron";
 import electronUpdater from 'electron-updater';
 import nodemailer from 'nodemailer';
 import MailComposer from 'nodemailer/lib/mail-composer';
@@ -58,6 +58,7 @@ async function createSharedAppObject() {
     startupArgs,
     isDefaultApp,
     setAsDefaultApp,
+    askForMediaAccess,
     onScreenSharingSelect,
     restartApp,
     checkForUpdate,
@@ -375,6 +376,14 @@ function setAsDefaultApp(protocol: string) {
 
 function showFileInFolder(filePath: string) {
   shell.showItemInFolder(filePath);
+}
+
+async function askForMediaAccess(mediaType: string) {
+  if (systemPreferences.getMediaAccessStatus(mediaType) == "granted") {
+    return true;
+  } else {
+    return await systemPreferences.askForMediaAccess(mediaType);
+  }
 }
 
 function onScreenSharingSelect(onSelect: (screens: DesktopCapturerSource[], error?: Error) => Promise<DesktopCapturerSource>,

e2/build/entitlements.mac.plist
@@ -10,5 +10,11 @@
     <true/>
     <key>com.apple.security.cs.disable-library-validation</key>
     <true/>
+    <key>com.apple.security.device.camera</key>
+    <true/>
+    <key>com.apple.security.device.microphone</key>
+    <true/>
+    <key>com.apple.security.device.audio-input</key>
+    <true/>
   </dict>
 </plist>

commit 3c4e8cc186f497ca23f2c94107f8e39be6c3d0f5

Meet: Web: On Mac, take webmail, not Mac desktop - regressed by 3da43bbef - #937 #993

app/logic/Meet/LocalMediaDeviceStreams.ts
@@ -3,6 +3,7 @@ import { appGlobal } from "../app";
 import { notifyChangedAccessor, notifyChangedProperty } from "../util/Observable";
 import { Lock } from "../util/Lock";
 import { assert } from "../util/util";
+import { webMail } from "../build";
 import { gt } from "../../l10n/l10n";
 
 /** Grabs the user's camera, mic or screen, and
@@ -38,7 +39,7 @@ export class LocalMediaDeviceStreams extends MediaDeviceStreams {
     await this.setCameraMicOn(this._cameraOn, on, this._cameraDevice, device);
   }
   async setCameraMicOn(cameraOn: boolean, micOn: boolean, cameraDevice: string = this._cameraDevice, micDevice: string = this._micDevice) {
-    const isMac = navigator.platform?.includes("Mac");
+    const isMac = navigator.platform?.includes("Mac") && !webMail;
     if (isMac) {
       if (cameraOn) {
         let cameraAccess = await appGlobal.remoteApp.askForMediaAccess('camera');