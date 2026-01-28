aa12bc91650aa1e616dc5b0f609cf8f83a912557
Ben Bucksch <ben.bucksch@beonex.com>
2026-01-28 11:46:23 +0100

    Calendar: Invitation incoming in mail: Button to go to calendar on that day; Change color of proposal

--- app/frontend/Calendar/DisplayEvent/EventInDayView.svelte
+++ app/frontend/Calendar/DisplayEvent/EventInDayView.svelte
@@ -1,6 +1,17 @@
 <vbox class="calendar" flex>
-  <hbox class="date header">
-    {getFormattedDateString(calendarStart, { weekday: "short", day: "numeric", month: "short" })}
+  <hbox class="header">
+    <hbox flex />
+    <hbox class="date">
+      {getFormattedDateString(calendarStart, { weekday: "short", day: "numeric", month: "short" })}
+    </hbox>
+    <hbox class="buttons" flex>
+      <RoundButton
+        label={$t`Open calendar`}
+        icon={CalendarIcon}
+        onClick={() => openEventFromOtherApp(event, false)}
+        border={false}
+        />
+    </hbox>
   </hbox>
   <DayViewGrid
     events={appGlobal.calendarEvents}
@@ -21,10 +32,14 @@
 
 <script lang="ts">
   import type { Event } from "../../../logic/Calendar/Event";
+  import { openEventFromOtherApp } from "../open";
   import { appGlobal } from "../../../logic/app";
   import DayViewGrid from "../DayView/DayViewGrid.svelte";
-  import { getFormattedDateString } from "../../Util/date";
   import EventProposal from "../SML/Shared/EventProposal.svelte";
+  import RoundButton from "../../Shared/RoundButton.svelte";
+  import CalendarIcon from "lucide-svelte/icons/calendar";
+  import { getFormattedDateString } from "../../Util/date";
+  import { t } from "../../../l10n/l10n";
   import { ArrayColl } from "svelte-collections";
 
   export let event: Event;
@@ -33,8 +48,11 @@
 </script>
 
 <style>
-  .date.header {
-    justify-content: center;
+  .header {
+    align-items: center;
+  }
+  .buttons {
+    justify-content: end;
   }
   .calendar :global(.header) {
     background-color: var(--leftbar-bg);
@@ -49,5 +67,9 @@
   .proposal {
     height: 100%;
     width: 100%;
+  }
+  .proposal :global(.event-proposal) {
+    background-color: rgb(0, 159, 159);
+    color: white;
   }
 </style>
