d483b4aa43b78ff617fd0e7dc3c3eb15efa63f32
Ben Bucksch <ben.bucksch@beonex.com>
2026-02-12 06:15:27 +0100

    Logging: Add search field

--- app/frontend/Settings/About/Debug.svelte
+++ app/frontend/Settings/About/Debug.svelte
@@ -1,8 +1,14 @@
 <vbox class="dialog" flex>
-  <h1>{$t`Debug log`}</h1>
+  <hbox class="header">
+    <hbox class="title">{$t`Debug log`}</hbox>
+    <hbox flex />
+    <hbox class="search">
+      <input type="search" bind:value={searchInput} placeholder={$t`Search`} />
+    </hbox>
+  </hbox>
   <Scroll>
     <div class="log-history" bind:this={logHistoryEl}>
-      {#each $logHistory.each as log}
+      {#each $filteredLog.each as log}
         <div class="line selectable value">
           <span class="time">{log.time.toISOString().substring(11, 23)}</span>
           <span class="message">{log.message()}</span>
@@ -28,6 +34,10 @@
   import Scroll from "../../Shared/Scroll.svelte";
   import { t } from "../../../l10n/l10n";
 
+  let searchInput = "";
+  $: searchTerm = searchInput.toLowerCase();
+  $: filteredLog = logHistory.filterObservable(entry => entry.message().toLowerCase().includes(searchTerm));
+
   let logHistoryEl: HTMLDivElement;
   async function copyLogHistory() {
     navigator.clipboard.writeText(logHistoryEl.innerText + "\n");
@@ -35,8 +45,13 @@
 </script>
 
 <style>
-  .dialog {
-    margin-block-start: -32px;
+  .header {
+    margin-block-end: 12px;
+    padding-inline-end: 12px;
+  }
+  .title {
+    font-size: 24px;
+    font-weight: bold;
   }
   .line {
     white-space: pre-wrap;
