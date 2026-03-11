e6e3321154748400c5684d84ce572a0e37e2b281
Jeremy Chang <jeremy.chang@beonex.com>
2026-03-11 17:40:53 -0600

    SQLEMailMigrate: Don't fail to increment user_version

--- app/logic/Mail/SQL/SQLEmailMigrate.ts
+++ app/logic/Mail/SQL/SQLEmailMigrate.ts
@@ -1,8 +1,7 @@
 import sql from "../../../../lib/rs-sqlite";
 
 // <copied from="createDatabase.ts">
-// Runs or fails silently and doesn't increment user_version
 export const createFolderIDDateSentIndex = sql`
-  CREATE INDEX index_email_folderID_dateSent
+  CREATE INDEX IF NOT EXISTS index_email_folderID_dateSent
   ON email (folderID, dateSent DESC);
 `;
