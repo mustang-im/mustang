mustang-im/mustang

commit 3a9fd7cdde79d479f07356c51daa9a28bb37b981

Account: Generic logout() and disconnect()

app/logic/Mail/JMAP/JMAPAccount.ts
@@ -407,12 +407,10 @@ export class JMAPAccount extends MailAccount {
     }
   }
 
-  async logout(): Promise<void> {
+  async disconnect(): Promise<void> {
     this.stopPolling();
     this.session = null;
-    if (this.oAuth2) {
-      await this.oAuth2.logout();
-    }
+    await super.logout();
   }
 
   async send(email: EMail): Promise<void> {

app/logic/Abstract/Account.ts
@@ -110,6 +110,8 @@ export class Account extends Observable {
   }
 
   async logout(): Promise<void> {
+    await this.disconnect();
+    await this.oAuth2?.logout();
   }
 
   /**

app/logic/Mail/ActiveSync/ActiveSyncAccount.ts
@@ -125,10 +125,6 @@ export class ActiveSyncAccount extends MailAccount {
     });
   }
 
-  async logout(): Promise<void> {
-    await this.oAuth2?.logout();
-  }
-
   needsLicense(): boolean {
     return true;
   }

app/logic/Mail/EWS/EWSAccount.ts
@@ -140,12 +140,11 @@ export class EWSAccount extends MailAccount {
   }
 
   async logout(): Promise<void> {
-    if (this.mainAccount) {
+    if (this.mainAccount) { // TODO Why?
       await this.mainAccount.logout();
       return;
     }
-    await this.unsubscribeAllNotifications();
-    await this.oAuth2?.logout();
+    await super.logout();
   }
 
   async disconnect(): Promise<void> {

app/logic/Mail/Graph/GraphAccount.ts
@@ -357,11 +357,8 @@ export class GraphAccount extends MailAccount {
     }
   }
 
-  async logout(): Promise<void> {
+  async disconnect(): Promise<void> {
     this.stopPolling();
-    if (this.oAuth2) {
-      await this.oAuth2.logout();
-    }
   }
 
   async send(email: EMail): Promise<void> {

app/logic/Mail/OWA/OWAAccount.ts
@@ -214,14 +214,13 @@ export class OWAAccount extends MailAccount {
   }
 
   async logout(): Promise<void> {
-    if (this.mainAccount) {
+    if (this.mainAccount) { // TODO Why?
       await this.mainAccount.logout();
       return;
     }
     this.hasLoggedIn = false;
-    if (this.oAuth2) {
-      await this.oAuth2.logout();
-    } else {
+    await super.logout();
+    if (!this.oAuth2) {
       await appGlobal.remoteApp.OWA.clearStorageData(this.partition);
     }
   }

commit 15c791af99719261e9a9c16e461265668030809a

JMAP: Fix logout - regressed by 3a9fd7cdd

app/logic/Mail/JMAP/JMAPAccount.ts
@@ -410,7 +410,6 @@ export class JMAPAccount extends MailAccount {
   async disconnect(): Promise<void> {
     this.stopPolling();
     this.session = null;
-    await super.logout();
   }
 
   async send(email: EMail): Promise<void> {