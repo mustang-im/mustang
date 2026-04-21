mustang-im/mustang

commit 23e4bc82b43b37c8631e68a074d12e4b17533425

Calendar: Make it possible to edit and delete unedited instances again (#1128)

app/logic/Calendar/Event.ts
@@ -364,9 +364,12 @@ export class Event extends Observable {
    * copyEditableFieldsFrom and recurring events need extra care. */
   copyFrom(original: Event) {
     this.copyEditableFieldsFrom(original);
-    this.recurrenceStartTime = original.recurrenceStartTime ? new Date(original.recurrenceStartTime) : null;
+    this.recurrenceStartTime = original.recurrenceStartTime;
     this.recurrenceCase = original.recurrenceCase;
     this.recurrenceRule = original.recurrenceRule;
+    this.instances.replaceAll(original.instances);
+    this.exceptions.replaceAll(original.exceptions);
+    this.exclusions.replaceAll(original.exclusions);
     this.parentEvent = original.parentEvent;
   }
 
@@ -580,8 +583,10 @@ export class Event extends Observable {
    * Saves the event to the server and to the database.
    */
   async save() {
-    await this.saveLocally();
+    // Temporary to work around #1127 - save to server first,
+    // while the event's parent is still unedited.
     await this.saveToServer();
+    await this.saveLocally();
   }
 
   /**

app/frontend/Calendar/DisplayEvent/DialogHeader.svelte
@@ -113,6 +113,7 @@
     if (event.seriesStatus == "only") {
       await event.parentEvent.deleteIt();
     } else {
+      event.parentEvent?.cancelEditing();
       await event.deleteIt();
     }
     $selectedEvent = null;

app/frontend/Calendar/EditEvent/DialogHeader.svelte
@@ -217,6 +217,7 @@
   }
 
   async function onSave() {
+    event.parentEvent?.cancelEditing();
     await saveEvent(event);
     onClose();
   }
@@ -263,8 +264,8 @@
   }
 
   async function onSaveException() {
-    await saveEvent(event);
     event.parentEvent?.cancelEditing();
+    await saveEvent(event);
     onClose();
   }
 
@@ -273,6 +274,7 @@
     if (event.seriesStatus == "only") {
       await event.parentEvent.deleteIt();
     } else {
+      event.parentEvent?.cancelEditing();
       await event.deleteIt();
     }
     $selectedEvent = null;

commit 4f8b18f6b407a5aa6ab89c9f8ef29610a7d3f674

Calendar: Repeat: Only restore instances, exceptions and exclusions of unedited recurring masters (#1170)
Regression from #1128, which while it fixed editing events and recurring master events, broke cloning events, causing issue #1164. I checked and this change doesn't seem to regress #1128.

app/logic/Calendar/Event.ts
@@ -367,9 +367,11 @@ export class Event extends Observable {
     this.recurrenceStartTime = original.recurrenceStartTime;
     this.recurrenceCase = original.recurrenceCase;
     this.recurrenceRule = original.recurrenceRule;
-    this.instances.replaceAll(original.instances);
-    this.exceptions.replaceAll(original.exceptions);
-    this.exclusions.replaceAll(original.exclusions);
+    if (this.recurrenceRule) {
+      this.instances.replaceAll(original.instances);
+      this.exceptions.replaceAll(original.exceptions);
+      this.exclusions.replaceAll(original.exclusions);
+    }
     this.parentEvent = original.parentEvent;
   }
 