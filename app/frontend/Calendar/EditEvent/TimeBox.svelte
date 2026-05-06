7293c9191aaa99b8ccfde55a9d1859528a13e07d
Jeremy Chang <jeremy.chang@beonex.com>
2026-05-06 11:12:32 -0600

    Comment out TimezonePicker because it uses svelte/internal which is no longer supported in Svelte 5

--- app/frontend/Calendar/EditEvent/TimeBox.svelte
+++ app/frontend/Calendar/EditEvent/TimeBox.svelte
@@ -66,7 +66,7 @@
     {#if showTimezone}
       <vbox class="timezone">
         <hbox class="timezone-selector">
-          <TimezonePicker bind:timezone={event.timezone} />
+          <!-- <TimezonePicker bind:timezone={event.timezone} /> -->
           <hbox class="buttons">
             <RoundButton
               label={$t`Back to local timezone`}
@@ -132,7 +132,7 @@
   import GlobeIcon from "lucide-svelte/icons/globe";
   import MultipleDaysIcon from "lucide-svelte/icons/calendar-plus";
   import XIcon from "lucide-svelte/icons/x";
-  import TimezonePicker from "timezone-picker-svelte";
+  // import TimezonePicker from "timezone-picker-svelte";
   import { myTimezone, isSameTimezone } from "../../Util/date";
   import { getDateTimeLocale, t } from "../../../l10n/l10n";
 
