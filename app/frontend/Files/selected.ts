5691eaa53e6734b6148ebf449a583e2c018c9310
Ben Bucksch <ben.bucksch@beonex.com>
2026-06-04 08:41:13 +0200

    Files UI: Default to Cloud files, if such accounts are set up

--- app/frontend/Files/selected.ts
+++ app/frontend/Files/selected.ts
@@ -7,12 +7,13 @@ import { isDemo } from "../AppsBar/demo";
 import { FilesView } from "./LeftPane/PaneViewSwitcher.svelte";
 import { writable, type Writable, get } from "svelte/store";
 import { ArrayColl } from "svelte-collections";
+import { appGlobal } from "../../logic/app";
 
 export let selectedFile: Writable<FileOrDirectory> = writable(null);
 export let selectedFiles = new ArrayColl<FileOrDirectory>();
 export let selectedFolder: Writable<Directory> = writable(null);
 export let selectedAccount: Writable<FileSharingAccount> = writable(null);
-export let selectedLeftTab: Writable<FilesView> = writable(get(isDemo) ? FilesView.Harddrive : FilesView.Person);
+export let selectedLeftTab: Writable<FilesView> = writable(appGlobal.fileSharingAccounts.hasItems ? FilesView.CloudStorage : FilesView.Person);
 
 /** If set, this file will be display as on the right pane, full page.
  * Only for some supported file types.
