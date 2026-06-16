9a13df781b53f54a596de75085bb604045b32fe9
Ben Bucksch <ben.bucksch@beonex.com>
2026-06-16 09:22:00 +0200

    Signal: Use Signal's root CA for .signal.org hosts

--- desktop/src/main/index.ts
+++ desktop/src/main/index.ts
@@ -4,6 +4,7 @@ import { ipcMain } from 'electron/main';
 import { join } from 'path'
 import { electronApp, is } from '@electron-toolkit/utils'
 import icon from '../../build/icon.png?asset'
+import { installSignalServiceCATrust } from './signalServiceCA'
 
 function createWindow(): void {
   try {
@@ -142,6 +143,7 @@ async function whenReady() {
   handleCommandline(process.argv.splice(1));
 
   allowCrossDomainRequestsFromFrontend();
+  installSignalServiceCATrust(); // trust Signal's private service CA (chat/storage/cdn/cdsi/sfu)
 
   createMenu();
   createWindow();
