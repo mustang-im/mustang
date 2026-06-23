mustang-im/mustang

commit d8503dfe661057acc57bee2a571f327b6d1fe1fc

Contacts: Addressbook color not updating (#1255)

app/frontend/Contacts/AddressbookChanger.svelte
@@ -1,5 +1,5 @@
 <AccountDropDown
-  bind:selectedAccount={selectedAddressbook}
+  selectedAccount={selectedAddressbook}
   accounts={addressbooks}
   filterByWorkspace={false}
   icon={selectedAddressbook?.icon ?? AddressbookIcon}
@@ -19,7 +19,7 @@
   export let person: Person;
   export let withLabel = false;
 
-  let selectedAddressbook = person?.addressbook;
+  $: selectedAddressbook = person?.addressbook;
   $: addressbooks = appGlobal.addressbooks.filter(acc => acc.workspace == $selectedWorkspace || !$selectedWorkspace);
 
   async function onChangeAddressbook(newAddressbook: Addressbook) {

commit 9a1d3369c6d77915efc63764658f487ae5d3f99c

Contacts: Handle changing the addressbook of a new contact (#1268)
- Kind of a regression from #1255 because we weren't getting the latest address book to change to, `$: selectedAddress` is already assigned before onChangeAddressbook and is updated after onChangeAddressbook which never happens in this case
- `person.moveToAddressbook(newAddressbook)` causes the UI to create another contact because it cannot remove an unsaved contact

app/frontend/Contacts/AddressbookChanger.svelte
@@ -4,7 +4,7 @@
   filterByWorkspace={false}
   icon={selectedAddressbook?.icon ?? AddressbookIcon}
   {withLabel}
-  on:select={() => catchErrors(() => onChangeAddressbook(selectedAddressbook))}
+  on:select={(ev) => catchErrors(() => onChangeAddressbook(ev.detail as Addressbook))}
   />
 
 <script lang="ts">
@@ -24,6 +24,10 @@
 
   async function onChangeAddressbook(newAddressbook: Addressbook) {
     console.log("Selected addressbook", newAddressbook?.name, "old", person?.addressbook?.name);
-    person.moveToAddressbook(newAddressbook);
+    if (!person.dbID) {
+      person.addressbook = newAddressbook;
+    } else {
+      person.moveToAddressbook(newAddressbook);
+    }
   }
 </script>