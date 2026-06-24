591756f17ebf6d05201f5ad102afe762f8343d62
Ben Bucksch <ben.bucksch@beonex.com>
2026-06-25 00:51:16 +0200

    Calendar: New event button: Add account dropdown when clicking the (+) button directly

--- app/frontend/Calendar/NewEventButton.svelte
+++ app/frontend/Calendar/NewEventButton.svelte
@@ -1,15 +1,27 @@
-<hbox class="new-event-button">
-  <RoundButton
-    classes="add-button create"
-    label={$t`New event`}
-    icon={AddToCalendarIcon}
-    onClick={addEvent}
-    iconSize="22px"
-    padding="6px"
-    />
+<hbox class="new-event-button" style:background-color={$selectedCalendar.color}>
+  <ButtonMenu bind:isMenuOpen>
+    <RoundButton
+      classes="add-button create"
+      label={$t`New event`}
+      icon={AddToCalendarIcon}
+      iconSize="22px"
+      padding="6px"
+      slot="control"
+      onClick={() => isMenuOpen = !isMenuOpen}
+      />
+    {#each appGlobal.calendars.each as calendar}
+      <hbox class="menuitem" style="--calendar-color: {calendar.color}">
+        <MenuItem
+          icon={calendar.icon ?? CalendarIcon}
+          label={calendar.name}
+          onClick={() => addEvent(calendar)}
+          />
+      </hbox>
+    {/each}
+  </ButtonMenu>
   <AccountDropDown
     accounts={appGlobal.calendars}
-    selectedAccount={$selectedCalendar}
+    bind:selectedAccount={$selectedCalendar}
     withLabel={false}
     filterByWorkspace={true}
     icon={CalendarIcon}
@@ -19,19 +31,22 @@
 <script lang="ts">
   import { setNewEventTime } from "./event";
   import { openEvent } from "./open";
+  import type { Calendar } from "../../logic/Calendar/Calendar";
   import { selectedCalendar, selectedDate } from "./selected";
   import { appGlobal } from "../../logic/app";
   import AccountDropDown from "../Shared/AccountDropDown.svelte";
+  import ButtonMenu from "../Shared/Menu/ButtonMenu.svelte";
+  import MenuItem from "../Shared/Menu/MenuItem.svelte";
   import RoundButton from "../Shared/RoundButton.svelte";
   import AddToCalendarIcon from "lucide-svelte/icons/plus";
   import CalendarIcon from "lucide-svelte/icons/calendar";
-  import { assert } from "../../logic/util/util";
   import { t } from "../../l10n/l10n";
 
-  function addEvent() {
-    $selectedCalendar ??= appGlobal.calendars.first;
-    assert($selectedCalendar, $t`Please set up a calendar first`);
-    let event = $selectedCalendar.newEvent();
+  let isMenuOpen = false;
+
+  function addEvent(calendar: Calendar) {
+    $selectedCalendar = calendar;
+    let event = calendar.newEvent();
     setNewEventTime(event, false, $selectedDate);
     openEvent(event, true);
   }
@@ -52,5 +67,14 @@
     justify-content: center;
     position: relative;
     margin-inline-end: 8px;
+  }
+  .menuitem :global(.menuitem .icon) {
+    color: var(--calendar-color);
+  }
+  .new-event-button :global(.account-selector .icon) {
+    color: white;
+  }
+  .new-event-button :global(.account-selector select) {
+    color: white;
   }
 </style>
