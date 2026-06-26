3fbcc9f1671d09b89061e8a4e2c38daa59bc0492
Jeremy Chang <jeremy.chang@beonex.com>
2026-06-26 15:25:28 -0600

    move newPerson above lastPersons

--- app/frontend/Contacts/Person/Selected.ts
+++ app/frontend/Contacts/Person/Selected.ts
@@ -3,9 +3,9 @@ import { ArrayColl } from "svelte-collections";
 import { writable } from "svelte/store";
 
 export const selectedPerson = writable<Person>(null);
-export const newPerson = writable<Person>(null);
 export const selectedContactEntry = writable<ContactEntry>(null);
 
+export const newPerson = writable<Person>(null);
 export const lastPersons = new ArrayColl<Person>();
 /*selectedPerson.subscribe(person => {
   lastPersons.remove(person);
