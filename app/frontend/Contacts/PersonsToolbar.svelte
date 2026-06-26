3c09135d86344681afb6f6204a25e09c99551044
Jeremy Chang <jeremy.chang@beonex.com>
2026-06-26 15:38:23 -0600

    Fix icon alignment a little bit

--- app/frontend/Contacts/PersonsToolbar.svelte
+++ app/frontend/Contacts/PersonsToolbar.svelte
@@ -27,8 +27,10 @@
 </hbox>
 {#if $newPerson}
   <hbox class="new-person" class:selected={$selectedPerson == $newPerson}>
-    <PersonLine person={$newPerson} {pictureSize} />
-    <SparklesIcon size={pictureSize} />
+    <PersonLine person={$newPerson} pictureSize={20} />
+    <hbox class="icon">
+      <SparklesIcon size={18} />
+    </hbox>
   </hbox>
 {/if}
 
@@ -52,8 +54,6 @@
   export let persons: Collection<Person>;
   /** in/out */
   export let selectedAddressbook: Addressbook;
-
-  let pictureSize = appGlobal.isMobile ? 32 : 20;
 
   function addPerson() {
     let addressbook = selectedAddressbook ?? appGlobal.addressbooks.first;
@@ -104,6 +104,11 @@
   }
   .new-person {
     margin-block-end: 10px;
+    padding-inline: 2px;
+  }
+  .new-person .icon {
+    margin-block: auto;
+    margin-inline-end: 2px;
   }
   .new-person.selected {
     background-color: var(--selected-bg);
