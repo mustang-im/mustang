ced6d1f47cde38afa41af6d37433e57f41a66b2d
Neil Rashbrook <neil.rashbrook@beonex.com>
2026-06-30 23:05:28 +0100

    Calendar: Exchange: Read all-day events as using local midnight #1271

--- app/logic/Calendar/EWS/ExchangeEvent.ts
+++ app/logic/Calendar/EWS/ExchangeEvent.ts
@@ -1,4 +1,15 @@
 import { Event } from "../Event";
 
 export class ExchangeEvent extends Event {
+  // ActiveSync 16.x gives all-day events as a UTC-style string with no time
+  // part and no time zone. This is special-cased in the ActiveSync code.
+  // Other Exchange protocols provide a time zone and the time in UTC that
+  // would be midnight in that time zone, but we need them to use local time.
+  protected fixupExchangeAllDayEvent() {
+    if (this.allDay && this.timezone) {
+      this.startTime = new Date(this.startTime.toLocaleDateString('lt', { timeZone: this.timezone }) + "T00:00:00");
+      this.endTime = new Date(this.endTime.toLocaleDateString('lt', { timeZone: this.timezone }) + "T00:00:00");
+      this.timezone = null;
+    }
+  }
 }
