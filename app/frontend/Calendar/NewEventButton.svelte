33b89fcec758fcbc38eeb2f974a8c3e89dffeba3
Ben Bucksch <ben.bucksch@beonex.com>
2026-08-26 00:49:25 +0200

    Calendar: + button: Remove the selected account

--- app/frontend/Calendar/NewEventButton.svelte
+++ app/frontend/Calendar/NewEventButton.svelte
@@ -1,4 +1,4 @@
-<hbox class="new-event-button" style:background-color={$selectedCalendar?.color ?? "black"}>
+<hbox class="new-event-button">
   {#if haveMultipleCalendars}
     <ButtonMenu bind:isMenuOpen>
       <RoundButton
@@ -30,22 +30,12 @@
       onClick={() => createNewEvent($selectedCalendar, $selectedDate, false)}
       />
   {/if}
-  {#if haveMultipleCalendars}
-    <AccountDropDown
-      accounts={appGlobal.calendars}
-      bind:selectedAccount={$selectedCalendar}
-      withLabel={false}
-      filterByWorkspace={true}
-      icon={CalendarIcon}
-      />
-  {/if}
 </hbox>
 
 <script lang="ts">
   import { createNewEvent } from "./event";
   import { selectedCalendar, selectedDate } from "./selected";
   import { appGlobal } from "../../logic/app";
-  import AccountDropDown from "../Shared/AccountDropDown.svelte";
   import ButtonMenu from "../Shared/Menu/ButtonMenu.svelte";
   import MenuItem from "../Shared/Menu/MenuItem.svelte";
   import RoundButton from "../Shared/RoundButton.svelte";
@@ -66,21 +56,13 @@
     border-bottom: 1px solid var(--border);
     border-radius: 30px;
     margin: 4px;
-    padding-inline-end: 6px;
   }
   .new-event-button :global(.add-button) {
     align-items: center;
     justify-content: center;
     position: relative;
-    margin-inline-end: 8px;
   }
   .menuitem :global(.menuitem .icon) {
     color: var(--calendar-color);
-  }
-  .new-event-button :global(.account-selector .icon) {
-    color: white;
-  }
-  .new-event-button :global(.account-selector select) {
-    color: white;
   }
 </style>
