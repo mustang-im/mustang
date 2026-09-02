40c875c1db7648565a6cae7a5ba0ae3e86311385
Ben Bucksch <ben.bucksch@beonex.com>
2026-09-02 15:54:02 +0200

    POP3: Setup: Warn that other devices will not see the mails
    Shown when POP3 is selected, from autoconfig or in the manual config.

---
+++ app/frontend/Setup/Mail/POP3Warning.svelte
@@ -0,0 +1,9 @@
+<StatusMessage status="warning" message={$t`POP3 only downloads mails to this device. Your phone and other devices will not see which mails you read, moved or deleted, and once the mails are deleted from the server, they are gone there. If your provider offers IMAP, please use IMAP instead.`}>
+  <AlertIcon slot="icon" size={20} />
+</StatusMessage>
+
+<script lang="ts">
+  import StatusMessage from "../Shared/StatusMessage.svelte";
+  import AlertIcon from "lucide-svelte/icons/alert-triangle";
+  import { t } from "../../../l10n/l10n";
+</script>
