3295fffd6566f744b3a4a8fe73ef9b0da2d39c6b
Ben Bucksch <ben.bucksch@beonex.com>
2026-09-10 02:59:57 +0200

    Topic: Sanitize the HTML - leftover from b8e1a2206

--- app/frontend/Topic/Page/ParagraphDisplay.svelte
+++ app/frontend/Topic/Page/ParagraphDisplay.svelte
@@ -1,10 +1,11 @@
 <vbox class="paragraph font-normal">
-  {@html paragraph.html}
+  {@html sanitizeHTMLWithoutStyleTag($paragraph.html)}
 </vbox>
 
 <script lang="ts">
   import { Paragraph } from "../../../logic/Topic/PageContent";
   import { Topic } from "../../../logic/Topic/Topic";
+  import { sanitizeHTMLWithoutStyleTag } from "../../../logic/util/convertHTML";
 
   export let topic: Topic;
   export let paragraph: Paragraph;
